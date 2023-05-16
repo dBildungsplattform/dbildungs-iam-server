ce3e8ecfdb7844fd34c3ee1a26a031bb472bd0be
Firas Shmit <firas.shmit@capgemini.com>
2023-05-16 09:42:40 +0200

    EW-509 did some change in main

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  try {
+    const app = await NestFactory.create(AppModule);
+    await app.listen(3000);
+    console.log('Server started successfully!');
+  } catch (error) {
+    console.error('Failed to start server:', error);
+  }
 }
 bootstrap();
