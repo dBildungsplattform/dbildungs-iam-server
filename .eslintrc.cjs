93b386eee6d4b56d591ab5408be2b07646d06e94
psachmann <patrick.sachmann@capgemini.com>
2023-05-26 14:59:07 +0200

    EW-497 setting up eslint and prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,58 @@
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+    extends: [
+        'airbnb-typescript/base',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+        'prettier',
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        project: true,
+        tsconfigRootDir: __dirname,
+        sourceType: 'module',
+    },
+    plugins: [
+        '@typescript-eslint/eslint-plugin',
+        'import',
+        'prettier',
+    ],
+    root: true,
+    env: {
+        node: true,
+        jest: true,
+    },
+    ignorePatterns: ['.eslintrc.cjs', '.prettierrc.cjs'],
+    rules: {
+        'prettier/prettier': ['warn'],
+        '@typescript-eslint/explicit-function-return-type': 'error',
+        '@typescript-eslint/no-explicit-any': 'error',
+        'no-void': ['error', { allowAsStatement: true }],
+        'max-classes-per-file': 'off',
+        'class-methods-use-this': 'off',
+        'no-param-reassign': 'off',
+        'no-underscore-dangle': 'off',
+        '@typescript-eslint/unbound-method': 'error',
+        '@typescript-eslint/no-unused-vars': 'error',
+        '@typescript-eslint/no-empty-interface': [
+            'error',
+            {
+                allowSingleExtends: true,
+            },
+        ],
+    },
+    overrides: [
+        {
+            files: ['**/*spec.ts'],
+            plugins: ['jest'],
+            env: {
+                jest: true,
+            },
+            rules: {
+                // you should turn the original rule off *only* for test files
+                '@typescript-eslint/unbound-method': 'off',
+                'jest/unbound-method': 'error',
+            },
+        },
+    ],
+};
---
+++ .prettierrc.cjs
@@ -0,0 +1,8 @@
+/** @type {import("prettier").Config} */
+module.exports = {
+    semi: true,
+    singleQuote: true,
+    printWidth: 120,
+    trailingComma: 'all',
+    endOfLine: 'lf',
+};
