df9737cd675e89c44a9c9a626d835aae27a1a2f4
Firas Shmit <firas.shmit@capgemini.com>
2023-08-16 16:44:56 +0200

    EW-500 fixed lint errors

--- src/modules/person/api/person.controller.ts
+++ src/modules/person/api/person.controller.ts
@@ -40,8 +40,8 @@ export class PersonController {
     @ApiInternalServerErrorResponse({ description: 'Internal server error while getting the person.' })
     public async findPersonById(@Param() params: PersonByIdParams): Promise<PersonResponse | HttpException> {
         try {
-           const person = await this.uc.findPersonById(params.personId);
-           return person;
+            const person = await this.uc.findPersonById(params.personId);
+            return person;
         } catch (error) {
             return new HttpException('Requested entity does not exist', HttpStatus.NOT_FOUND);
         }
