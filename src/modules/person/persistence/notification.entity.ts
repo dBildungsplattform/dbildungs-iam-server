19313d1aba985be8180ab5a069175477be889b09
Kristoff Kiefer <kristoff.kiefer@capgemini.com>
2023-08-23 11:26:16 +0200

    Added associations

--- src/modules/person/persistence/notification.entity.ts
+++ src/modules/person/persistence/notification.entity.ts
@@ -1,5 +1,6 @@
 import { TimestampedEntity } from './timestamped.entity.js';
-import { Entity } from '@mikro-orm/core';
+import { Entity, Property } from '@mikro-orm/core';
+import { PersonEntity } from './person.entity.js';
 
 @Entity({ tableName: 'notification' })
 export class NotificationEntity extends TimestampedEntity<NotificationEntity, 'id'> {
@@ -8,4 +9,10 @@ export class NotificationEntity extends TimestampedEntity<NotificationEntity, 'i
     }
 
     public readonly id!: string;
+
+    @Property()
+    public source!: PersonEntity;
+
+    @Property()
+    public target!: PersonEntity;
 }
