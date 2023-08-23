311f9911a9821b5ce88f8a380f19f4185a3da18c
Kristoff Kiefer <kristoff.kiefer@capgemini.com>
2023-08-23 11:25:59 +0200

    Prettified code

--- src/modules/person/persistence/person.entity.ts
+++ src/modules/person/persistence/person.entity.ts
@@ -1,10 +1,11 @@
-import { randomUUID } from 'crypto';
-import { AutoMap } from '@automapper/classes';
-import { ArrayType, BaseEntity, DateTimeType, Entity, Enum, PrimaryKey, Property } from '@mikro-orm/core';
-import { Gender, TrustLevel } from '../domain/person.enums.js';
+import {randomUUID} from 'crypto';
+import {AutoMap} from '@automapper/classes';
+import {ArrayType, DateTimeType, Entity, Enum, PrimaryKey, Property} from '@mikro-orm/core';
+import {Gender, TrustLevel} from '../domain/person.enums.js';
+import {TimestampedEntity} from "./timestamped.entity.js";
 
-@Entity({ tableName: 'person' })
-export class PersonEntity extends BaseEntity<PersonEntity, 'id'> {
+@Entity({tableName: 'person'})
+export class PersonEntity extends TimestampedEntity<PersonEntity, 'id'> {
     /**
      * @deprecated This constructor is for automapper only.
      */
@@ -12,17 +13,11 @@ export class PersonEntity extends BaseEntity<PersonEntity, 'id'> {
         super();
     }
 
-    @PrimaryKey({ onCreate: () => randomUUID() })
+    @PrimaryKey({onCreate: () => randomUUID()})
     public readonly id!: string;
 
-    @Property({ onCreate: () => new Date(), type: DateTimeType })
-    public readonly createdAt!: Date;
-
-    @Property({ onCreate: () => new Date(), onUpdate: () => new Date(), type: DateTimeType })
-    public readonly updatedAt!: Date;
-
     @AutoMap()
-    @Property({ nullable: true })
+    @Property({nullable: true})
     public referrer?: string;
 
     @AutoMap()
@@ -30,7 +25,7 @@ export class PersonEntity extends BaseEntity<PersonEntity, 'id'> {
     public client!: string;
 
     @AutoMap()
-    @Property({ nullable: true })
+    @Property({nullable: true})
     public readonly mainOrganization?: string;
 
     @AutoMap()
@@ -42,58 +37,58 @@ export class PersonEntity extends BaseEntity<PersonEntity, 'id'> {
     public firstName!: string;
 
     @AutoMap()
-    @Property({ nullable: true })
+    @Property({nullable: true})
     public initialsLastName?: string;
 
     @AutoMap()
-    @Property({ nullable: true })
+    @Property({nullable: true})
     public initialsFirstName?: string;
 
     @AutoMap()
-    @Property({ nullable: true })
+    @Property({nullable: true})
     public nickName?: string;
 
     @AutoMap()
-    @Property({ nullable: true })
+    @Property({nullable: true})
     public nameTitle?: string;
 
     @AutoMap()
-    @Property({ nullable: true, type: ArrayType })
+    @Property({nullable: true, type: ArrayType})
     public nameSalutation?: string[];
 
     @AutoMap()
-    @Property({ nullable: true, type: ArrayType })
+    @Property({nullable: true, type: ArrayType})
     public namePrefix?: string[];
 
     @AutoMap()
-    @Property({ nullable: true, type: ArrayType })
+    @Property({nullable: true, type: ArrayType})
     public nameSuffix?: string[];
 
     @AutoMap()
-    @Property({ nullable: true })
+    @Property({nullable: true})
     public nameSortIndex?: string;
 
     @AutoMap()
-    @Property({ nullable: true, type: DateTimeType })
+    @Property({nullable: true, type: DateTimeType})
     public birthDate?: Date;
 
     @AutoMap()
-    @Property({ nullable: true })
+    @Property({nullable: true})
     public birthPlace?: string;
 
     @AutoMap()
-    @Enum({ items: () => Gender, nullable: true })
+    @Enum({items: () => Gender, nullable: true})
     public gender?: Gender;
 
     @AutoMap()
-    @Property({ nullable: true })
+    @Property({nullable: true})
     public localization?: string = 'de-DE';
 
     @AutoMap()
-    @Enum({ items: () => TrustLevel, nullable: true })
+    @Enum({items: () => TrustLevel, nullable: true})
     public trustLevel?: TrustLevel;
 
     @AutoMap()
-    @Property({ nullable: true })
+    @Property({nullable: true})
     public isInformationBlocked?: boolean;
 }
