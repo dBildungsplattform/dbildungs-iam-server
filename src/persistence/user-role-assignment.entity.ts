0eb49affbb76d29070ebf3e2ce08d9f2219c0fa0
Kristoff Kiefer <kristoff.kiefer@capgemini.com>
2023-08-29 12:51:28 +0200

    Added ref to SchoolStructureNode

--- src/persistence/user-role-assignment.entity.ts
+++ src/persistence/user-role-assignment.entity.ts
@@ -2,15 +2,19 @@ import { TimestampedEntity } from './timestamped.entity.js';
 import { Entity, ManyToOne, PrimaryKey } from '@mikro-orm/core';
 import { PersonEntity } from '../modules/person/persistence/person.entity.js';
 import { RoleEntity } from './role.entity.js';
+import {SchoolStructureNodeEntity} from "./school-structure-node.entity.js";
 
 @Entity({ tableName: 'user_role_assignment' })
 export class UserRoleAssignmentEntity extends TimestampedEntity<UserRoleAssignmentEntity, 'id'> {
     @PrimaryKey()
     public id!: string;
 
-    @ManyToOne(() => PersonEntity)
-    public Person!: PersonEntity;
+    @ManyToOne()
+    public person!: PersonEntity;
 
-    @ManyToOne(() => RoleEntity)
-    public Role!: RoleEntity;
+    @ManyToOne()
+    public role!: RoleEntity;
+
+    @ManyToOne()
+    public schoolStructureNode!: SchoolStructureNodeEntity;
 }
