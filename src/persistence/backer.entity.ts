8943f23d1efebb4146eadf932f8b650858a6972b
Kristoff Kiefer <kristoff.kiefer@capgemini.com>
2023-08-29 12:32:04 +0200

    Renamed Sponsor to Backer

--- src/persistence/backer.entity.ts
+++ src/persistence/backer.entity.ts
@@ -2,4 +2,4 @@ import { SchoolStructureNodeEntity } from './school-structure-node.entity.js';
 import { Entity } from '@mikro-orm/core';
 
 @Entity({ discriminatorColumn: 'nodeType', discriminatorValue: 'sponsor' })
-export class SponsorEntity extends SchoolStructureNodeEntity {}
+export class BackerEntity extends SchoolStructureNodeEntity {}
