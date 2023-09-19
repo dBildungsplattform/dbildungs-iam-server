aae48de53d6b7d3d99d8ef1af82818600fbdc2ee
Kristoff Kiefer <kristoff.kiefer@capgemini.com>
2023-09-19 12:23:39 +0200

    Added basic infrastructure for probes

--- src/health/health.module.ts
+++ src/health/health.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { TerminusModule } from '@nestjs/terminus';
-import { HealthController } from './health.controller.js';
 
-@Module({ imports: [TerminusModule], controllers: [HealthController] })
+@Module({ imports: [TerminusModule] })
 export class HealthModule {}
