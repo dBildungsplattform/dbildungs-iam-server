1f61bfdeef8ab44de908a5418a0e079643bc1ffc
Kristoff Kiefer <kristoff.kiefer@capgemini.com>
2023-09-20 17:02:49 +0200

    Checking for KeyCloak availability

--- src/health/health.controller.spec.ts
+++ src/health/health.controller.spec.ts
@@ -4,10 +4,12 @@ import {
     HealthCheckService,
     HealthIndicatorFunction,
     HealthIndicatorResult,
+    HttpHealthIndicator,
     MikroOrmHealthIndicator,
 } from '@nestjs/terminus';
 import { createMock, DeepMocked } from '@golevelup/ts-jest';
 import { SqlEntityManager } from '@mikro-orm/postgresql';
+import { KeycloakConfig } from '../shared/config/index.js';
 
 describe('HealthController', () => {
     let controller: HealthController;
@@ -15,16 +17,27 @@ describe('HealthController', () => {
     let healthCheckService: DeepMocked<HealthCheckService>;
     let mikroOrmHealthIndicator: MikroOrmHealthIndicator;
     let entityManager: SqlEntityManager;
+    let httpHealthIndicator: DeepMocked<HttpHealthIndicator>;
+    const keycloakConfig: KeycloakConfig = {
+        CLIENT_ID: '',
+        PASSWORD: '',
+        REALM_NAME: '',
+        USERNAME: '',
+        BASE_URL: 'http://keycloak.test',
+    };
     beforeEach(async () => {
         healthCheckService = createMock<HealthCheckService>();
         mikroOrmHealthIndicator = createMock<MikroOrmHealthIndicator>();
         entityManager = createMock<SqlEntityManager>();
+        httpHealthIndicator = createMock<HttpHealthIndicator>();
         const module: TestingModule = await Test.createTestingModule({
             controllers: [HealthController],
             providers: [
                 { provide: HealthCheckService, useValue: healthCheckService },
                 { provide: MikroOrmHealthIndicator, useValue: mikroOrmHealthIndicator },
                 { provide: SqlEntityManager, useValue: entityManager },
+                { provide: HttpHealthIndicator, useValue: httpHealthIndicator },
+                { provide: KeycloakConfig, useValue: keycloakConfig },
             ],
         }).compile();
 
@@ -39,16 +52,21 @@ describe('HealthController', () => {
         await controller.check();
 
         expect(healthCheckService.check).toHaveBeenCalled();
-        const lastCallArgs: jest.ArgsType<HealthCheckService['check']> | undefined =
-            healthCheckService.check.mock.lastCall;
-        expect(lastCallArgs).toHaveLength(1);
-        const indicators: HealthIndicatorFunction[] | undefined = lastCallArgs?.[0];
-        expect(indicators).toHaveLength(1);
+        const indicators: HealthIndicatorFunction[] | undefined = healthCheckService.check.mock.lastCall?.[0];
         const firstIndicator: (() => PromiseLike<HealthIndicatorResult> | HealthIndicatorResult) | undefined =
             indicators?.[0];
-        expect(firstIndicator).not.toBeNull();
-        await firstIndicator?.call(indicators?.[0]);
-
+        expect(firstIndicator).toBeDefined();
+        // Explanation: We get back the lambdas that the HealthCheck would call and call them
+        // ourselves to make sure they do the right things
+        await firstIndicator?.call(firstIndicator);
         expect(mikroOrmHealthIndicator.pingCheck).toHaveBeenCalled();
+
+        const secondIndicator: (() => PromiseLike<HealthIndicatorResult> | HealthIndicatorResult) | undefined =
+            indicators?.[1];
+        expect(secondIndicator).toBeDefined();
+        await secondIndicator?.call(secondIndicator);
+
+        expect(httpHealthIndicator.pingCheck).toHaveBeenCalled();
+        expect(httpHealthIndicator.pingCheck).toBeCalledWith('keycloak', 'http://keycloak.test');
     });
 });
--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -4,24 +4,30 @@ import {
     HealthCheckResult,
     HealthCheckService,
     HealthIndicatorResult,
+    HttpHealthIndicator,
     MikroOrmHealthIndicator,
 } from '@nestjs/terminus';
 import { EntityManager } from '@mikro-orm/postgresql';
+import { KeycloakConfig } from '../shared/config/index.js';
 
 @Controller('health')
 export class HealthController {
     public constructor(
         private health: HealthCheckService,
         private mikroOrm: MikroOrmHealthIndicator,
+        private http: HttpHealthIndicator,
         private em: EntityManager,
+        private keycloakConfig: KeycloakConfig,
     ) {}
 
     @Get()
     @HealthCheck()
     public check(): Promise<HealthCheckResult> {
+        const baseUrl: string = this.keycloakConfig.BASE_URL;
         return this.health.check([
             (): Promise<HealthIndicatorResult> =>
                 this.mikroOrm.pingCheck('database', { connection: this.em.getConnection() }),
+            (): Promise<HealthIndicatorResult> => this.http.pingCheck('keycloak', baseUrl),
         ]);
     }
 }
