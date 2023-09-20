0a2ec6266735e9aa59b6be53c57538076f51fefa
Kristoff Kiefer <kristoff.kiefer@capgemini.com>
2023-09-20 15:34:47 +0200

    Improved Coverage

--- src/health/health.controller.spec.ts
+++ src/health/health.controller.spec.ts
@@ -1,12 +1,26 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { HealthController } from './health.controller.js';
+import { HealthCheckService, HealthIndicatorFunction, MikroOrmHealthIndicator } from '@nestjs/terminus';
+import { createMock, DeepMocked } from '@golevelup/ts-jest';
+import { SqlEntityManager } from '@mikro-orm/postgresql';
 
 describe('HealthController', () => {
     let controller: HealthController;
 
+    let healthCheckService: DeepMocked<HealthCheckService>;
+    let mikroOrmHealthIndicator: MikroOrmHealthIndicator;
+    let entityManager: SqlEntityManager;
     beforeEach(async () => {
+        healthCheckService = createMock<HealthCheckService>();
+        mikroOrmHealthIndicator = createMock<MikroOrmHealthIndicator>();
+        entityManager = createMock<SqlEntityManager>();
         const module: TestingModule = await Test.createTestingModule({
             controllers: [HealthController],
+            providers: [
+                { provide: HealthCheckService, useValue: healthCheckService },
+                { provide: MikroOrmHealthIndicator, useValue: mikroOrmHealthIndicator },
+                { provide: SqlEntityManager, useValue: entityManager },
+            ],
         }).compile();
 
         controller = module.get<HealthController>(HealthController);
@@ -14,5 +28,19 @@ describe('HealthController', () => {
 
     it('should be defined', () => {
         expect(controller).toBeDefined();
+    });
+
+    it('should Perform all health checks', async () => {
+        await controller.check();
+
+        expect(healthCheckService.check).toHaveBeenCalled();
+        const lastCallArgs = healthCheckService.check.mock.lastCall;
+        expect(lastCallArgs).toHaveLength(1);
+        const indicators = lastCallArgs![0];
+        expect(indicators).toHaveLength(1);
+        const firstIndicator: HealthIndicatorFunction = indicators[0]!;
+        await firstIndicator.call(indicators[0]);
+
+        expect(mikroOrmHealthIndicator.pingCheck).toHaveBeenCalled();
     });
 });
