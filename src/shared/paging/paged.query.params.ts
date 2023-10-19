cefaef3a53b854a7dda8b6fad7a27dc094207564
psachmann <patrick.sachmann@capgemini.com>
2023-10-19 13:18:30 +0200

    EW-561 fixing review comments

--- src/shared/paging/paged.query.params.ts
+++ src/shared/paging/paged.query.params.ts
@@ -1,10 +1,11 @@
 import { AutoMap } from '@automapper/classes';
 import { ApiPropertyOptional } from '@nestjs/swagger';
-import { IsOptional } from 'class-validator';
+import { IsOptional, Min } from 'class-validator';
 
 export abstract class PagedQueryParams {
     @AutoMap()
     @IsOptional()
+    @Min(0)
     @ApiPropertyOptional({
         description: 'The offset of the paginated list.',
         required: false,
@@ -13,6 +14,7 @@ export abstract class PagedQueryParams {
 
     @AutoMap()
     @IsOptional()
+    @Min(1)
     @ApiPropertyOptional({
         description: 'The requested limit for the page size.',
         required: false,
