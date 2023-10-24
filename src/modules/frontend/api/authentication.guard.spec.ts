839e2405f904968b22d931e19ddc3eaafe15ee12
Marvin Rode (Cap) <127723478+marode-cap@users.noreply.github.com>
2023-10-24 09:11:46 +0200

    Add backend http service (#54)

--- src/modules/frontend/api/authentication.guard.spec.ts
+++ src/modules/frontend/api/authentication.guard.spec.ts
@@ -3,6 +3,7 @@ import { ExecutionContext } from '@nestjs/common';
 import { HttpArgumentsHost } from '@nestjs/common/interfaces/index.js';
 import { Test, TestingModule } from '@nestjs/testing';
 import { FastifyRequest } from 'fastify';
+import { TokenSet } from 'openid-client';
 
 import { AuthenticatedGuard } from './authentication.guard.js';
 import { SessionData } from './frontend.controller.js';
@@ -29,7 +30,7 @@ describe('AuthenticatedGuard', () => {
     });
 
     describe('canActivate', () => {
-        it('should return true when the session contains a token', () => {
+        it('should return true when the session is valid', () => {
             const sessionMock: DeepMocked<SessionData> = createMock<SessionData>();
             const contextMock: ExecutionContext = createMock<ExecutionContext>({
                 switchToHttp: () =>
@@ -40,14 +41,36 @@ describe('AuthenticatedGuard', () => {
                             }),
                     }),
             });
-            sessionMock.get.mockReturnValueOnce('SomeToken');
+            const tokenSet: TokenSet = new TokenSet();
+            tokenSet.expires_in = 5 * 60; // 5 Minutes
+            sessionMock.get.mockReturnValueOnce(tokenSet);
 
             const result: boolean = sut.canActivate(contextMock);
 
             expect(result).toBe(true);
         });
 
-        it("should return false when the session doesn't contain a token", () => {
+        it('should return false when the tokens are expired', () => {
+            const sessionMock: DeepMocked<SessionData> = createMock<SessionData>();
+            const contextMock: ExecutionContext = createMock<ExecutionContext>({
+                switchToHttp: () =>
+                    createMock<HttpArgumentsHost>({
+                        getRequest: () =>
+                            createMock<FastifyRequest>({
+                                session: sessionMock,
+                            }),
+                    }),
+            });
+            const tokenSet: TokenSet = new TokenSet();
+            tokenSet.expires_in = -5 * 60 * 1_000; // 5 Minutes ago
+            sessionMock.get.mockReturnValueOnce(tokenSet);
+
+            const result: boolean = sut.canActivate(contextMock);
+
+            expect(result).toBe(false);
+        });
+
+        it('should return false when the session has no tokens', () => {
             const sessionMock: DeepMocked<SessionData> = createMock<SessionData>();
             const contextMock: ExecutionContext = createMock<ExecutionContext>({
                 switchToHttp: () =>
