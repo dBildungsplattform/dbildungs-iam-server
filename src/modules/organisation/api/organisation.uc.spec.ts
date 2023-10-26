f2981e2b4bbfd41ca6950cef8efccb448ef62a61
Firas Shmit <firas.shmit@capgemini.com>
2023-10-26 11:53:27 +0200

    EW-499 expanded uc test of orga for findall method

--- src/modules/organisation/api/organisation.uc.spec.ts
+++ src/modules/organisation/api/organisation.uc.spec.ts
@@ -8,6 +8,10 @@ import { CreateOrganisationDto } from './create-organisation.dto.js';
 import { EntityCouldNotBeCreated } from '../../../shared/error/entity-could-not-be-created.error.js';
 import { OrganisationDo } from '../domain/organisation.do.js';
 import { EntityNotFoundError } from '../../../shared/error/entity-not-found.error.js';
+import { OrganisationsTyp } from '../domain/organisation.enum.js';
+import { FindOrganisationDto } from './find-organisation.dto.js';
+import { OrganisationResponse } from './organisation.response.js';
+import { Paged } from '../../../shared/paging/paged.js';
 
 describe('OrganisationUc', () => {
     let module: TestingModule;
@@ -81,6 +85,53 @@ describe('OrganisationUc', () => {
             await expect(organisationUc.findOrganisationById(organisation.id)).rejects.toThrowError(
                 EntityNotFoundError,
             );
+        });
+    });
+
+    describe('findAll', () => {
+        const findOrganisationDto: FindOrganisationDto = {
+            kennung: 'kennung',
+            name: 'name',
+            typ: OrganisationsTyp.SCHULE,
+            offset: 0,
+            limit: 0,
+        };
+
+        it('should find all organizations that match with query param', async () => {
+            const firstOrganisation: OrganisationDo<true> = DoFactory.createOrganisation(true);
+            const secondOrganisation: OrganisationDo<true> = DoFactory.createOrganisation(true);
+
+            organisationServiceMock.findAllOrganizations.mockResolvedValue({
+                total: 2,
+                offset: 0,
+                limit: 0,
+                items: [firstOrganisation, secondOrganisation],
+            });
+
+            const result: Paged<OrganisationResponse> = await organisationUc.findAll(findOrganisationDto);
+
+            expect(result.total).toBe(2);
+            expect(result.items).toHaveLength(2);
+            expect(result.items[0]?.name).toEqual(firstOrganisation.name);
+            expect(result.items[1]?.name).toEqual(secondOrganisation.name);
+            expect(result.items[0]?.kennung).toEqual(firstOrganisation.kennung);
+            expect(result.items[1]?.kennung).toEqual(secondOrganisation.kennung);
+            expect(result.items[0]?.typ).toEqual(firstOrganisation.typ);
+            expect(result.items[1]?.typ).toEqual(secondOrganisation.typ);
+        });
+
+        it('should return an empty array when no matching organizations are found', async () => {
+            organisationServiceMock.findAllOrganizations.mockResolvedValue({
+                total: 0,
+                offset: 0,
+                limit: 0,
+                items: [],
+            });
+
+            const emptyResult: Paged<OrganisationResponse> = await organisationUc.findAll(findOrganisationDto);
+
+            expect(emptyResult.total).toBe(0);
+            expect(emptyResult.items).toHaveLength(0);
         });
     });
 });
