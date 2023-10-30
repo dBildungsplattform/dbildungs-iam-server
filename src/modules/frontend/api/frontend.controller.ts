5f3704d2ba40e8060a3956941ebb2d8e30c78d35
Marvin Rode <marvin.rode@capgemini.com>
2023-10-30 16:15:42 +0100

    Implement logout

--- src/modules/frontend/api/frontend.controller.ts
+++ src/modules/frontend/api/frontend.controller.ts
@@ -1,4 +1,14 @@
-import { Controller, Get, Post, Req, Res, Session, UseGuards } from '@nestjs/common';
+import {
+    Controller,
+    Get,
+    Inject,
+    InternalServerErrorException,
+    Query,
+    Req,
+    Res,
+    Session,
+    UseGuards,
+} from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import {
     ApiAcceptedResponse,
@@ -11,17 +21,18 @@ import {
 } from '@nestjs/swagger';
 import { Request, Response } from 'express';
 import { SessionData } from 'express-session';
-import { UserinfoResponse } from 'openid-client';
+import { Client, UserinfoResponse } from 'openid-client';
 
 import { FrontendConfig, ServerConfig } from '../../../shared/config/index.js';
-import { AuthenticatedGuard, CurrentUser, LoginGuard, User } from '../auth/index.js';
+import { AuthenticatedGuard, CurrentUser, LoginGuard, OIDC_CLIENT, User } from '../auth/index.js';
+import { RedirectQueryParams } from './redirect.query.params.js';
 
 @ApiTags('frontend')
 @Controller({ path: 'frontend' })
 export class FrontendController {
     private defaultRedirect: string;
 
-    public constructor(configService: ConfigService<ServerConfig>) {
+    public constructor(configService: ConfigService<ServerConfig>, @Inject(OIDC_CLIENT) private client: Client) {
         this.defaultRedirect = configService.getOrThrow<FrontendConfig>('FRONTEND').DEFAULT_AUTH_REDIRECT;
     }
 
@@ -29,23 +40,44 @@ export class FrontendController {
     @Get('login')
     @ApiOperation({ summary: 'Used to start OIDC authentication.' })
     @ApiResponse({ status: 302, description: 'Redirection to orchestrate OIDC flow.' })
-    @ApiQuery({
-        name: 'redirectUrl',
-        required: false,
-        type: 'string',
-        description: 'User will be redirected here after login',
-    })
+    @ApiQuery({ type: RedirectQueryParams })
     public login(@Res() res: Response, @Session() session: SessionData): void {
         const target: string = session.redirectUrl ?? this.defaultRedirect;
         session.redirectUrl = undefined;
         res.redirect(target);
     }
 
-    @UseGuards(AuthenticatedGuard)
-    @Post('logout')
+    @Get('logout')
     @ApiAcceptedResponse({ description: 'The person was successfully logged out.' })
-    public logout(@Req() req: Request): void {
-        req.session.destroy(() => {});
+    public logout(@Req() req: Request, @Res() res: Response, @Query() query: RedirectQueryParams): void {
+        const user: User | undefined = req.user as User | undefined;
+        const idToken: string | undefined = user?.id_token;
+
+        req.logout((logoutErr?: Error) => {
+            if (logoutErr) {
+                throw new InternalServerErrorException('Could not log out', { cause: logoutErr });
+            }
+
+            req.session.destroy((destroyErr?: Error) => {
+                if (destroyErr) {
+                    throw new InternalServerErrorException('Could not destroy session', { cause: destroyErr });
+                }
+
+                const redirectUrl: string = query.redirectUrl ?? this.defaultRedirect;
+
+                if (idToken && this.client.issuer.metadata.end_session_endpoint) {
+                    const endSessionUrl: string = this.client.endSessionUrl({
+                        id_token_hint: idToken,
+                        post_logout_redirect_uri: redirectUrl,
+                        client_id: this.client.metadata.client_id,
+                    });
+
+                    res.redirect(endSessionUrl);
+                } else {
+                    res.redirect(redirectUrl);
+                }
+            });
+        });
     }
 
     @Get('logininfo')
