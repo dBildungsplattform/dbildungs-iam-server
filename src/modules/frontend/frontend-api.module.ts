a7e0aebe3c16b6812773e95a772806cd6234fb6a
Alexander Weber <alexander.weber@capgemini.com>
2023-11-03 09:16:51 +0100

    updated package lock

--- src/modules/frontend/frontend-api.module.ts
+++ src/modules/frontend/frontend-api.module.ts
@@ -8,7 +8,11 @@ import { BackendHttpService } from './outbound/backend-http.service.js';
 import { LoggerModule } from '../../core/logging/logger.module.js';
 
 @Module({
-    imports: [HttpModule, LoggerModule.register(FrontendApiModule.name), PassportModule.register({ session: true, defaultStrategy: 'oidc', keepSessionInfo: true })],
+    imports: [
+        HttpModule,
+        LoggerModule.register(FrontendApiModule.name),
+        PassportModule.register({ session: true, defaultStrategy: 'oidc', keepSessionInfo: true }),
+    ],
     providers: [AuthenticatedGuard, BackendHttpService, OpenIdConnectStrategy, SessionSerializer, OIDCClientProvider],
     controllers: [FrontendController],
 })
