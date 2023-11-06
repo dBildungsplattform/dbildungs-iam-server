819388a29f400ae3f385fc93469c09fec84e3346
Marvin Rode (Cap) <127723478+marode-cap@users.noreply.github.com>
2023-11-06 14:19:20 +0100

    SPSH-112 BFF Logout process (#62)
    * Add redis to compose.yaml
    
    * Implement logic
    
    * Add tests
    
    * Mock openid-client everywhere
    
    * Use fake-real OpenID Client instead of mock
    
    * Update docs
    
    * Make redirect configurable and update realm
    
    * Update readme
    
    * Add ability to send redirect with login request
    
    * Implement logout
    
    * Add tests
    
    * Fix tests
    
    * Remove error from logout
    
    * Add query to API docs
    
    * Remove custom redirect from logout
    
    ---------
    
    Co-authored-by: Kristoff Kiefer <31993948+kristoff-kiefer@users.noreply.github.com>

--- src/modules/frontend/api/frontend.controller.ts
+++ src/modules/frontend/api/frontend.controller.ts
@@ -1,8 +1,8 @@
-import { Controller, Get, Post, Req, Res, Session, UseGuards } from '@nestjs/common';
+import { Controller, Get, Inject, Logger, Post, Req, Res, Session, UseGuards } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import {
-    ApiAcceptedResponse,
     ApiForbiddenResponse,
+    ApiInternalServerErrorResponse,
     ApiOkResponse,
     ApiOperation,
     ApiQuery,
@@ -11,41 +11,71 @@ import {
 } from '@nestjs/swagger';
 import { Request, Response } from 'express';
 import { SessionData } from 'express-session';
-import { UserinfoResponse } from 'openid-client';
+import { Client, UserinfoResponse } from 'openid-client';
 
 import { FrontendConfig, ServerConfig } from '../../../shared/config/index.js';
-import { AuthenticatedGuard, CurrentUser, LoginGuard, User } from '../auth/index.js';
+import { AuthenticatedGuard, CurrentUser, LoginGuard, OIDC_CLIENT, User } from '../auth/index.js';
+import { RedirectQueryParams } from './redirect.query.params.js';
 
 @ApiTags('frontend')
 @Controller({ path: 'frontend' })
 export class FrontendController {
-    private defaultRedirect: string;
+    private readonly logger: Logger = new Logger(FrontendController.name);
 
-    public constructor(configService: ConfigService<ServerConfig>) {
-        this.defaultRedirect = configService.getOrThrow<FrontendConfig>('FRONTEND').DEFAULT_AUTH_REDIRECT;
+    private readonly defaultLoginRedirect: string;
+
+    private readonly logoutRedirect: string;
+
+    public constructor(configService: ConfigService<ServerConfig>, @Inject(OIDC_CLIENT) private client: Client) {
+        const frontendConfig: FrontendConfig = configService.getOrThrow<FrontendConfig>('FRONTEND');
+        this.defaultLoginRedirect = frontendConfig.DEFAULT_LOGIN_REDIRECT;
+        this.logoutRedirect = frontendConfig.LOGOUT_REDIRECT;
     }
 
     @UseGuards(LoginGuard)
     @Get('login')
     @ApiOperation({ summary: 'Used to start OIDC authentication.' })
     @ApiResponse({ status: 302, description: 'Redirection to orchestrate OIDC flow.' })
-    @ApiQuery({
-        name: 'redirectUrl',
-        required: false,
-        type: 'string',
-        description: 'User will be redirected here after login',
-    })
+    @ApiQuery({ type: RedirectQueryParams })
     public login(@Res() res: Response, @Session() session: SessionData): void {
-        const target: string = session.redirectUrl ?? this.defaultRedirect;
+        const target: string = session.redirectUrl ?? this.defaultLoginRedirect;
         session.redirectUrl = undefined;
         res.redirect(target);
     }
 
-    @UseGuards(AuthenticatedGuard)
     @Post('logout')
-    @ApiAcceptedResponse({ description: 'The person was successfully logged out.' })
-    public logout(@Req() req: Request): void {
-        req.session.destroy(() => {});
+    @ApiOperation({ summary: 'Used to log out the current user.' })
+    @ApiResponse({ status: 302, description: 'Redirect to logout.' })
+    @ApiInternalServerErrorResponse({ description: 'Internal server error while trying to log out.' })
+    public logout(@Req() req: Request, @Res() res: Response): void {
+        const user: User | undefined = req.user as User | undefined;
+        const idToken: string | undefined = user?.id_token;
+
+        req.logout((logoutErr?: Error) => {
+            if (logoutErr) {
+                // Error should not stop logout process
+                this.logger.log('An error occurres while trying to log out', logoutErr);
+            }
+
+            req.session.destroy((destroyErr?: Error) => {
+                if (destroyErr) {
+                    // Error should not stop logout process
+                    this.logger.log('An error occurred while trying to destroy the session', destroyErr);
+                }
+
+                if (this.client.issuer.metadata.end_session_endpoint) {
+                    const endSessionUrl: string = this.client.endSessionUrl({
+                        id_token_hint: idToken,
+                        post_logout_redirect_uri: this.logoutRedirect,
+                        client_id: this.client.metadata.client_id,
+                    });
+
+                    res.redirect(endSessionUrl);
+                } else {
+                    res.redirect(this.logoutRedirect);
+                }
+            });
+        });
     }
 
     @Get('logininfo')
