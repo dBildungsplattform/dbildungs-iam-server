cd6f67cb95013ca679b24f89794a13a5f2ef468b
Kristoff Kiefer <31993948+kristoff-kiefer@users.noreply.github.com>
2023-11-20 13:05:45 +0100

    Spsh 130 (#52)
    * Health check for Keycloak
    
    * Refactor LoginService to get the client externally
    
    * Export LoginService for external use
    
    * Prettify LoginService
    
    * Add KeycloakHealthCheck to Health endpoint
    
    * Fixed linter problems
    
    * Update src/modules/health/keycloak.health-indicator.ts
    
    Co-authored-by: WahlMartin <132356096+WahlMartin@users.noreply.github.com>
    
    * Rename HealthIndicator
    
    * Fixed vulnerability
    
    ---------
    
    Co-authored-by: WahlMartin <132356096+WahlMartin@users.noreply.github.com>

--- src/modules/health/health.module.ts
+++ src/modules/health/health.module.ts
@@ -2,6 +2,11 @@ import { Module } from '@nestjs/common';
 import { TerminusModule } from '@nestjs/terminus';
 import { HealthController } from './health.controller.js';
 import { HttpModule } from '@nestjs/axios';
+import { KeycloakHealthIndicator } from './keycloak.health-indicator.js';
 
-@Module({ imports: [TerminusModule, HttpModule], controllers: [HealthController] })
+@Module({
+    imports: [TerminusModule, HttpModule],
+    providers: [KeycloakHealthIndicator],
+    controllers: [HealthController],
+})
 export class HealthModule {}
