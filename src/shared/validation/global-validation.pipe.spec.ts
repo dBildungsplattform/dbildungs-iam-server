8769bab3ab6d1762a536e71a85b39c4c90407aae
Firas Shmit <firas.shmit@capgemini.com>
2023-11-21 11:03:20 +0100

    EW-644 deleted integration test & modfied unit test

--- src/shared/validation/global-validation.pipe.spec.ts
+++ src/shared/validation/global-validation.pipe.spec.ts
@@ -1,9 +1,51 @@
+import { Test, TestingModule } from '@nestjs/testing';
 import { GlobalValidationPipe } from './global-validation.pipe.js';
+import { DetailedValidationError } from './detailed-validation.error.js';
+import { ValidationError } from 'class-validator';
 
 describe('GlobalValidationPipe', () => {
-    describe('constructor', () => {
-        it('should instantiate type', () => {
-            expect(new GlobalValidationPipe()).toBeInstanceOf(GlobalValidationPipe);
-        });
+    let module: TestingModule;
+    let sut: GlobalValidationPipe;
+
+    beforeAll(async () => {
+        module = await Test.createTestingModule({
+            providers: [GlobalValidationPipe],
+        }).compile();
+        sut = module.get(GlobalValidationPipe);
+    });
+
+    afterAll(async () => {
+        await module.close();
+    });
+
+    it('should be defined', () => {
+        expect(sut).toBeDefined();
+    });
+
+    it('should be instance of ValidationPipe', async () => {
+        const validationError: ValidationError = {
+            property: '',
+        };
+        await sut
+            .transform(validationError, {
+                type: 'body',
+                metatype: DetailedValidationError,
+            })
+            .catch((error: ValidationError) => {
+                expect(error).toBeInstanceOf(DetailedValidationError);
+            });
+    });
+
+    it('should throw DetailedValidation error on failure', async () => {
+        const validationError: ValidationError = {
+            property: '',
+        };
+
+        await expect(
+            sut.transform(validationError, {
+                type: 'body',
+                metatype: DetailedValidationError,
+            }),
+        ).rejects.toThrow(DetailedValidationError);
     });
 });
