79b4793e9fd7e4b7e651e1b1a9b3dfab3e2d1737
psachmann <patrick.sachmann@capgemini.com>
2023-11-22 10:40:20 +0100

    EW-646-logger-module updating class-logger.spec.ts

--- src/core/logging/class-logger.spec.ts
+++ src/core/logging/class-logger.spec.ts
@@ -1,97 +1,167 @@
-import { ClassLogger } from './class-logger.js';
+import { DeepMocked, createMock } from '@golevelup/ts-jest';
 import { Test, TestingModule } from '@nestjs/testing';
+import { Logger } from 'winston';
+import { ConfigTestModule } from '../../../test/utils/config-test.module.js';
+import { ClassLogger } from './class-logger.js';
+import { LoggerModule } from './logger.module.js';
 import { ModuleLogger } from './module-logger.js';
-import { createMock } from '@golevelup/ts-jest';
 
 describe('ClassLogger', () => {
-    let loggerModule: TestingModule;
+    let module: TestingModule;
+    let sut: ClassLogger;
 
-    let moduleLogger: ModuleLogger;
+    const loggerMock: DeepMocked<Logger> = createMock<Logger>();
+    const testModuleName: string = 'TestModule';
+    const createTestMessage: (message: string, trace?: unknown) => Record<string, unknown> = (
+        message: string,
+        trace?: unknown,
+    ) => ({
+        context: `${testModuleName}.${ClassLogger.name}`,
+        message,
+        trace,
+    });
 
-    beforeEach(async () => {
-        moduleLogger = createMock<ModuleLogger>();
-        loggerModule = await Test.createTestingModule({
-            providers: [ClassLogger, { provide: ModuleLogger, useValue: moduleLogger }],
-        }).compile();
+    beforeAll(async () => {
+        module = await Test.createTestingModule({
+            imports: [LoggerModule.register(testModuleName), ConfigTestModule],
+        })
+            .overrideProvider(ModuleLogger)
+            .useValue(
+                createMock<ModuleLogger>({
+                    moduleName: testModuleName,
+                    getLogger: () => loggerMock,
+                }),
+            )
+            .compile();
+        sut = await module.resolve(ClassLogger);
     });
 
     afterEach(async () => {
-        await loggerModule.close();
+        await module.close();
+    });
+
+    beforeEach(() => {
+        jest.clearAllMocks();
+    });
+
+    it('should be defined', () => {
+        expect(sut).toBeDefined();
     });
 
     describe('when a message is logged', () => {
-        it('should log appropriately for level debug', async () => {
-            const classLogger: ClassLogger = await loggerModule.resolve(ClassLogger);
+        it('should log appropriately for level debug', () => {
+            sut.debug('Blah');
 
-            classLogger.debug('Blah');
-
-            expect(moduleLogger.getLogger().debug).toHaveBeenCalledTimes(1);
+            expect(loggerMock.debug).toHaveBeenCalledTimes(1);
+            expect(loggerMock.debug).toHaveBeenCalledWith(createTestMessage('Blah'));
         });
 
-        it('should log appropriately for level notice', async () => {
-            const classLogger: ClassLogger = await loggerModule.resolve(ClassLogger);
+        it('should log appropriately for level notice', () => {
+            sut.notice('Blah');
 
-            classLogger.notice('Blah');
-
-            expect(moduleLogger.getLogger().notice).toHaveBeenCalledTimes(1);
+            expect(loggerMock.notice).toHaveBeenCalledTimes(1);
+            expect(loggerMock.notice).toHaveBeenCalledWith(createTestMessage('Blah'));
         });
 
-        it('should log appropriately for level info', async () => {
-            const classLogger: ClassLogger = await loggerModule.resolve(ClassLogger);
+        it('should log appropriately for level info', () => {
+            sut.info('Blah');
 
-            classLogger.info('Blah');
-
-            expect(moduleLogger.getLogger().info).toHaveBeenCalledTimes(1);
+            expect(loggerMock.info).toHaveBeenCalledTimes(1);
+            expect(loggerMock.info).toHaveBeenCalledWith(createTestMessage('Blah'));
         });
 
-        it('should log appropriately for level warning', async () => {
-            const classLogger: ClassLogger = await loggerModule.resolve(ClassLogger);
+        it('should log appropriately for level warning', () => {
+            sut.warning('Blah');
 
-            classLogger.warning('Blah');
-
-            expect(moduleLogger.getLogger().warning).toHaveBeenCalledTimes(1);
+            expect(loggerMock.warning).toHaveBeenCalledTimes(1);
+            expect(loggerMock.warning).toHaveBeenCalledWith(createTestMessage('Blah'));
         });
 
-        it('should log appropriately for level alert', async () => {
-            const classLogger: ClassLogger = await loggerModule.resolve(ClassLogger);
+        it('should log appropriately for level alert', () => {
+            sut.alert('Blah');
 
-            classLogger.alert('Blah');
-
-            expect(moduleLogger.getLogger().alert).toHaveBeenCalledTimes(1);
+            expect(loggerMock.alert).toHaveBeenCalledTimes(1);
+            expect(loggerMock.alert).toHaveBeenCalledWith(createTestMessage('Blah'));
         });
 
-        it('should log appropriately for level error', async () => {
-            const classLogger: ClassLogger = await loggerModule.resolve(ClassLogger);
+        it('should log appropriately for level error', () => {
+            sut.error('Blah');
 
-            classLogger.error('Blah');
-
-            expect(moduleLogger.getLogger().error).toHaveBeenCalledTimes(1);
+            expect(loggerMock.error).toHaveBeenCalledTimes(1);
+            expect(loggerMock.error).toHaveBeenCalledWith(createTestMessage('Blah'));
         });
 
-        it('should log appropriately for level crit', async () => {
-            const classLogger: ClassLogger = await loggerModule.resolve(ClassLogger);
+        it('should log appropriately for level crit', () => {
+            sut.crit('Blah');
 
-            classLogger.crit('Blah');
-
-            expect(moduleLogger.getLogger().crit).toHaveBeenCalledTimes(1);
+            expect(loggerMock.crit).toHaveBeenCalledTimes(1);
+            expect(loggerMock.crit).toHaveBeenCalledWith(createTestMessage('Blah'));
         });
 
-        it('should log appropriately for level emerg', async () => {
-            const classLogger: ClassLogger = await loggerModule.resolve(ClassLogger);
+        it('should log appropriately for level emerg', () => {
+            sut.emerg('Blah');
 
-            classLogger.emerg('Blah');
-
-            expect(moduleLogger.getLogger().emerg).toHaveBeenCalledTimes(1);
+            expect(loggerMock.emerg).toHaveBeenCalledTimes(1);
+            expect(loggerMock.emerg).toHaveBeenCalledWith(createTestMessage('Blah'));
         });
     });
 
     describe('when a message with a trace was logged', () => {
-        it('should take the trace into account', async () => {
-            const classLogger: ClassLogger = await loggerModule.resolve(ClassLogger);
+        it('should take the trace into account for level debug', () => {
+            sut.debug('Blah2', 'TraceInfo');
 
-            classLogger.debug('Blah2', 'TraceInfo');
+            expect(loggerMock.debug).toHaveBeenCalledTimes(1);
+            expect(loggerMock.debug).toHaveBeenCalledWith(createTestMessage('Blah2', 'TraceInfo'));
+        });
 
-            expect(moduleLogger.getLogger().debug).toHaveBeenCalledTimes(1);
+        // AI next 40 lines
+        it('should take the trace into account for level notice', () => {
+            sut.notice('Blah2', 'TraceInfo');
+
+            expect(loggerMock.notice).toHaveBeenCalledTimes(1);
+            expect(loggerMock.notice).toHaveBeenCalledWith(createTestMessage('Blah2', 'TraceInfo'));
+        });
+
+        it('should take the trace into account for level info', () => {
+            sut.info('Blah2', 'TraceInfo');
+
+            expect(loggerMock.info).toHaveBeenCalledTimes(1);
+            expect(loggerMock.info).toHaveBeenCalledWith(createTestMessage('Blah2', 'TraceInfo'));
+        });
+
+        it('should take the trace into account for level warning', () => {
+            sut.warning('Blah2', 'TraceInfo');
+
+            expect(loggerMock.warning).toHaveBeenCalledTimes(1);
+            expect(loggerMock.warning).toHaveBeenCalledWith(createTestMessage('Blah2', 'TraceInfo'));
+        });
+
+        it('should take the trace into account for level alert', () => {
+            sut.alert('Blah2', 'TraceInfo');
+
+            expect(loggerMock.alert).toHaveBeenCalledTimes(1);
+            expect(loggerMock.alert).toHaveBeenCalledWith(createTestMessage('Blah2', 'TraceInfo'));
+        });
+
+        it('should take the trace into account for level error', () => {
+            sut.error('Blah2', 'TraceInfo');
+
+            expect(loggerMock.error).toHaveBeenCalledTimes(1);
+            expect(loggerMock.error).toHaveBeenCalledWith(createTestMessage('Blah2', 'TraceInfo'));
+        });
+
+        it('should take the trace into account for level crit', () => {
+            sut.crit('Blah2', 'TraceInfo');
+
+            expect(loggerMock.crit).toHaveBeenCalledTimes(1);
+            expect(loggerMock.crit).toHaveBeenCalledWith(createTestMessage('Blah2', 'TraceInfo'));
+        });
+
+        it('should take the trace into account for level emerg', () => {
+            sut.emerg('Blah2', 'TraceInfo');
+
+            expect(loggerMock.emerg).toHaveBeenCalledTimes(1);
+            expect(loggerMock.emerg).toHaveBeenCalledWith(createTestMessage('Blah2', 'TraceInfo'));
         });
     });
 });
