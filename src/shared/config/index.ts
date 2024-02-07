6084013604105eb94094028d3a2b3711ad152182
Marvin Rode (Cap) <127723478+marode-cap@users.noreply.github.com>
2024-02-07 15:23:07 +0100

    SPSH-144 Add hierarchical structure to organisation (#96)
    Add hierarchies between organizations

--- src/shared/config/index.ts
+++ src/shared/config/index.ts
@@ -2,6 +2,7 @@ export * from './config.loader.js';
 export * from './db.config.js';
 export * from './frontend.config.js';
 export * from './host.config.js';
+export * from './data.config.js';
 export * from './json.config.js';
 export * from './keycloak.config.js';
 export * from './redis.config.js';
