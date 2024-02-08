76e5cf780bc46b37069ed30089b49244e3930eee
Caspar Neumann <caspar.neumann@capgemini.com>
2024-02-08 15:41:36 +0100

    Change Person-Kontext Module & Add Tests

--- src/modules/person-kontext/person-kontext.module.ts
+++ src/modules/person-kontext/person-kontext.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { LoggerModule } from '../../core/logging/logger.module.js';
 import { PersonenkontextRepo } from '../person-kontext/persistence/personenkontext.repo.js';
 import { PersonenkontextService } from '../person-kontext/domain/personenkontext.service.js';
+import { PersonModule } from '../person/person.module.js';
+import { PersonRepo } from '../person/persistence/person.repo.js';
 
 @Module({
-    imports: [LoggerModule.register(PersonKontextModule.name)],
-    providers: [PersonenkontextRepo, PersonenkontextService],
-    exports: [PersonenkontextService],
+    imports: [PersonModule, LoggerModule.register(PersonKontextModule.name)],
+    providers: [PersonenkontextRepo, PersonenkontextService, PersonRepo],
+    exports: [PersonenkontextService, PersonenkontextRepo],
 })
 export class PersonKontextModule {}
