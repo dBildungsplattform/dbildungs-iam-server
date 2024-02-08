75f75f5d8c7234e21e5c7c3ab984e184dce022e0
Caspar Neumann <caspar.neumann@capgemini.com>
2024-02-08 15:50:00 +0100

    Change Person-Kontext-Api Module & Add Tests

---
+++ src/modules/person-kontext/person-kontext-api.module.spec.ts
@@ -0,0 +1,59 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import {
+    ConfigTestModule,
+    DatabaseTestModule,
+    MapperTestModule,
+    KeycloakConfigTestModule,
+} from '../../../test/utils/index.js';
+import { PersonenkontextUc } from '../person-kontext/api/personenkontext.uc.js';
+import { PersonKontextApiModule } from './person-kontext-api.module.js';
+import { PersonenkontextController } from './api/personenkontext.controller.js';
+import { PersonenkontextService } from './domain/personenkontext.service.js';
+import { PersonenkontextRepo } from './persistence/personenkontext.repo.js';
+import { PersonRepo } from '../person/persistence/person.repo.js';
+
+describe('PersonKontextApiModule', () => {
+    let module: TestingModule;
+
+    beforeAll(async () => {
+        module = await Test.createTestingModule({
+            imports: [
+                ConfigTestModule,
+                DatabaseTestModule.forRoot(),
+                MapperTestModule,
+                PersonKontextApiModule,
+                KeycloakConfigTestModule.forRoot(),
+            ],
+        }).compile();
+    });
+
+    afterAll(async () => {
+        await module.close();
+    });
+
+    it('should be defined', () => {
+        expect(module).toBeDefined();
+    });
+
+    describe('when module is initialized', () => {
+        it('should resolve PersonkontextController', () => {
+            expect(module.get(PersonenkontextController)).toBeInstanceOf(PersonenkontextController);
+        });
+
+        it('should resolve PersonkontextUc', () => {
+            expect(module.get(PersonenkontextUc)).toBeInstanceOf(PersonenkontextUc);
+        });
+
+        it('should resolve PersonkontextUc', () => {
+            expect(module.get(PersonenkontextService)).toBeInstanceOf(PersonenkontextService);
+        });
+
+        it('should resolve PersonkontextUc', () => {
+            expect(module.get(PersonenkontextRepo)).toBeInstanceOf(PersonenkontextRepo);
+        });
+
+        it('should resolve PersonkontextUc', () => {
+            expect(module.get(PersonRepo)).toBeInstanceOf(PersonRepo);
+        });
+    });
+});
--- src/modules/person-kontext/person-kontext-api.module.ts
+++ src/modules/person-kontext/person-kontext-api.module.ts
@@ -2,10 +2,14 @@ import { Module } from '@nestjs/common';
 import { LoggerModule } from '../../core/logging/logger.module.js';
 import { PersonenkontextUc } from '../person-kontext/api/personenkontext.uc.js';
 import { PersonenkontextController } from '../person-kontext/api/personenkontext.controller.js';
+import { PersonModule } from '../person/person.module.js';
+import { PersonenkontextService } from './domain/personenkontext.service.js';
+import { PersonenkontextRepo } from './persistence/personenkontext.repo.js';
+import { PersonRepo } from '../person/persistence/person.repo.js';
 
 @Module({
-    imports: [LoggerModule.register(PersonKontextApiModule.name)],
-    providers: [PersonenkontextUc],
+    imports: [PersonModule, LoggerModule.register(PersonKontextApiModule.name)],
+    providers: [PersonenkontextUc, PersonenkontextService, PersonenkontextRepo, PersonRepo],
     controllers: [PersonenkontextController],
 })
 export class PersonKontextApiModule {}
