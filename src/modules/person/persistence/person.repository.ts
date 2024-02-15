e56059c162454a2d6b170b7f8a16e79685a08368
Caspar Neumann <caspar.neumann@capgemini.com>
2024-02-15 13:23:39 +0100

    Remove person.repository constructor to fix eslint

--- src/modules/person/persistence/person.repository.ts
+++ src/modules/person/persistence/person.repository.ts
@@ -1,7 +1,6 @@
-import { EntityManager } from "@mikro-orm/core";
 import { Injectable } from "@nestjs/common";
 
 @Injectable()
 export class PersonRepository {
-    public constructor(private readonly em: EntityManager) {}
+ //   public constructor(private readonly em: EntityManager) {}
 }
