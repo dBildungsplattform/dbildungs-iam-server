1bab341f8fa7b41c7861dd0ed7129bb5de45554b
Marvin Rode <marvin.rode@capgemini.com>
2024-02-16 13:41:50 +0100

    Fix Test Coverage

---
+++ src/modules/service-provider/api/service-provider.response.spec.ts
@@ -0,0 +1,39 @@
+import { DoFactory } from '../../../../test/utils/index.js';
+import { ServiceProvider } from '../domain/service-provider.js';
+import { ServiceProviderResponse } from './service-provider.response.js';
+
+describe('ServiceProviderResponse', () => {
+    describe('when provider has logo and mime-type', () => {
+        it('should set hasLogo to true', () => {
+            const provider: ServiceProvider<true> = DoFactory.createServiceProvider(true);
+
+            const response: ServiceProviderResponse = new ServiceProviderResponse(provider);
+
+            expect(response.hasLogo).toBe(true);
+        });
+    });
+
+    describe('when provider has no logo', () => {
+        it('should set hasLogo to false', () => {
+            const provider: ServiceProvider<true> = DoFactory.createServiceProvider(true, {
+                logo: undefined,
+            });
+
+            const response: ServiceProviderResponse = new ServiceProviderResponse(provider);
+
+            expect(response.hasLogo).toBe(false);
+        });
+    });
+
+    describe('when provider has no logo mime-type', () => {
+        it('should set hasLogo to false', () => {
+            const provider: ServiceProvider<true> = DoFactory.createServiceProvider(true, {
+                logoMimeType: undefined,
+            });
+
+            const response: ServiceProviderResponse = new ServiceProviderResponse(provider);
+
+            expect(response.hasLogo).toBe(false);
+        });
+    });
+});
