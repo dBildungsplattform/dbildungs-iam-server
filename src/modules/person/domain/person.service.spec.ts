dd16f4337df230e94b0d19c202916270455af5e7
Caspar Neumann <caspar.neumann@capgemini.com>
2024-02-23 09:18:30 +0100

    Remove person.service.sepc unneccessary tests

--- src/modules/person/domain/person.service.spec.ts
+++ src/modules/person/domain/person.service.spec.ts
@@ -1,15 +1,8 @@
 import { Dictionary, Mapper } from '@automapper/core';
 import { getMapperToken } from '@automapper/nestjs';
-import { faker } from '@faker-js/faker';
 import { createMock, DeepMocked } from '@golevelup/ts-jest';
 import { Test, TestingModule } from '@nestjs/testing';
-import {
-    DomainError,
-    EntityCouldNotBeUpdated,
-    EntityNotFoundError,
-    MismatchedRevisionError,
-    PersonAlreadyExistsError,
-} from '../../../shared/error/index.js';
+import { EntityNotFoundError } from '../../../shared/error/index.js';
 import { DoFactory } from '../../../../test/utils/do-factory.js';
 import { PersonRepo } from '../persistence/person.repo.js';
 import { PersonDo } from './person.do.js';
@@ -51,55 +44,6 @@ describe('PersonService', () => {
 
     it('should be defined', () => {
         expect(personService).toBeDefined();
-    });
-
-    describe('createPerson', () => {
-        describe('when person does not exist', () => {
-            it('should create person', async () => {
-                const person: PersonDo<true> = DoFactory.createPerson(true);
-                personRepoMock.findByReferrer.mockResolvedValue(null);
-                personRepoMock.save.mockResolvedValue(person);
-                mapperMock.map.mockReturnValue(person as unknown as Dictionary<unknown>);
-                const result: Result<PersonDo<true>> = await personService.createPerson({
-                    keycloakUserId: person.keycloakUserId,
-                    vorname: person.vorname,
-                    familienname: person.familienname,
-                    mandant: faker.string.uuid(),
-                    id: undefined,
-                    createdAt: undefined,
-                    updatedAt: undefined,
-                    revision: '1',
-                });
-                expect(result).toEqual<Result<PersonDo<true>>>({
-                    ok: true,
-                    value: person,
-                });
-            });
-        });
-
-        describe('when person already exists', () => {
-            it('should return domain error', async () => {
-                const person: PersonDo<true> = DoFactory.createPerson(true, { referrer: faker.string.uuid() });
-                personRepoMock.findByReferrer.mockResolvedValue(person);
-                const result: Result<PersonDo<true>> = await personService.createPerson({
-                    keycloakUserId: person.keycloakUserId,
-                    vorname: person.vorname,
-                    familienname: person.familienname,
-                    mandant: person.mandant,
-                    referrer: person.referrer as string,
-                    id: undefined,
-                    createdAt: undefined,
-                    updatedAt: undefined,
-                    revision: '1',
-                });
-                expect(result).toEqual<Result<PersonDo<true>>>({
-                    ok: false,
-                    error: new PersonAlreadyExistsError(
-                        `Person with referrer ${person.referrer as string} already exists`,
-                    ),
-                });
-            });
-        });
     });
 
     describe('findPersonById', () => {
@@ -155,81 +99,6 @@ describe('PersonService', () => {
 
             expect(result.items).toBeInstanceOf(Array);
             expect(result.items).toHaveLength(0);
-        });
-    });
-
-    describe('updatePerson', () => {
-        describe('when person is updated successfully', () => {
-            it('should return updated person', async () => {
-                const personDo: PersonDo<true> = DoFactory.createPerson(true, {
-                    revision: '1',
-                });
-                const personDoWithUpdatedRevision: PersonDo<true> = Object.assign({}, personDo, {
-                    revision: '2',
-                });
-
-                personRepoMock.findById.mockResolvedValue(personDo);
-                personRepoMock.save.mockResolvedValue(personDoWithUpdatedRevision);
-
-                const result: Result<PersonDo<true>, DomainError> = await personService.updatePerson(personDo);
-
-                expect(result).toEqual<Result<PersonDo<true>, DomainError>>({
-                    ok: true,
-                    value: personDoWithUpdatedRevision,
-                });
-                expect(personRepoMock.save).toHaveBeenCalledWith(personDoWithUpdatedRevision);
-            });
-        });
-
-        describe('when entity is not found', () => {
-            it('should return EntityNotFoundError', async () => {
-                const response: Result<PersonDo<true>, DomainError> = await personService.updatePerson(
-                    {} as PersonDo<true>,
-                );
-
-                expect(response).toEqual<Result<PersonDo<true>, DomainError>>({
-                    ok: false,
-                    error: new EntityNotFoundError('Person'),
-                });
-            });
-        });
-
-        describe('when revision does not match', () => {
-            it('should return MismatchedRevisionError', async () => {
-                const personDo: PersonDo<true> = DoFactory.createPerson(true);
-                const personDoWithWrongRevision: PersonDo<true> = {
-                    ...personDo,
-                    revision: 'wrongRevision',
-                };
-
-                personRepoMock.findById.mockResolvedValue(personDo);
-
-                const result: Result<PersonDo<true>, DomainError> = await personService.updatePerson(
-                    personDoWithWrongRevision,
-                );
-
-                expect(result).toEqual<Result<PersonDo<true>, DomainError>>({
-                    ok: false,
-                    error: new MismatchedRevisionError(
-                        `Revision ${personDoWithWrongRevision.revision} does not match revision ${personDo.revision} of stored person.`,
-                    ),
-                });
-            });
-        });
-
-        describe('when could not be stored', () => {
-            it('should return EntityCouldNotBeUpdatedError', async () => {
-                const personDo: PersonDo<true> = DoFactory.createPerson(true);
-
-                personRepoMock.findById.mockResolvedValue(personDo);
-
-                const result: Result<PersonDo<true>, DomainError> = await personService.updatePerson(personDo);
-
-                expect(result).toEqual<Result<PersonDo<true>, DomainError>>({
-                    ok: false,
-                    error: new EntityCouldNotBeUpdated('Person', personDo.id),
-                });
-            });
         });
     });
 });
