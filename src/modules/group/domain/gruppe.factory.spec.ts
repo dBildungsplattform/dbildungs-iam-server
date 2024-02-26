5c356b28a552b67620dc4f0ebc17b2e72e464ae8
Firas Shmit <firas.shmit@capgemini.com>
2024-02-26 22:42:41 +0100

    EW-656 implemented test for groups factory

---
+++ src/modules/group/domain/gruppe.factory.spec.ts
@@ -0,0 +1,78 @@
+import { Gruppe } from './gruppe.js';
+import { GruppenFactory } from './gruppe.factory.js';
+import { CreateGroupBodyParams } from '../api/create-group.body.params.js';
+import { faker } from '@faker-js/faker';
+import {
+    Bildungsziele,
+    Faecherkanon,
+    GruppenTyp,
+    Gruppenbereich,
+    Gruppendifferenzierung,
+    Gruppenrollen,
+} from './gruppe.enums.js';
+import { Referenzgruppen } from './referenzgruppen.js';
+import { Jahrgangsstufe } from '../../personenkontext/domain/personenkontext.enums.js';
+
+describe('createGroup', () => {
+    let factory: GruppenFactory;
+    const createGroupBodyParams: CreateGroupBodyParams = {
+        bezeichnung: faker.lorem.word(),
+        typ: GruppenTyp.KLASSE,
+        bereich: Gruppenbereich.PFLICHT,
+        differenzierung: Gruppendifferenzierung.E,
+        bildungsziele: [Bildungsziele.GS, Bildungsziele.HS],
+        jahrgangsstufen: [Jahrgangsstufe.JAHRGANGSSTUFE_1, Jahrgangsstufe.JAHRGANGSSTUFE_2],
+        faecher: [Faecherkanon.DE],
+        referenzgruppen: [
+            new Referenzgruppen({
+                id: faker.string.uuid(),
+                rollen: [Gruppenrollen.LEHR],
+            }),
+        ],
+        laufzeit: {
+            von: new Date(),
+            bis: new Date(),
+        },
+    };
+
+    const gruppe: Gruppe<false> = Gruppe.construct(
+        '',
+        '',
+        '',
+        createGroupBodyParams.bezeichnung,
+        createGroupBodyParams.typ,
+        faker.lorem.word(),
+        faker.lorem.word(),
+        faker.lorem.word(),
+        createGroupBodyParams.bereich,
+        createGroupBodyParams.optionen,
+        createGroupBodyParams.differenzierung,
+        createGroupBodyParams.bildungsziele,
+        createGroupBodyParams.jahrgangsstufen,
+        createGroupBodyParams.faecher,
+        createGroupBodyParams.referenzgruppen,
+        createGroupBodyParams.laufzeit,
+    );
+
+    beforeAll(() => {
+        factory = new GruppenFactory();
+    });
+
+    describe('when creating a group', () => {
+        it('should create a group', () => {
+            jest.spyOn(Gruppe, 'createGroup').mockReturnValue(gruppe);
+
+            const result: Gruppe<false> = factory.createGroup(createGroupBodyParams);
+
+            expect(result).toEqual(gruppe);
+            expect(result.getBeschreibung()).toEqual(gruppe.getBeschreibung());
+            expect(result.getBezeichnung()).toEqual(gruppe.getBezeichnung());
+            expect(result.getBereich()).toEqual(gruppe.getBereich());
+            expect(result.getBildungsziele()).toEqual(gruppe.getBildungsziele());
+            expect(result.getDifferenzierung()).toEqual(gruppe.getDifferenzierung());
+            expect(result.getFaecher()).toEqual(gruppe.getFaecher());
+            expect(result.getTyp()).toEqual(gruppe.getTyp());
+            expect(result.getJahrgangsstufen()).toEqual(gruppe.getJahrgangsstufen());
+        });
+    });
+});
