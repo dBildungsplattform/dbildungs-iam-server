0a3aecfc2da8b55ac4b24c293a8f9bde5e2b51a1
DPDS93CT <cornelius.tegtmeyer@dataport.de>
2024-02-26 16:22:23 +0100

    fix issues persisting cascading SystemRecht for Rolle

--- src/modules/rolle/entity/rolle-systemrecht.entity.ts
+++ src/modules/rolle/entity/rolle-systemrecht.entity.ts
@@ -3,12 +3,12 @@ import { RolleEntity } from './rolle.entity.js';
 import { RollenSystemRecht } from '../domain/rolle.enums.js';
 
 @Entity({ tableName: 'rolle_systemrecht' })
-export class RolleSystemRechtEntity extends BaseEntity {
+export class RolleSystemrechtEntity extends BaseEntity {
     @ManyToOne({ primary: true, entity: () => RolleEntity })
     public rolle!: Rel<RolleEntity>;
 
     @Enum({ primary: true, items: () => RollenSystemRecht })
     public systemRecht!: RollenSystemRecht;
 
-    public [PrimaryKeyProp]?: ['rolle', 'systemRecht'];
+    public [PrimaryKeyProp]?: ['rolle', 'systemrecht'];
 }
