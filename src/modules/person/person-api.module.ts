f02491cd16ab7141e36465d858f6c082164c4329
Caspar Neumann <caspar.neumann@capgemini.com>
2024-03-06 13:17:44 +0100

    Remove KeycloakAdministrationModule from Person-Api-Module

--- src/modules/person/person-api.module.ts
+++ src/modules/person/person-api.module.ts
@@ -3,18 +3,12 @@ import { LoggerModule } from '../../core/logging/logger.module.js';
 import { PersonApiMapperProfile } from './api/person-api.mapper.profile.js';
 import { PersonController } from './api/person.controller.js';
 import { PersonModule } from './person.module.js';
-import { KeycloakAdministrationModule } from '../keycloak-administration/keycloak-administration.module.js';
 import { PersonFrontendController } from './api/person.frontend.controller.js';
 import { PersonenkontextUc } from '../personenkontext/api/personenkontext.uc.js';
 import { PersonenKontextModule } from '../personenkontext/personenkontext.module.js';
 
 @Module({
-    imports: [
-        PersonModule,
-        PersonenKontextModule,
-        KeycloakAdministrationModule,
-        LoggerModule.register(PersonApiModule.name),
-    ],
+    imports: [PersonModule, PersonenKontextModule, LoggerModule.register(PersonApiModule.name)],
     providers: [PersonApiMapperProfile, PersonenkontextUc],
     controllers: [PersonController, PersonFrontendController],
 })
