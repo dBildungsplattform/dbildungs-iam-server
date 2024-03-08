e527a558d89681b6c5d5d2b4d96e4a1d80a908d1
Caspar Neumann <caspar.neumann@capgemini.com>
2024-03-08 11:19:01 +0100

    Add Tests Aggregate attachServiceProvider()

--- src/modules/rolle/domain/rolle.spec.ts
+++ src/modules/rolle/domain/rolle.spec.ts
@@ -1,8 +1,47 @@
+import { faker } from '@faker-js/faker';
 import { DoFactory } from '../../../../test/utils/index.js';
-import { RollenMerkmal } from './rolle.enums.js';
+import { RollenArt, RollenMerkmal } from './rolle.enums.js';
 import { Rolle } from './rolle.js';
+import { Test, TestingModule } from '@nestjs/testing';
+import { ServiceProviderRepo } from '../../service-provider/repo/service-provider.repo.js';
+import { RolleRepo } from '../repo/rolle.repo.js';
+import { DeepMocked, createMock } from '@golevelup/ts-jest';
+import { DomainError } from '../../../shared/error/domain.error.js';
+import { ServiceProvider } from '../../service-provider/domain/service-provider.js';
+import { RolleFactory } from './rolle.factory.js';
 
 describe('Rolle Aggregate', () => {
+    let module: TestingModule;
+    let rolleFactory: RolleFactory;
+    let serviceProviderRepo: DeepMocked<ServiceProviderRepo>;
+    //    let rolleRepo: DeepMocked<RolleRepo>;
+
+    beforeAll(async () => {
+        module = await Test.createTestingModule({
+            providers: [
+                RolleFactory,
+                {
+                    provide: ServiceProviderRepo,
+                    useValue: createMock<ServiceProviderRepo>(),
+                },
+                {
+                    provide: RolleRepo,
+                    useValue: createMock<RolleRepo>(),
+                },
+            ],
+        }).compile();
+        rolleFactory = module.get(RolleFactory);
+        serviceProviderRepo = module.get(ServiceProviderRepo);
+        //        rolleRepo = module.get(RolleRepo);
+    });
+
+    afterAll(async () => {
+        await module.close();
+    });
+
+    afterEach(() => {
+        jest.resetAllMocks();
+    });
     describe('addMerkmal', () => {
         it('should add merkmal if it does not exist', () => {
             const savedRolle: Rolle<true> = DoFactory.createRolle(true, { merkmale: [] });
@@ -42,6 +81,83 @@ describe('Rolle Aggregate', () => {
             savedRolle.removeMerkmal(RollenMerkmal.KOPERS_PFLICHT);
 
             expect(savedRolle.merkmale).toEqual([RollenMerkmal.BEFRISTUNG_PFLICHT]);
+        });
+    });
+
+    describe('attachServiceProvider', () => {
+        describe('when successfull', () => {
+            it('should ádd serviceProviderId to rolle field', async () => {
+                const serviceProviderIdToAttach: string = faker.string.uuid();
+                const rolle: Rolle<true> = rolleFactory.construct(
+                    faker.string.uuid(),
+                    faker.date.anytime(),
+                    faker.date.anytime(),
+                    '',
+                    '',
+                    RollenArt.LEHR,
+                    [],
+                    [],
+                );
+                const serviceProvider: ServiceProvider<true> = DoFactory.createServiceProvider(true);
+                serviceProvider.id = serviceProviderIdToAttach;
+                serviceProviderRepo.findById.mockResolvedValue(serviceProvider);
+
+                const result: void | DomainError = await rolle.attachServiceProvider(serviceProviderIdToAttach);
+
+                expect(result).not.toBeInstanceOf(DomainError);
+                expect(rolle.serviceProviderIds.includes(serviceProviderIdToAttach)).toBeTruthy();
+                expect(
+                    rolle.serviceProviderIds.filter((id: string) => id === serviceProviderIdToAttach).length,
+                ).toEqual(1);
+            });
+        });
+        describe('when serviceProvider does not exist', () => {
+            it('should return error', async () => {
+                const serviceProviderIdToAttach: string = faker.string.uuid();
+                const rolle: Rolle<true> = rolleFactory.construct(
+                    faker.string.uuid(),
+                    faker.date.anytime(),
+                    faker.date.anytime(),
+                    '',
+                    '',
+                    RollenArt.LEHR,
+                    [],
+                    [],
+                );
+                serviceProviderRepo.findById.mockResolvedValue(undefined);
+
+                const result: void | DomainError = await rolle.attachServiceProvider(serviceProviderIdToAttach);
+
+                expect(result).toBeInstanceOf(DomainError);
+                expect(rolle.serviceProviderIds.includes(serviceProviderIdToAttach)).toBeFalsy();
+            });
+        });
+        describe('when serviceProvider is already attached', () => {
+            it('should return error', async () => {
+                const serviceProviderIdToAttach: string = faker.string.uuid();
+                const rolle: Rolle<true> = rolleFactory.construct(
+                    faker.string.uuid(),
+                    faker.date.anytime(),
+                    faker.date.anytime(),
+                    '',
+                    '',
+                    RollenArt.LEHR,
+                    [],
+                    [serviceProviderIdToAttach],
+                );
+
+                const serviceProvider: ServiceProvider<true> = DoFactory.createServiceProvider(true);
+                serviceProvider.id = serviceProviderIdToAttach;
+                serviceProviderRepo.findById.mockResolvedValue(serviceProvider);
+
+                const result: void | DomainError = await rolle.attachServiceProvider(serviceProviderIdToAttach);
+
+                expect(result).toBeInstanceOf(DomainError);
+                expect(rolle.serviceProviderIds.includes(serviceProviderIdToAttach)).toBeTruthy();
+                expect(
+                    rolle.serviceProviderIds.filter((id: string) => id === serviceProviderIdToAttach).length,
+                ).toEqual(1);
+            });
         });
     });
 });
