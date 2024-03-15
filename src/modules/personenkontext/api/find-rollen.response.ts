b0f361bedc79e05d488308a9b32fa3283870cda3
DPDS93CT <cornelius.tegtmeyer@dataport.de>
2024-03-15 13:01:50 +0100

    adjust FindRollenResponse to use RollenResponse for moeglicheRollen

--- src/modules/personenkontext/api/find-rollen.response.ts
+++ src/modules/personenkontext/api/find-rollen.response.ts
@@ -1,14 +1,28 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { Rolle } from '../../rolle/domain/rolle.js';
+import { RolleResponse } from '../../rolle/api/rolle.response.js';
 
 export class FindRollenResponse {
     public constructor(moeglicheRollen: Rolle<true>[], total: number) {
-        this.moeglicheRollen = moeglicheRollen;
+        this.moeglicheRollen = moeglicheRollen.map((rolle: Rolle<true>) => this.createRolleResponse(rolle));
         this.total = total;
     }
 
-    @ApiProperty({ type: [Rolle<true>] })
-    public readonly moeglicheRollen: Rolle<true>[];
+    private createRolleResponse(rolle: Rolle<true>): RolleResponse {
+        return {
+            createdAt: rolle.createdAt,
+            updatedAt: rolle.updatedAt,
+            name: rolle.name,
+            id: rolle.id,
+            administeredBySchulstrukturknoten: rolle.administeredBySchulstrukturknoten,
+            rollenart: rolle.rollenart,
+            merkmale: rolle.merkmale,
+            systemrechte: rolle.systemrechte,
+        };
+    }
+
+    @ApiProperty({ type: [RolleResponse] })
+    public readonly moeglicheRollen: RolleResponse[];
 
     @ApiProperty({ type: Number })
     public readonly total: number;
