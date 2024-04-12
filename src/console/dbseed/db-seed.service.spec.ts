94dae7fd65b5d0d62c24abccd402415fd003c939
Caspar Neumann <caspar.neumann@capgemini.com>
2024-04-12 13:18:33 +0200

    Add Test When kcUser exists & is tried so bee seeded

--- src/console/dbseed/db-seed.service.spec.ts
+++ src/console/dbseed/db-seed.service.spec.ts
@@ -23,16 +23,19 @@ import { RolleFactory } from '../../modules/rolle/domain/rolle.factory.js';
 import { ServiceProviderRepo } from '../../modules/service-provider/repo/service-provider.repo.js';
 import { DBiamPersonenkontextRepo } from '../../modules/personenkontext/persistence/dbiam-personenkontext.repo.js';
 import { ServiceProviderFactory } from '../../modules/service-provider/domain/service-provider.factory.js';
-import { KeycloakUserService } from '../../modules/keycloak-administration/index.js';
+import { KeycloakUserService, UserDo } from '../../modules/keycloak-administration/index.js';
 import { KeycloakAdministrationService } from '../../modules/keycloak-administration/domain/keycloak-admin-client.service.js';
 import { KeycloakAdminClient } from '@s3pweb/keycloak-admin-client-cjs';
 import { KeycloakInstanceConfig } from '../../modules/keycloak-administration/keycloak-instance-config.js';
+import { Person } from '../../modules/person/domain/person.js';
 
 describe('DbSeedService', () => {
     let module: TestingModule;
     let dbSeedService: DbSeedService;
     let organisationRepoMock: DeepMocked<OrganisationRepo>;
     let rolleRepoMock: DeepMocked<RolleRepo>;
+    let personRepoMock: DeepMocked<PersonRepository>;
+    let kcUserService: DeepMocked<KeycloakUserService>;
 
     beforeAll(async () => {
         module = await Test.createTestingModule({
@@ -74,11 +77,17 @@ describe('DbSeedService', () => {
                     provide: ServiceProviderRepo,
                     useValue: createMock<ServiceProviderRepo>(),
                 },
+                {
+                    provide: KeycloakUserService,
+                    useValue: createMock<KeycloakUserService>(),
+                },
             ],
         }).compile();
         dbSeedService = module.get(DbSeedService);
         organisationRepoMock = module.get(OrganisationRepo);
         rolleRepoMock = module.get(RolleRepo);
+        personRepoMock = module.get(PersonRepository);
+        kcUserService = module.get(KeycloakUserService);
     });
 
     afterAll(async () => {
@@ -307,6 +316,42 @@ describe('DbSeedService', () => {
                 await expect(dbSeedService.seedServiceProvider(fileContentAsStr)).resolves.not.toThrow(
                     EntityNotFoundError,
                 );
+            });
+        });
+    });
+
+    describe('seedPerson', () => {
+        describe('person already exists in keycloak', () => {
+            it('should delete the person and then create it again', async () => {
+                const fileContentAsStr: string = fs.readFileSync(
+                    `./seeding/seeding-integration-test/existingPerson/02_person.json`,
+                    'utf-8',
+                );
+
+                const person: Person<true> = Person.construct(
+                    faker.string.uuid(),
+                    faker.date.past(),
+                    faker.date.recent(),
+                    faker.person.lastName(),
+                    faker.person.firstName(),
+                    '1',
+                    'testusername',
+                    faker.string.uuid(),
+                );
+
+                const existingUser: UserDo<true> = new UserDo<true>();
+                existingUser.id = faker.string.uuid();
+                existingUser.createdDate = faker.date.recent();
+                existingUser.username = 'testusername';
+
+                kcUserService.findOne.mockResolvedValueOnce({ ok: true, value: existingUser });
+                kcUserService.delete.mockResolvedValueOnce({ ok: true, value: undefined });
+                personRepoMock.create.mockResolvedValue(person);
+
+                await dbSeedService.seedPerson(fileContentAsStr);
+
+                await expect(dbSeedService.seedPerson(fileContentAsStr)).resolves.not.toThrow(EntityNotFoundError);
+                expect(kcUserService.delete).toHaveBeenCalled();
             });
         });
     });
