318c9281ea19280f7b0c52ec1bf42ace588b66ff
godtierbatuhan <boucharay1001@gmail.com>
2024-04-19 00:04:45 +0200

    Fixed tests

--- src/modules/person/api/person.frontend.controller.spec.ts
+++ src/modules/person/api/person.frontend.controller.spec.ts
@@ -59,6 +59,7 @@ describe('PersonFrontendController', () => {
             familienname: options.lastName,
             vorname: options.firstName,
             sichtfreigabe: SichtfreigabeType.NEIN,
+            suchFilter: ''
         };
         const person1: Person<true> = Person.construct(
             faker.string.uuid(),
--- src/modules/person/api/personen-query.param.spec.ts
+++ src/modules/person/api/personen-query.param.spec.ts
@@ -10,6 +10,7 @@ describe('PersonenQueryParam', () => {
         familienname: faker.person.lastName(),
         referrer: 'referrer',
         vorname: faker.person.firstName(),
+        suchFilter: ''
     };
 
     it('should convert a plain object to a class of PersonenQueryParam', () => {
