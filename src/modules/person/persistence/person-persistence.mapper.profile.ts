531750bfe1dc883824abfaa4276ccf366b7f4d7e
Marvin Rode <marvin.rode@capgemini.com>
2024-04-18 13:43:35 +0200

    Fix mapping errors

--- src/modules/person/persistence/person-persistence.mapper.profile.ts
+++ src/modules/person/persistence/person-persistence.mapper.profile.ts
@@ -1,4 +1,4 @@
-import { Mapper, MappingProfile, createMap, forMember, ignore } from '@automapper/core';
+import { Mapper, MappingProfile, createMap, forMember, ignore, mapFrom } from '@automapper/core';
 import { AutomapperProfile, getMapperToken } from '@automapper/nestjs';
 import { Inject, Injectable } from '@nestjs/common';
 import { PersonDo } from '../domain/person.do.js';
@@ -17,12 +17,24 @@ export class PersonPersistenceMapperProfile extends AutomapperProfile {
             createMap(mapper, PersonDo, PersonEntity);
             createMap(mapper, PersonEntity, PersonDo);
 
-            createMap(mapper, PersonenkontextDo, PersonenkontextEntity);
+            createMap(
+                mapper,
+                PersonenkontextDo,
+                PersonenkontextEntity,
+                forMember(
+                    (dest: PersonenkontextEntity) => dest.personId,
+                    mapFrom((from: PersonenkontextDo<boolean>) => ({ id: from.personId })),
+                ),
+            );
             createMap(
                 mapper,
                 PersonenkontextEntity,
                 PersonenkontextDo,
                 forMember((dest: PersonenkontextDo<boolean>) => dest.organisation, ignore()),
+                forMember(
+                    (dest: PersonenkontextDo<boolean>) => dest.personId,
+                    mapFrom((from: PersonenkontextEntity) => from.personId.id),
+                ),
             );
         };
     }
