02f0ee5ea6a096eb493781a2aa4f70f784b93b30
Marvin Rode <marvin.rode@capgemini.com>
2024-04-26 14:38:19 +0200

    Remove POC code

--- src/core/eventbus/event.module.ts
+++ src/core/eventbus/event.module.ts
@@ -3,7 +3,6 @@ import { Global, Module, OnApplicationBootstrap } from '@nestjs/common';
 
 import { ClassLogger } from '../logging/class-logger.js';
 import { LoggerModule } from '../logging/logger.module.js';
-import { EventPocController } from './poc/poc-controller.js';
 import { EventDiscoveryService } from './services/event-discovery.service.js';
 import { EventService } from './services/event.service.js';
 
@@ -11,7 +10,6 @@ import { EventService } from './services/event.service.js';
 @Module({
     imports: [LoggerModule.register(EventModule.name), DiscoveryModule],
     providers: [EventService, EventDiscoveryService],
-    controllers: [EventPocController], // TODO: Remove
     exports: [EventService],
 })
 export class EventModule implements OnApplicationBootstrap {
--- src/core/eventbus/poc/poc-event.ts
+++
@@ -1,8 +0,0 @@
-// todo: remove
-import { BaseEvent } from '../types/base-event.js';
-
-export class TestEvent extends BaseEvent {
-    public constructor(public message: string) {
-        super(TestEvent.name);
-    }
-}
