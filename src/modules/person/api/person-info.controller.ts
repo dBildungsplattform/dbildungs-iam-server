cef26fad463c4152f6e6b5194273211314ad0265
Patrick Sachmann <20001160+psachmann@users.noreply.github.com>
2024-05-02 14:04:39 +0200

    EW-755 updating the controller

--- src/modules/person/api/person-info.controller.ts
+++ src/modules/person/api/person-info.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseFilters } from '@nestjs/common';
 import {
     ApiBearerAuth,
     ApiOAuth2,
@@ -17,7 +17,12 @@ import { Person } from '../domain/person.js';
 import { PersonRepository } from '../persistence/person.repository.js';
 import { PersonInfoResponse } from './person-info.response.js';
 import { ClassLogger } from '../../../core/logging/class-logger.js';
+import { SchulConnexValidationErrorFilter } from '../../../shared/error/schulconnex-validation-error.filter.js';
+import { PersonApiMapper } from '../mapper/person-api.mapper.js';
 
+@UseFilters(SchulConnexValidationErrorFilter)
+@ApiBearerAuth()
+@ApiOAuth2(['openid'])
 @ApiTags('person-info')
 @Controller({ path: 'person-info' })
 export class PersonInfoController {
@@ -25,29 +30,28 @@ export class PersonInfoController {
         private readonly logger: ClassLogger,
         private readonly personRepository: PersonRepository,
         private readonly dBiamPersonenkontextRepo: DBiamPersonenkontextRepo,
+        private readonly mapper: PersonApiMapper,
     ) {
-        this.logger.info('personinfo controller created.');
+        this.logger.info(`Creating ${PersonInfoController.name}`);
     }
 
     @Get()
-    @ApiBearerAuth()
-    @ApiOAuth2(['openid'])
     @ApiOperation({ summary: 'Info about logged in person.' })
     @ApiUnauthorizedResponse({ description: 'person is not logged in.' })
     @ApiOkResponse({ description: 'Returns info about the person.', type: PersonInfoResponse })
     public async info(@Permissions() permissions: PersonPermissions): Promise<PersonInfoResponse> {
-        this.logger.info('Getting info about person.');
-        this.logger.info('Permissions: ' + permissions.personFields.id);
         const personId: string = permissions.personFields.id;
         const person: Option<Person<true>> = await this.personRepository.findById(personId);
+
         if (!person) {
             throw SchulConnexErrorMapper.mapSchulConnexErrorToHttpException(
-                SchulConnexErrorMapper.mapDomainErrorToSchulConnexError(new EntityNotFoundError('Person', personId)),
+                SchulConnexErrorMapper.mapDomainErrorToSchulConnexError(new EntityNotFoundError(Person.name, personId)),
             );
         }
 
         const personenkontexte: Personenkontext<true>[] = await this.dBiamPersonenkontextRepo.findByPerson(personId);
+        const response: PersonInfoResponse = this.mapper.toPersonInfoResponse(personId, person, personenkontexte);
 
-        return new PersonInfoResponse(person, personenkontexte);
+        return response;
     }
 }
