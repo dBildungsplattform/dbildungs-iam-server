f28c782a899829c579cd7b5d73a8053193ae00aa
Cornelius <144817755+DPDS93CT@users.noreply.github.com>
2024-05-22 11:00:09 +0200

    SPSH-595: Datenbankschema Migrationen (#495)
    * add command for migration, add migrations pckg to project, add tests
    
    * adjust docs
    
    * adjust README
    
    * add migrate up command
    
    * split db-migration commands in different console-files
    
    * adjust migration docu
    
    * add initial migration
    
    * trigger pipeline run
    
    * make migration available in cluster
    
    * add migration docu for DEV and TEST
    
    * change migration config
    
    * fix typo README

--- src/console/db.console.ts
+++ src/console/db.console.ts
@@ -2,12 +2,21 @@ import { Command, CommandRunner } from 'nest-commander';
 import { ClassLogger } from '../core/logging/class-logger.js';
 import { DbInitConsole } from './db-init.console.js';
 import { DbSeedConsole } from './dbseed/db-seed.console.js';
+import { DbInitMigrationConsole } from './dbmigrate/db-init-migration.console.js';
+import { DbCreateMigrationConsole } from './dbmigrate/db-create-migration.console.js';
+import { DbApplyMigrationConsole } from './dbmigrate/db-apply-migration.console.js';
 
 @Command({
     name: 'db',
     arguments: '<sub-command>',
     description: 'runs commands to manage the database',
-    subCommands: [DbInitConsole, DbSeedConsole],
+    subCommands: [
+        DbInitConsole,
+        DbSeedConsole,
+        DbInitMigrationConsole,
+        DbCreateMigrationConsole,
+        DbApplyMigrationConsole,
+    ],
 })
 export class DbConsole extends CommandRunner {
     public constructor(private readonly logger: ClassLogger) {
