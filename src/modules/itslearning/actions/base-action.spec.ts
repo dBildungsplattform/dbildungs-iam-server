a9b846fa4323e434bf1360d8fe610d6469eafb8b
Marvin Rode <marvin.rode@capgemini.com>
2024-06-06 08:58:35 +0200

    Formatting

--- src/modules/itslearning/actions/base-action.spec.ts
+++ src/modules/itslearning/actions/base-action.spec.ts
@@ -32,8 +32,8 @@ function buildXMLResponse(codeMajor: 'success' | 'failure', severity: 'status' |
 		xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
 		xmlns:xsd="http://www.w3.org/2001/XMLSchema">
 		${body}
-		</s:Body>
-	</s:Envelope>`;
+    </s:Body>
+</s:Envelope>`;
 }
 
 type DummyResponse = {
