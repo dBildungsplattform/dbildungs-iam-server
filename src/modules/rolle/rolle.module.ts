1c84e55d898d34485b3af3e7e997c70e052de0c7
Marvin Rode <marvin.rode@capgemini.com>
2024-06-19 09:14:12 +0200

    Undo formatting

--- src/modules/rolle/rolle.module.ts
+++ src/modules/rolle/rolle.module.ts
@@ -2,10 +2,9 @@ import { Module } from '@nestjs/common';
 import { LoggerModule } from '../../core/logging/logger.module.js';
 import { RolleRepo } from './repo/rolle.repo.js';
 import { ServiceProviderModule } from '../service-provider/service-provider.module.js';
-
+import { ServiceProviderRepo } from '../service-provider/repo/service-provider.repo.js';
 import { RolleFactory } from './domain/rolle.factory.js';
 import { OrganisationModule } from '../organisation/organisation.module.js';
-import { ServiceProviderRepo } from '../service-provider/repo/service-provider.repo.js';
 
 @Module({
     imports: [ServiceProviderModule, LoggerModule.register(RolleModule.name), OrganisationModule],
