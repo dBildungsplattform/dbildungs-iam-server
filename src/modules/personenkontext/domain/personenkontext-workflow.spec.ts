1ba551185fc8230a2ec21ad858ec09a4b0f22c20
godtierbatuhan <boucharay1001@gmail.com>
2024-06-27 22:30:02 +0200

    linting

--- src/modules/personenkontext/domain/personenkontext-workflow.spec.ts
+++ src/modules/personenkontext/domain/personenkontext-workflow.spec.ts
@@ -403,6 +403,28 @@ describe('PersonenkontextWorkflow', () => {
 
             expect(result.length).toEqual(2);
         });
+        it('should handle organisations with name and kennung', async () => {
+            const org1: OrganisationDo<true> = DoFactory.createOrganisation(true, {
+                name: 'Carl-Orff',
+                kennung: '123',
+            });
+            const org2: OrganisationDo<true> = DoFactory.createOrganisation(true, {
+                name: 'Amalie',
+                kennung: '321',
+            });
+            const orgsWithRecht: string[] = [org1.id, org2.id];
+
+            organisationRepoMock.find.mockResolvedValue([org1, org2]);
+            personpermissionsMock.getOrgIdsWithSystemrecht.mockResolvedValue(orgsWithRecht);
+
+            const result: OrganisationDo<true>[] = await anlage.findAllSchulstrukturknoten(
+                personpermissionsMock,
+                undefined,
+                10,
+            );
+
+            expect(result.length).toEqual(2);
+        });
     });
 
     describe('findRollenForOrganisation', () => {
