4249eb0e7f980d708c18306126401e7375369e6f
DPDS93CT <cornelius.tegtmeyer@dataport.de>
2024-07-03 10:26:52 +0200

    fix lint

--- src/modules/email/email.module.ts
+++ src/modules/email/email.module.ts
@@ -6,20 +6,13 @@ import { EmailGeneratorService } from './domain/email-generator.service.js';
 import { EmailEventHandler } from './domain/email-event-handler.js';
 import { EmailServiceRepo } from './persistence/email-service.repo.js';
 import { EventService } from '../../core/eventbus/index.js';
-import {PersonModule} from "../person/person.module.js";
-import {RolleModule} from "../rolle/rolle.module.js";
-import {ServiceProviderModule} from "../service-provider/service-provider.module.js";
+import { PersonModule } from '../person/person.module.js';
+import { RolleModule } from '../rolle/rolle.module.js';
+import { ServiceProviderModule } from '../service-provider/service-provider.module.js';
 
 @Module({
     imports: [PersonModule, RolleModule, ServiceProviderModule, LoggerModule.register(EmailModule.name)],
-    providers: [
-        EmailRepo,
-        EmailServiceRepo,
-        EmailFactory,
-        EmailGeneratorService,
-        EmailEventHandler,
-        EventService,
-    ],
+    providers: [EmailRepo, EmailServiceRepo, EmailFactory, EmailGeneratorService, EmailEventHandler, EventService],
     exports: [EmailRepo, EmailFactory, EmailGeneratorService, EmailEventHandler],
 })
 export class EmailModule {}
