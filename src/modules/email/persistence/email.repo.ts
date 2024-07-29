9c2460448d2d52a3ecf3baf1000a01da332fea81
Cornelius <144817755+DPDS93CT@users.noreply.github.com>
2024-07-29 13:57:27 +0200

    SPSH-746: E-Mailadresse für Lehrkräfte generieren (#549)
    * add email repo, aggregate, name-service and factory
    
    * adjust email tests
    
    * add events for email
    
    * adjust email-event-handler and events
    
    * add EmailServiceRepo
    
    * fix circle in dependencies for email, adjust tests
    
    * adjust Email aggregate to have valid-flag
    
    * adjust email-address generation to limit 64 before at-sign
    
    * fire PersonenkontextDeleteEvent in dbiam-personenkontext.repo
    
    * adjust email to reference emailAddresses
    
    * adjust tests for email
    
    * persist EmailAddresses when Email is persisted
    
    * trigger email-deletion for PersonDeletedEvent
    
    * enabling of existing Email via EventHandler
    
    * adjust emailAdress-persisting to Cascade=none
    
    * adjust re-enabling of existing email-address
    
    * adjust tests for email.repo and email-event-handler
    
    * fix email-service.repo
    
    * adjust disabling email-addresses when person is deleted
    
    * add ServiceProviderTarget EMAIL, remove IsValid from Email
    
    * adjust email-address generation
    
    * remove EmailEntity
    
    * fix imports of email.module
    
    * fix lint
    
    * adjustments
    
    * adjust aggregate, repo, factory, handler and generator for email
    
    * merge main into SPSH-746: fix tests
    
    * adjust email-address
    
    * populate email-address on personEntity before deletion for events
    
    * adjust poplating email of person for events
    
    * add tests
    
    * remove unnecessary code
    
    * adjust enabling of existing email in event-handler, fix imports
    
    * add RolleUpdatedEvent
    
    * adjust email-event-handler: introduce more generic handlePerson
    
    * rm unnecessary comments
    
    * update migrations
    
    * add missing migrations for cluster
    
    * adjust handlePerson to force email-creation for certain events
    
    * fix bug in email-event-handler
    
    * revert evaluation for email to KATEGORIE instead of TARGET-type
    
    * add test cases for DbiamPersonenUebersicht

---
+++ src/modules/email/persistence/email.repo.ts
@@ -0,0 +1,109 @@
+import { EntityManager, RequiredEntityData, rel } from '@mikro-orm/core';
+import { Injectable } from '@nestjs/common';
+import { PersonID } from '../../../shared/types/index.js';
+import { EmailAddressEntity } from './email-address.entity.js';
+import { EmailAddress } from '../domain/email-address.js';
+import { EmailAddressNotFoundError } from '../error/email-address-not-found.error.js';
+import { ClassLogger } from '../../../core/logging/class-logger.js';
+import { DomainError } from '../../../shared/error/index.js';
+import { PersonEntity } from '../../person/persistence/person.entity.js';
+
+function mapAggregateToData(emailAddress: EmailAddress<boolean>): RequiredEntityData<EmailAddressEntity> {
+    return {
+        // Don't assign createdAt and updatedAt, they are auto-generated!
+        id: emailAddress.id,
+        personId: rel(PersonEntity, emailAddress.personId),
+        address: emailAddress.address,
+        enabled: emailAddress.enabled,
+    };
+}
+
+function mapEntityToAggregate(entity: EmailAddressEntity): EmailAddress<boolean> {
+    return new EmailAddress(
+        entity.id,
+        entity.createdAt,
+        entity.updatedAt,
+        entity.personId.id,
+        entity.address,
+        entity.enabled,
+    );
+}
+
+@Injectable()
+export class EmailRepo {
+    public constructor(
+        private readonly em: EntityManager,
+        private readonly logger: ClassLogger,
+    ) {}
+
+    public async findByPerson(personId: PersonID): Promise<Option<EmailAddress<true>>> {
+        const emailAddressEntity: Option<EmailAddressEntity> = await this.em.findOne(
+            EmailAddressEntity,
+            {
+                personId: { $eq: personId },
+            },
+            {},
+        );
+        if (!emailAddressEntity) return undefined;
+
+        return mapEntityToAggregate(emailAddressEntity);
+    }
+
+    public async existsEmailAddress(address: string): Promise<boolean> {
+        const emailAddressEntity: Option<EmailAddressEntity> = await this.em.findOne(
+            EmailAddressEntity,
+            { address: address },
+            {},
+        );
+
+        return !!emailAddressEntity;
+    }
+
+    public async deactivateEmailAddress(emailAddress: string): Promise<EmailAddressEntity | EmailAddressNotFoundError> {
+        const emailAddressEntity: Option<EmailAddressEntity> = await this.em.findOne(
+            EmailAddressEntity,
+            { address: emailAddress },
+            {},
+        );
+        if (!emailAddressEntity) return new EmailAddressNotFoundError(emailAddress);
+
+        emailAddressEntity.enabled = false;
+        await this.em.persistAndFlush(emailAddressEntity);
+
+        return emailAddressEntity;
+    }
+
+    public async save(emailAddress: EmailAddress<boolean>): Promise<EmailAddress<true> | DomainError> {
+        if (emailAddress.id) {
+            return this.update(emailAddress);
+        } else {
+            return this.create(emailAddress);
+        }
+    }
+
+    private async create(emailAddress: EmailAddress<boolean>): Promise<EmailAddress<true> | DomainError> {
+        const emailAddressEntity: EmailAddressEntity = this.em.create(
+            EmailAddressEntity,
+            mapAggregateToData(emailAddress),
+        );
+        await this.em.persistAndFlush(emailAddressEntity);
+
+        return mapEntityToAggregate(emailAddressEntity);
+    }
+
+    private async update(emailAddress: EmailAddress<boolean>): Promise<EmailAddress<true> | DomainError> {
+        const emailAddressEntity: Option<EmailAddressEntity> = await this.em.findOne(EmailAddressEntity, {
+            id: emailAddress.id,
+        });
+
+        if (emailAddressEntity) {
+            emailAddressEntity.assign(mapAggregateToData(emailAddress), {});
+            await this.em.persistAndFlush(emailAddressEntity);
+        } else {
+            this.logger.error(`Email-address:${emailAddress.currentAddress} could not be found`);
+            return new EmailAddressNotFoundError(emailAddress.address);
+        }
+
+        return mapEntityToAggregate(emailAddressEntity);
+    }
+}
