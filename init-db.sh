ccf00ea0ff208f73f62d7f959e5d092dab6c450f
Timo K <timo.kleist@capgemini.com>
2024-08-06 14:43:29 +0200

    remove tryout files

--- init-db.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "postgres" <<-EOSQL
-    CREATE DATABASE privacyidea;
-    GRANT ALL PRIVILEGES ON DATABASE privacyidea TO $POSTGRES_USER;
-EOSQL
