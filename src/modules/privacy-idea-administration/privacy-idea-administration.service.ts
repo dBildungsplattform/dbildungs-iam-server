b85d203f9a7d7ad1bbbcef64654eefa9e8d4b88a
DeFi3298 <dennis.fischer@capgemini.com>
2024-08-06 09:04:00 +0200

    HardwareToken-Assignment
    Implemented Logic for Hardware-Token Assignment

--- src/modules/privacy-idea-administration/privacy-idea-administration.service.ts
+++ src/modules/privacy-idea-administration/privacy-idea-administration.service.ts
@@ -8,8 +8,15 @@ import {
     PrivacyIdeaResponseTokens,
     PrivacyIdeaToken,
     AuthenticaitonResponse,
+    ResetTokenResponse,
+    ResetTokenPayload,
+    TokenOTPSerialResponse,
+    AssignTokenResponse,
+    AssignTokenPayload,
+    TokenVerificationResponse,
     UserResponse,
 } from './privacy-idea-api.types.js';
+import { TokenError } from './api/errors/token-error.js';
 
 @Injectable()
 export class PrivacyIdeaAdministrationService {
@@ -197,5 +204,100 @@ export class PrivacyIdeaAdministrationService {
                 throw new Error(`Error adding user: Unknown error occurred`);
             }
         }
+    }
+
+    private async verifyTokenStatus(serial: string, token: string): Promise<TokenVerificationResponse> {
+        const endpoint: string = `/token?serial=${serial}`;
+        const baseUrl: string = process.env['PI_BASE_URL'] ?? 'http://localhost:5000';
+        const url: string = `${baseUrl}${endpoint}`;
+        const headers: { Authorization: string } = {
+            Authorization: `${token}`,
+        };
+        try {
+            const response: AxiosResponse<TokenVerificationResponse> = await firstValueFrom(
+                this.httpService.get(url, { headers: headers }),
+            );
+            return response.data;
+        } catch (error) {
+            throw new TokenError(
+                'Leider konnte ihr Hardware-Token aus technischen Gründen nicht aktiviert werden. Bitte versuchen Sie es zu einem späteren Zeitpunkt erneut. Falls das Problem bestehen bleibt, stellen Sie bitte eine Anfrage über den IQSH Helpdesk.--Link: https://www.secure-lernnetz.de/helpdesk/',
+                'general-token-error',
+            );
+        }
+    }
+
+    private async getSerialWithOTP(otp: string, token: string): Promise<TokenOTPSerialResponse> {
+        const endpoint: string = `/token/getserial/${otp}?unassigned=1`;
+        const baseUrl: string = process.env['PI_BASE_URL'] ?? 'http://localhost:5000';
+        const url: string = `${baseUrl}${endpoint}`;
+        const headers: { Authorization: string } = {
+            Authorization: `${token}`,
+        };
+
+        try {
+            const response: AxiosResponse<TokenOTPSerialResponse> = await firstValueFrom(
+                this.httpService.get(url, { headers: headers }),
+            );
+            return response.data;
+        } catch (error) {
+            throw new TokenError(
+                'Leider konnte ihr Hardware-Token aus technischen Gründen nicht aktiviert werden. Bitte versuchen Sie es zu einem späteren Zeitpunkt erneut. Falls das Problem bestehen bleibt, stellen Sie bitte eine Anfrage über den IQSH Helpdesk.--Link: https://www.secure-lernnetz.de/helpdesk/',
+                'general-token-error',
+            );
+        }
+    }
+
+    private async assignToken(serial: string, token: string, user: string): Promise<AssignTokenResponse> {
+        const endpoint: string = '/token/assign';
+        const baseUrl: string = process.env['PI_BASE_URL'] ?? 'http://localhost:5000';
+        const url: string = baseUrl + endpoint;
+        const headers: { Authorization: string; 'Content-Type': string } = {
+            Authorization: `${token}`,
+            'Content-Type': 'application/json',
+        };
+
+        const payload: AssignTokenPayload = {
+            serial,
+            user: user,
+            realm: 'defrealm',
+        };
+
+        try {
+            const response: AxiosResponse<AssignTokenResponse> = await firstValueFrom(
+                this.httpService.post(url, payload, { headers }),
+            );
+            return response.data;
+        } catch (error) {
+            throw new TokenError(
+                'Leider konnte ihr Hardware-Token aus technischen Gründen nicht aktiviert werden. Bitte versuchen Sie es zu einem späteren Zeitpunkt erneut. Falls das Problem bestehen bleibt, stellen Sie bitte eine Anfrage über den IQSH Helpdesk.--Link: https://www.secure-lernnetz.de/helpdesk/',
+                'general-token-error',
+            );
+        }
+    }
+
+    public async assignHardwareToken(serial: string, otp: string, user: string): Promise<AssignTokenResponse> {
+        const token: string = await this.getJWTToken();
+        const tokenVerificationResponse: TokenVerificationResponse = await this.verifyTokenStatus(serial, token);
+        //Check token existence
+        if (tokenVerificationResponse.result.value.count === 0) {
+            throw new TokenError(
+                'Die eingegebene Seriennummer konnte leider nicht gefunden werden. Vergewissern Sie sich bitte, das Sie eine korrekte Seriennummer eingegeben haben.',
+                'token-not-found',
+            );
+        }
+        //Check token assigned or not
+        if (tokenVerificationResponse.result.value.tokens[0]?.username !== '') {
+            throw new TokenError(
+                'Die eingegebene Seriennummer wird bereits aktiv verwendet. Bitte überprüfen Sie ihre Eingabe und versuchen Sie es erneut.',
+                'token-already-assigned',
+            );
+        }
+        //Verify otp input
+        const tokenOTPserialResponse: TokenOTPSerialResponse = await this.getSerialWithOTP(otp, token);
+        if (tokenOTPserialResponse.result.value.serial !== serial) {
+            throw new TokenError('Ungültiger Code. Bitte versuchen Sie es erneut.', 'token-otp-not-valid');
+        }
+        // Call assignToken
+        return this.assignToken(serial, token, user);
     }
 }
