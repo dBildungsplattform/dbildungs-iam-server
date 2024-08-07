eae6a5f57729753f2daee93b618142844869ed82
Nael-Alshowaikh <153004766+Nael-Alshowaikh@users.noreply.github.com>
2024-08-07 17:33:05 +0200

    SPSH-738: eslint fix

--- src/modules/organisation/api/organisation.controller.ts
+++ src/modules/organisation/api/organisation.controller.ts
@@ -12,7 +12,6 @@ import {
     ApiUnauthorizedResponse,
 } from '@nestjs/swagger';
 import { SchulConnexErrorMapper } from '../../../shared/error/schul-connex-error.mapper.js';
-import { SchulConnexError } from '../../../shared/error/schul-connex.error.js';
 import { SchulConnexValidationErrorFilter } from '../../../shared/error/schulconnex-validation-error.filter.js';
 import { Paged, PagingHeadersObject } from '../../../shared/paging/index.js';
 import { PagedResponse } from '../../../shared/paging/paged.response.js';
