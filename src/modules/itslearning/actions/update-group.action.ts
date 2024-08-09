ea99ed5b7ad44283b46df20254b2cf6347428532
Marvin Rode <marvin.rode@capgemini.com>
2024-08-09 15:45:17 +0200

    Implement update and delete group actions

---
+++ src/modules/itslearning/actions/update-group.action.ts
@@ -0,0 +1,81 @@
+import { DomainError } from '../../../shared/error/domain.error.js';
+import { IMS_COMMON_SCHEMA, IMS_GROUP_MAN_DATA_SCHEMA, IMS_GROUP_MAN_MESS_SCHEMA } from '../schemas.js';
+import { IMSESAction } from './base-action.js';
+
+// Incomplete
+export type UpdateGroupParams = {
+    id: string;
+
+    name: string;
+    type: 'Unspecified' | 'Site' | 'School' | 'Course' | 'CourseGroup';
+
+    parentId: string;
+
+    longDescription?: string;
+    fullDescription?: string;
+};
+
+type UpdateGroupResponseBody = {
+    updateGroupResponse: undefined;
+};
+
+export class UpdateGroupAction extends IMSESAction<UpdateGroupResponseBody, void> {
+    public override action: string = 'http://www.imsglobal.org/soap/gms/updateGroup';
+
+    public constructor(private readonly params: UpdateGroupParams) {
+        super();
+    }
+
+    public override buildRequest(): object {
+        let extension: object[] | undefined;
+        if (this.params.type === 'Course') {
+            extension = [
+                {
+                    'ims1:fieldName': 'course',
+                    'ims1:fieldType': 'String',
+                    'ims1:fieldValue': this.params.name,
+                },
+                {
+                    'ims1:fieldName': 'course/code',
+                    'ims1:fieldType': 'String',
+                    'ims1:fieldValue': this.params.name,
+                },
+            ];
+        }
+
+        return {
+            'ims:updateGroupRequest': {
+                '@_xmlns:ims': IMS_GROUP_MAN_MESS_SCHEMA,
+                '@_xmlns:ims1': IMS_COMMON_SCHEMA,
+                '@_xmlns:ims2': IMS_GROUP_MAN_DATA_SCHEMA,
+
+                'ims:sourcedId': {
+                    'ims1:identifier': this.params.id,
+                },
+
+                'ims:group': {
+                    'ims2:relationship': {
+                        'ims2:relation': 'Parent',
+                        'ims2:sourceId': {
+                            'ims1:identifier': this.params.parentId,
+                        },
+                    },
+                    'ims2:description': {
+                        'ims2:descShort': this.params.name,
+                        'ims2:descLong': this.params.longDescription,
+                        'ims2:descFull': this.params.fullDescription,
+                    },
+                    'ims2:extension': extension && { 'ims1:extensionField': extension },
+                },
+            },
+        };
+    }
+
+    public override parseBody(): Result<void, DomainError> {
+        // Response does not contain data
+        return {
+            ok: true,
+            value: undefined,
+        };
+    }
+}
