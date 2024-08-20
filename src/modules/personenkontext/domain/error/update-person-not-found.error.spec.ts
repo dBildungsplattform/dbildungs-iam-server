b5dc5d80754a54383fadfc970274a393090dfdcd
godtierbatuhan <boucharay1001@gmail.com>
2024-08-20 20:30:08 +0200

    test

--- src/modules/personenkontext/domain/error/update-person-not-found.error.spec.ts
+++ src/modules/personenkontext/domain/error/update-person-not-found.error.spec.ts
@@ -1,20 +1,17 @@
 import { UpdatePersonNotFoundError } from './update-person-not-found.error.js';
-import { PersonenkontexteUpdateError } from './personenkontexte-update.error.js';
 
 describe('UpdatePersonNotFoundError', () => {
     it('should create an instance of UpdatePersonNotFoundError', () => {
         const error: UpdatePersonNotFoundError = new UpdatePersonNotFoundError();
 
         expect(error).toBeInstanceOf(UpdatePersonNotFoundError);
-        expect(error).toBeInstanceOf(PersonenkontexteUpdateError);
+        expect(error).toBeInstanceOf(UpdatePersonNotFoundError);
     });
 
     it('should have the correct error message', () => {
         const error: UpdatePersonNotFoundError = new UpdatePersonNotFoundError();
 
-        expect(error.message).toBe(
-            'Personenkontexte could not be updated because newer versions of personenkontexte exist.',
-        );
+        expect(error.message).toBe('Personenkontexte could not be updated because the person has been deleted.');
     });
 
     it('should handle no details gracefully', () => {
