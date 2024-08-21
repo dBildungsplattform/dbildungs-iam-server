02d6f6094d218517f4314df733a18fac633023ab
Kristoff Kiefer <kristoff.kiefer@capgemini.com>
2024-08-21 15:08:47 +0200

    Revert "Change URL for School-SH"
    This reverts commit cb338f8027d3d958ebc1f6112125b9124687cab0.

--- migrations/Migration20240816120248.ts
+++ migrations/Migration20240816120248.ts
@@ -1,9 +1,13 @@
 import { Migration } from '@mikro-orm/migrations';
 
 export class Migration20240816120248 extends Migration {
-    public up(): void {
-        this.addSql("UPDATE service_provider SET url = 'https://school-sh.qs.schule-sh.de/' WHERE name = 'School-SH';");
+    async up(): Promise<void> {
+        this.addSql('alter table "rolle" add column "ist_technisch" boolean not null default false;');
+
+        this.addSql("update rolle set ist_technisch = true where name = 'Migration';");
     }
 
-    public override down(): void {}
+    override async down(): Promise<void> {
+        this.addSql('alter table "rolle" drop column "ist_technisch";');
+    }
 }
