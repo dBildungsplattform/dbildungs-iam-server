cb338f8027d3d958ebc1f6112125b9124687cab0
Kristoff Kiefer <kristoff.kiefer@capgemini.com>
2024-08-21 12:23:51 +0200

    Change URL for School-SH

--- migrations/Migration20240816120248.ts
+++ migrations/Migration20240816120248.ts
@@ -1,13 +1,9 @@
 import { Migration } from '@mikro-orm/migrations';
 
 export class Migration20240816120248 extends Migration {
-    async up(): Promise<void> {
-        this.addSql('alter table "rolle" add column "ist_technisch" boolean not null default false;');
-
-        this.addSql("update rolle set ist_technisch = true where name = 'Migration';");
+    public up(): void {
+        this.addSql("UPDATE service_provider SET url = 'https://school-sh.qs.schule-sh.de/' WHERE name = 'School-SH';");
     }
 
-    override async down(): Promise<void> {
-        this.addSql('alter table "rolle" drop column "ist_technisch";');
-    }
+    public override down(): void {}
 }
