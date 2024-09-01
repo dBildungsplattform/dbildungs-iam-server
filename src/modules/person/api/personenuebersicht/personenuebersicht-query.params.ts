e0b2d8b374b7b0fd1162a1e4c3468cb408dfd77c
godtierbatuhan <boucharay1001@gmail.com>
2024-09-01 15:21:00 +0200

    PersonIds not only an array

--- src/modules/person/api/personenuebersicht/personenuebersicht-query.params.ts
+++ src/modules/person/api/personenuebersicht/personenuebersicht-query.params.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsArray, IsString } from 'class-validator';
+import { IsString } from 'class-validator';
 import { PagedQueryParams } from '../../../../shared/paging/index.js';
 import { PersonID } from '../../../../shared/types/aggregate-ids.types.js';
 
@@ -8,7 +8,6 @@ export class PersonenuebersichtQueryParams extends PagedQueryParams {
         description: 'An array of IDs for the persons.',
         type: [String],
     })
-    @IsArray()
     @IsString({ each: true })
     public readonly personIds?: PersonID[];
 }
