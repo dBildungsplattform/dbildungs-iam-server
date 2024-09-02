8158bd8cd18f2d9fa134832303db1251318e7420
Caspar Neumann <caspar.neumann@capgemini.com>
2024-09-02 12:25:00 +0200

    Remove Controller Changes

--- src/modules/personenkontext/api/dbiam-personenkontext.controller.ts
+++ src/modules/personenkontext/api/dbiam-personenkontext.controller.ts
@@ -10,25 +10,24 @@ import {
     ApiTags,
     ApiUnauthorizedResponse,
 } from '@nestjs/swagger';
-import { DomainError, EntityCouldNotBeCreated } from '../../../shared/error/index.js';
+import { DomainError } from '../../../shared/error/index.js';
 import { SchulConnexErrorMapper } from '../../../shared/error/schul-connex-error.mapper.js';
 import { SchulConnexValidationErrorFilter } from '../../../shared/error/schulconnex-validation-error.filter.js';
 import { Permissions } from '../../authentication/api/permissions.decorator.js';
 import { PersonPermissions } from '../../authentication/domain/person-permissions.js';
+import { DBiamPersonenkontextService } from '../domain/dbiam-personenkontext.service.js';
+import { PersonenkontextFactory } from '../domain/personenkontext.factory.js';
 import { Personenkontext } from '../domain/personenkontext.js';
 import { DBiamPersonenkontextResponse } from './response/dbiam-personenkontext.response.js';
 import { DBiamPersonenkontextRepo } from '../persistence/dbiam-personenkontext.repo.js';
+import { PersonenkontextSpecificationError } from '../specification/error/personenkontext-specification.error.js';
 import { DbiamPersonenkontextBodyParams } from './param/dbiam-personenkontext.body.params.js';
 import { DBiamFindPersonenkontexteByPersonIdParams } from './param/dbiam-find-personenkontext-by-personid.params.js';
 import { DbiamPersonenkontextError } from './dbiam-personenkontext.error.js';
 import { PersonenkontextExceptionFilter } from './personenkontext-exception-filter.js';
 import { PersonenkontexteUpdateExceptionFilter } from './personenkontexte-update-exception-filter.js';
+import { OrganisationMatchesRollenartError } from '../specification/error/organisation-matches-rollenart.error.js';
 import { AuthenticationExceptionFilter } from '../../authentication/api/authentication-exception-filter.js';
-import { DbiamPersonenkontextFactory } from '../domain/dbiam-personenkontext.factory.js';
-import { PersonenkontexteUpdate } from '../domain/personenkontexte-update.js';
-import { PermissionsOverride } from '../../../shared/permissions/permissions-override.js';
-import { PersonenkontexteUpdateError } from '../domain/error/personenkontexte-update.error.js';
-import { RollenSystemRecht } from '../../rolle/domain/rolle.enums.js';
 
 @UseFilters(
     new SchulConnexValidationErrorFilter(),
@@ -43,7 +42,8 @@ import { RollenSystemRecht } from '../../rolle/domain/rolle.enums.js';
 export class DBiamPersonenkontextController {
     public constructor(
         private readonly personenkontextRepo: DBiamPersonenkontextRepo,
-        private readonly dbiamPersonenkontextFactory: DbiamPersonenkontextFactory,
+        private readonly dbiamPersonenkontextService: DBiamPersonenkontextService,
+        private readonly personenkontextFactory: PersonenkontextFactory,
     ) {}
 
     @Get(':personId')
@@ -70,16 +70,6 @@ export class DBiamPersonenkontextController {
         return result.value.map((k: Personenkontext<true>) => new DBiamPersonenkontextResponse(k));
     }
 
-    /**
-     * In consultation with Kristoff Kiefer, the person context workflow aggregate is used internally in this Controller (The Controller is just a Wrapper),
-     * so that we can save ourselves the duplication of work and have one dedicated workflow for creating kontexts internally.
-     * This means that there is more logic than usual in the controller, but the endpoint may only be used by the migration anyway and will therefore be deleted after GoLive
-     *
-     * @param params
-     * @param permissions
-     * @returns DBiamPersonenkontextResponse of created Kontext
-     */
-
     @Post()
     @HttpCode(HttpStatus.CREATED)
     @ApiCreatedResponse({
@@ -97,57 +87,42 @@ export class DBiamPersonenkontextController {
         @Body() params: DbiamPersonenkontextBodyParams,
         @Permissions() permissions: PersonPermissions,
     ): Promise<DBiamPersonenkontextResponse> {
-        const isMigrationUser: boolean = await permissions.hasSystemrechteAtRootOrganisation([
-            RollenSystemRecht.MIGRATION_DURCHFUEHREN,
-        ] satisfies RollenSystemRecht[]);
-
-        if (!isMigrationUser) {
-            throw SchulConnexErrorMapper.mapSchulConnexErrorToHttpException(
-                SchulConnexErrorMapper.mapDomainErrorToSchulConnexError(new EntityCouldNotBeCreated('Personkontext')),
-            );
-        }
-
-        const dbiamPersonenkontextBodyParams: DbiamPersonenkontextBodyParams[] = (
-            await this.personenkontextRepo.findByPerson(params.personId)
-        ).map((pk: Personenkontext<true>) => ({
-            personId: pk.personId,
-            organisationId: pk.organisationId,
-            rolleId: pk.rolleId,
-        }));
-
-        dbiamPersonenkontextBodyParams.push({
-            //Dopplungs Check is avoided due to performance (anyways checked by DB-UNIQUE Constraint)
-            personId: params.personId,
-            organisationId: params.organisationId,
-            rolleId: params.rolleId,
-        });
-
-        const pkUpdate: PersonenkontexteUpdate = this.dbiamPersonenkontextFactory.createNewPersonenkontexteUpdate(
+        // Construct new personenkontext
+        const newPersonenkontext: Personenkontext<false> = this.personenkontextFactory.createNew(
             params.personId,
-            new Date(),
-            0,
-            dbiamPersonenkontextBodyParams,
-            new PermissionsOverride(permissions).grantPersonModifyPermission(params.personId),
+            params.organisationId,
+            params.rolleId,
+            undefined,
+            undefined,
+            undefined,
+            undefined,
+            undefined,
+            undefined,
         );
 
-        const updateResult: Personenkontext<true>[] | PersonenkontexteUpdateError = await pkUpdate.update();
+        //Check specifications
+        const specificationCheckError: Option<PersonenkontextSpecificationError> =
+            await this.dbiamPersonenkontextService.checkSpecifications(newPersonenkontext);
+        if (specificationCheckError) {
+            throw specificationCheckError;
+        }
 
-        if (updateResult instanceof PersonenkontexteUpdateError) {
-            throw SchulConnexErrorMapper.mapSchulConnexErrorToHttpException(
-                SchulConnexErrorMapper.mapDomainErrorToSchulConnexError(updateResult),
-            );
-        }
-        const createdPk: Personenkontext<true> | undefined = updateResult.find(
-            (pk: Personenkontext<true>) =>
-                pk.personId === params.personId &&
-                pk.rolleId === params.rolleId &&
-                pk.organisationId === params.organisationId,
+        // Save personenkontext
+        const saveResult: Result<Personenkontext<true>, DomainError> = await this.personenkontextRepo.saveAuthorized(
+            newPersonenkontext,
+            permissions,
         );
-        if (!createdPk) {
+
+        if (!saveResult.ok) {
+            if (saveResult.error instanceof OrganisationMatchesRollenartError) {
+                throw saveResult.error;
+            }
+
             throw SchulConnexErrorMapper.mapSchulConnexErrorToHttpException(
-                SchulConnexErrorMapper.mapDomainErrorToSchulConnexError(new EntityCouldNotBeCreated('Personkontext')),
+                SchulConnexErrorMapper.mapDomainErrorToSchulConnexError(saveResult.error),
             );
         }
-        return new DBiamPersonenkontextResponse(createdPk);
+
+        return new DBiamPersonenkontextResponse(saveResult.value);
     }
 }
