4feba6388bfc95b6cf7c6743ea20eb233c030a99
Marvin Rode <marvin.rode@capgemini.com>
2024-09-03 16:01:38 +0200

    Add tests for specifications

---
+++ src/modules/personenkontext/specification/nur-lehr-und-lern-an-klasse.spec.ts
@@ -0,0 +1,73 @@
+import { DeepMocked, createMock } from '@golevelup/ts-jest';
+import { DoFactory } from '../../../../test/utils/do-factory.js';
+import { OrganisationsTyp } from '../../organisation/domain/organisation.enums.js';
+import { OrganisationRepository } from '../../organisation/persistence/organisation.repository.js';
+import { RollenArt } from '../../rolle/domain/rolle.enums.js';
+import { RolleRepo } from '../../rolle/repo/rolle.repo.js';
+import { Personenkontext } from '../domain/personenkontext.js';
+import { NurLehrUndLernAnKlasse } from './nur-lehr-und-lern-an-klasse.js';
+
+describe('NurLehrUndLernAnKlasse specification', () => {
+    const organisationRepoMock: DeepMocked<OrganisationRepository> = createMock();
+    const rolleRepoMock: DeepMocked<RolleRepo> = createMock();
+
+    afterEach(() => {
+        jest.resetAllMocks();
+    });
+
+    it('should return true, if organisation is klasse and rolle is LEHR', async () => {
+        const specification: NurLehrUndLernAnKlasse = new NurLehrUndLernAnKlasse(organisationRepoMock, rolleRepoMock);
+        organisationRepoMock.findById.mockResolvedValueOnce(
+            DoFactory.createOrganisation(true, { typ: OrganisationsTyp.KLASSE }),
+        );
+        rolleRepoMock.findById.mockResolvedValueOnce(DoFactory.createRolle(true, { rollenart: RollenArt.LEHR }));
+
+        const pk: Personenkontext<boolean> = DoFactory.createPersonenkontext(false);
+
+        await expect(specification.isSatisfiedBy(pk)).resolves.toBe(true);
+    });
+
+    it('should return true, if organisation is klasse and rolle is LERN', async () => {
+        const specification: NurLehrUndLernAnKlasse = new NurLehrUndLernAnKlasse(organisationRepoMock, rolleRepoMock);
+        organisationRepoMock.findById.mockResolvedValueOnce(
+            DoFactory.createOrganisation(true, { typ: OrganisationsTyp.KLASSE }),
+        );
+        rolleRepoMock.findById.mockResolvedValueOnce(DoFactory.createRolle(true, { rollenart: RollenArt.LEHR }));
+
+        const pk: Personenkontext<boolean> = DoFactory.createPersonenkontext(false);
+
+        await expect(specification.isSatisfiedBy(pk)).resolves.toBe(true);
+    });
+
+    it('should return true, if organisation is not KLASSE', async () => {
+        const specification: NurLehrUndLernAnKlasse = new NurLehrUndLernAnKlasse(organisationRepoMock, rolleRepoMock);
+        organisationRepoMock.findById.mockResolvedValueOnce(
+            DoFactory.createOrganisation(true, { typ: OrganisationsTyp.SCHULE }),
+        );
+
+        const pk: Personenkontext<boolean> = DoFactory.createPersonenkontext(false);
+
+        await expect(specification.isSatisfiedBy(pk)).resolves.toBe(true);
+    });
+
+    it('should return false, if organisation is not found', async () => {
+        const specification: NurLehrUndLernAnKlasse = new NurLehrUndLernAnKlasse(organisationRepoMock, rolleRepoMock);
+        organisationRepoMock.findById.mockResolvedValueOnce(undefined);
+
+        const pk: Personenkontext<boolean> = DoFactory.createPersonenkontext(false);
+
+        await expect(specification.isSatisfiedBy(pk)).resolves.toBe(false);
+    });
+
+    it('should return true, if rolle is not found', async () => {
+        const specification: NurLehrUndLernAnKlasse = new NurLehrUndLernAnKlasse(organisationRepoMock, rolleRepoMock);
+        organisationRepoMock.findById.mockResolvedValueOnce(
+            DoFactory.createOrganisation(true, { typ: OrganisationsTyp.KLASSE }),
+        );
+        rolleRepoMock.findById.mockResolvedValueOnce(undefined);
+
+        const pk: Personenkontext<boolean> = DoFactory.createPersonenkontext(false);
+
+        await expect(specification.isSatisfiedBy(pk)).resolves.toBe(false);
+    });
+});
