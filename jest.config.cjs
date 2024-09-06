40141c2701a77ba79c75812b110a327f4fa489fa
DeFi3298 <dennis.fischer@capgemini.com>
2024-09-06 13:16:35 +0200

    not intended commenting out of jest.config.cjs

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,54 +1,54 @@
-// /** @type {import('ts-jest').JestConfigWithTsJest} */
-// const commonConfig = {
-//     testEnvironment: 'node',
-//     rootDir: './src',
-//     preset: 'ts-jest/presets/default-esm',
-//     moduleNameMapper: {
-//         '^(\\.{1,2}/.*)\\.js$': '$1',
-//         "^lodash-es$": "lodash",
-//     },
-//     transform: {
-//         '^.+\\.tsx?$': [
-//             'ts-jest',
-//             {
-//                 useESM: true,
-//             },
-//         ],
-//     },
-// };
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+const commonConfig = {
+    testEnvironment: 'node',
+    rootDir: './src',
+    preset: 'ts-jest/presets/default-esm',
+    moduleNameMapper: {
+        '^(\\.{1,2}/.*)\\.js$': '$1',
+        '^lodash-es$': 'lodash',
+    },
+    transform: {
+        '^.+\\.tsx?$': [
+            'ts-jest',
+            {
+                useESM: true,
+            },
+        ],
+    },
+};
 
-// /** @type {import('jest').Config} */
-// module.exports = {
-//     projects: [
-//         {
-//             displayName: 'unit',
-//             testRegex: '.*\\.spec\\.ts$',
-//             ...commonConfig,
-//         },
-//         {
-//             displayName: 'integration',
-//             testRegex: '/.*\\.integration-spec\\.ts$',
-//             ...commonConfig,
-//         },
-//     ],
-//     coverageDirectory: './coverage',
-//     coverageProvider: 'babel',
-//     collectCoverageFrom: [
-//         '**/*.ts',
-//         '!**/*.d.ts',
-//         '!**/*.spec.ts',
-//         '!**/*.integration-spec.ts',
-//         '!**/index.ts',
-//         '!**/main.ts',
-//         '!**/*.types.ts',
-//     ],
-//     coverageThreshold: {
-//         global: {
-//             statements: 100,
-//             branches: 100,
-//             functions: 100,
-//             lines: 100,
-//         },
-//     },
-//     testTimeout: 1000000,
-// };
+/** @type {import('jest').Config} */
+module.exports = {
+    projects: [
+        {
+            displayName: 'unit',
+            testRegex: '.*\\.spec\\.ts$',
+            ...commonConfig,
+        },
+        {
+            displayName: 'integration',
+            testRegex: '/.*\\.integration-spec\\.ts$',
+            ...commonConfig,
+        },
+    ],
+    coverageDirectory: './coverage',
+    coverageProvider: 'babel',
+    collectCoverageFrom: [
+        '**/*.ts',
+        '!**/*.d.ts',
+        '!**/*.spec.ts',
+        '!**/*.integration-spec.ts',
+        '!**/index.ts',
+        '!**/main.ts',
+        '!**/*.types.ts',
+    ],
+    coverageThreshold: {
+        global: {
+            statements: 100,
+            branches: 100,
+            functions: 100,
+            lines: 100,
+        },
+    },
+    testTimeout: 1000000,
+};
