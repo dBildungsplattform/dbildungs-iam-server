b62782c2c20addd0226e609b225d509dcf942193
Youssef Bouchara <101522419+YoussefBouch@users.noreply.github.com>
2024-09-10 16:23:39 +0200

    SPSH-1024 (#643)
    * Total var shows the number of rows in the DB before applying the Limit
    
    * new property in header to show total count of paginated items.
    
    * Tests and Lint
    
    * Fix duplication

--- src/shared/paging/paged.response.ts
+++ src/shared/paging/paged.response.ts
@@ -12,6 +12,9 @@ export class PagedResponse<T> {
     @Exclude()
     public readonly limit: number;
 
+    @Exclude()
+    public readonly pageTotal?: number;
+
     @ApiProperty()
     public readonly items: T[];
 
@@ -20,5 +23,6 @@ export class PagedResponse<T> {
         this.offset = page.offset;
         this.limit = page.limit;
         this.items = page.items;
+        this.pageTotal = page.pageTotal;
     }
 }
