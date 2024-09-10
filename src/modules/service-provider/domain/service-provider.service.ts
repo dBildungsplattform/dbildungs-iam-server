f598189a6a4f3d61f16d1f7ffa4124ce0707a163
Yannick Clausen <yannick.clausen@capgemini.com>
2024-09-10 10:51:30 +0200

    add test for sp-service

--- src/modules/service-provider/domain/service-provider.service.ts
+++ src/modules/service-provider/domain/service-provider.service.ts
@@ -15,14 +15,15 @@ export class ServiceProviderService {
         const serviceProviders: Map<string, ServiceProvider<true>> = new Map();
         for (const rolleId of rolleIds) {
             const rolle: Option<Rolle<true>> = await this.rolleRepo.findById(rolleId);
-            if (rolle) {
-                for (const serviceProviderId of rolle.serviceProviderIds) {
-                    if (serviceProviders.has(serviceProviderId)) continue;
-                    const serviceProvider: Option<ServiceProvider<true>> =
-                        await this.serviceProviderRepo.findById(serviceProviderId);
-                    if (serviceProvider) {
-                        serviceProviders.set(serviceProviderId, serviceProvider);
-                    }
+            if (!rolle) continue;
+
+            for (const serviceProviderId of rolle.serviceProviderIds) {
+                if (serviceProviders.has(serviceProviderId)) continue;
+
+                const serviceProvider: Option<ServiceProvider<true>> =
+                    await this.serviceProviderRepo.findById(serviceProviderId);
+                if (serviceProvider) {
+                    serviceProviders.set(serviceProviderId, serviceProvider);
                 }
             }
         }
