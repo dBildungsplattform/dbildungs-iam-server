91777cfe6e8423dd9e290141311c6477c78d5fd0
Marvin Rode <marvin.rode@capgemini.com>
2024-09-18 15:19:48 +0200

    Fix last test

--- src/modules/service-provider/domain/service-provider.factory.spec.ts
+++ src/modules/service-provider/domain/service-provider.factory.spec.ts
@@ -1,7 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ServiceProviderFactory } from './service-provider.factory.js';
 import { faker } from '@faker-js/faker';
-import { ServiceProviderKategorie, ServiceProviderTarget } from './service-provider.enum.js';
+import { ServiceProviderKategorie, ServiceProviderSystem, ServiceProviderTarget } from './service-provider.enum.js';
 import { ServiceProvider } from './service-provider.js';
 
 describe('ServiceProviderFactory', () => {
@@ -37,6 +37,7 @@ describe('ServiceProviderFactory', () => {
                 const id: string = faker.string.uuid();
                 const keycloakGroup: string = faker.string.alpha();
                 const keycloakRole: string = faker.string.alpha();
+                const system: ServiceProviderSystem = faker.helpers.enumValue(ServiceProviderSystem);
                 const example: ServiceProvider<true> = {
                     id: id,
                     createdAt: created,
@@ -50,6 +51,7 @@ describe('ServiceProviderFactory', () => {
                     logoMimeType: undefined,
                     keycloakGroup: keycloakGroup,
                     keycloakRole: keycloakRole,
+                    system: system,
                 };
                 const serviceProvider: ServiceProvider<true> = sut.construct(
                     id,
@@ -64,6 +66,7 @@ describe('ServiceProviderFactory', () => {
                     undefined,
                     keycloakGroup,
                     keycloakRole,
+                    system,
                 );
 
                 expect(serviceProvider).toEqual(example);
