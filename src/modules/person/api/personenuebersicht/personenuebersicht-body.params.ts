00483d28a963eff79b0c6229f97722e69b0e4f29
Nael-Alshowaikh <153004766+Nael-Alshowaikh@users.noreply.github.com>
2024-09-19 17:19:37 +0200

    SPSH-863: removed the other ones

--- src/modules/person/api/personenuebersicht/personenuebersicht-body.params.ts
+++ src/modules/person/api/personenuebersicht/personenuebersicht-body.params.ts
@@ -1,31 +1,9 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsEnum, IsOptional, IsString } from 'class-validator';
+import { IsString } from 'class-validator';
 import { PagedQueryParams } from '../../../../shared/paging/index.js';
 import { PersonID } from '../../../../shared/types/aggregate-ids.types.js';
-import { ScopeOrder } from '../../../../shared/persistence/scope.enums.js';
-import { SortFieldPersonenuebersicht } from '../../domain/person.enums.js';
 
 export class PersonenuebersichtBodyParams extends PagedQueryParams {
-    @IsOptional()
-    @IsEnum(ScopeOrder)
-    @ApiProperty({
-        enum: ScopeOrder,
-        required: false,
-        nullable: true,
-        description: 'Order to sort by.',
-    })
-    public readonly sortOrder?: ScopeOrder;
-
-    @IsOptional()
-    @IsEnum(SortFieldPersonenuebersicht)
-    @ApiProperty({
-        enum: SortFieldPersonenuebersicht,
-        required: false,
-        nullable: true,
-        description: 'Field to sort by.',
-    })
-    public readonly sortField?: SortFieldPersonenuebersicht;
-
     @ApiProperty({
         description: 'An array of IDs for the persons.',
         type: [String],
