c54150da81ecd99eaaaff35529fa880f275e7b22
DPDS93CT <cornelius.tegtmeyer@dataport.de>
2024-09-20 11:27:48 +0200

    fix migrations

--- migrations/Migration20240920092347.ts
+++ migrations/Migration20240920092347.ts
@@ -1,6 +1,6 @@
 import { Migration } from '@mikro-orm/migrations';
 
-export class Migration20240920091808 extends Migration {
+export class Migration20240920092347 extends Migration {
 
   async up(): Promise<void> {
     this.addSql('alter table "organisation" add column "email_domain" varchar(255) null;');
