0454bc57aa849ee917ea72a5126d3a04b61134ea
DPDS93CT <cornelius.tegtmeyer@dataport.de>
2024-10-08 08:10:47 +0200

    adjust mirgration for email-address-status-enum

--- migrations/Migration20240924145510.ts
+++ migrations/Migration20240924145510.ts
@@ -4,10 +4,14 @@ export class Migration20240924145510 extends Migration {
 
   async up(): Promise<void> {
     this.addSql('alter table "email_address" add column "ox_user_id" varchar(255) null;');
+
+    this.addSql('alter type "email_address_status_enum" add value if not exists \'FAILED\';');
   }
 
   override async down(): Promise<void> {
     this.addSql('alter table "email_address" drop column "ox_user_id";');
+
+    //removal of single values of enum-type is not possible
   }
 
 }
