7d147ec1d87f36da4f4b2be2deb607a4c25e9451
Marvin Rode <marvin.rode@capgemini.com>
2024-10-09 10:07:30 +0200

    Remove data-migration

--- migrations/Migration20241008115945.ts
+++ migrations/Migration20241008115945.ts
@@ -1,9 +1,15 @@
 import { Migration } from '@mikro-orm/migrations';
 
-// This migration is split into two (Migration20241008115945, Migration20241008115946), because each migration is a
-// transaction and changes to enums need to be committed before use.
 export class Migration20241008115945 extends Migration {
     public async up(): Promise<void> {
         this.addSql(`ALTER TYPE "rollen_system_recht_enum" ADD VALUE IF NOT EXISTS 'PERSON_SYNCHRONISIEREN';`);
     }
+
+    // No down-migration, because it would be unnecessarily complicated
+    // Would need to be:
+    // - Rename old enum type
+    // - create new enum type
+    // - Change all databases depending on the type
+    // - Delete old enum type
+    // public async down(): Promise<void> {}
 }
