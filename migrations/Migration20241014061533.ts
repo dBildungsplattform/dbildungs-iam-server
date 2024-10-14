65facc0803b29647fcba8bc98458cb853729b703
Phael Mouko <haby-phael.mouko@capgemini.com>
2024-10-14 08:30:48 +0200

    SPSH-1078: Fixed migration issues after merge with main

--- migrations/Migration20241014061533.ts
+++ migrations/Migration20241014061533.ts
@@ -1,9 +1,11 @@
 import { Migration } from '@mikro-orm/migrations';
 
-export class Migration20241009191750 extends Migration {
+export class Migration20241014061533 extends Migration {
 
   async up(): Promise<void> {
     this.addSql('create table "importdataitem" ("id" uuid not null, "created_at" timestamptz not null, "updated_at" timestamptz not null, "importvorgang_id" uuid not null, "familienname" varchar(255) not null, "vorname" varchar(255) not null, "klasse" varchar(255) null, "personalnummer" varchar(255) null, constraint "importdataitem_pkey" primary key ("id"));');
+
+    this.addSql('alter type "email_address_status_enum" add value if not exists \'FAILED\';');
 
     this.addSql('alter type "rollen_system_recht_enum" add value if not exists \'IMPORT_DURCHFUEHREN\';');
   }
