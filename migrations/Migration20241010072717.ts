b17c20651dd34a7c038bef11532a559c3cc15d5f
Timo K <timo.kleist@capgemini.com>
2024-10-15 15:57:43 +0200

    remove setting kcid in migration

--- migrations/Migration20241010072717.ts
+++ migrations/Migration20241010072717.ts
@@ -4,8 +4,5 @@ export class Migration20241010072717 extends Migration {
     public async up(): Promise<void> {
         this.addSql('alter type "rollen_system_recht_enum" add value if not exists \'CRON_DURCHFUEHREN\';');
         this.addSql("update rolle set ist_technisch = true where name = 'Technischer Nutzer';");
-        this.addSql(
-            "update person set keycloak_user_id='7baf74aa-565f-4cfc-9d5a-8f1a3f374dc9' where referrer = 'technical';",
-        );
     }
 }
