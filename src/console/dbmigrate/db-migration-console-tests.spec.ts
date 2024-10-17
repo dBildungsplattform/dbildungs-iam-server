acdcf79927108b578e3a4f6ed03a2eeb3aa35ea4
Timo K <timo.kleist@capgemini.com>
2024-10-17 11:29:04 +0200

    mock migrator method

--- src/console/dbmigrate/db-migration-console-tests.spec.ts
+++ src/console/dbmigrate/db-migration-console-tests.spec.ts
@@ -11,7 +11,6 @@ import { DbInitMigrationConsole } from './db-init-migration.console.js';
 import { DbApplyMigrationConsole, MigrationType } from './db-apply-migration.console.js';
 import { DbCreateMigrationConsole } from './db-create-migration.console.js';
 import { Migrator } from '@mikro-orm/migrations';
-import { ClassLogger } from '../../core/logging/class-logger.js';
 
 describe('DbMigrateConsole', () => {
     let module: TestingModule;
@@ -19,44 +18,19 @@ describe('DbMigrateConsole', () => {
     let dbMigrationCreate: DbCreateMigrationConsole;
     let dbMigrationApply: DbApplyMigrationConsole;
     let orm: MikroORM;
-    let migrator: jest.Mocked<Migrator>;
-    let logger: jest.Mocked<ClassLogger>;
+    let migrator: Migrator;
 
     beforeAll(async () => {
-        migrator = {
-            getPendingMigrations: jest.fn(),
-            up: jest.fn(),
-        } as unknown as jest.Mocked<Migrator>;
-
-        const ormMock: Partial<MikroORM> = {
-            getMigrator: jest.fn().mockReturnValue(migrator),
-        };
-
-        logger = {
-            info: jest.fn(),
-        } as unknown as jest.Mocked<ClassLogger>;
-
         module = await Test.createTestingModule({
             imports: [ConfigTestModule, DatabaseTestModule.forRoot({ isDatabaseRequired: true }), LoggingTestModule],
-            providers: [
-                DbInitMigrationConsole,
-                DbCreateMigrationConsole,
-                DbApplyMigrationConsole,
-                {
-                    provide: MikroORM,
-                    useValue: ormMock,
-                },
-                {
-                    provide: ClassLogger,
-                    useValue: logger,
-                },
-            ],
+            providers: [DbInitMigrationConsole, DbCreateMigrationConsole, DbApplyMigrationConsole],
         }).compile();
         dbMigrationInit = module.get(DbInitMigrationConsole);
         dbMigrationCreate = module.get(DbCreateMigrationConsole);
         dbMigrationApply = module.get(DbApplyMigrationConsole);
-
         orm = module.get(MikroORM);
+        migrator = orm.getMigrator();
+
         fs.rmSync('test-migrations', { recursive: true, force: true });
     }, 2 * DEFAULT_TIMEOUT_FOR_TESTCONTAINERS);
 
@@ -86,36 +60,41 @@ describe('DbMigrateConsole', () => {
 
         describe('when migration-apply is executed', () => {
             it('should not throw an error', async () => {
+                jest.spyOn(migrator, 'getPendingMigrations').mockReturnValueOnce(Promise.resolve([]));
                 await expect(dbMigrationApply.run([])).resolves.not.toThrow();
             });
         });
     });
 
     describe('DbApplyMigrationConsole', () => {
+        beforeEach(() => {
+            jest.spyOn(migrator, 'up').mockResolvedValueOnce(Promise.resolve([]));
+        });
+
         it('should apply all pending migrations', async () => {
-            const migrations: UmzugMigration[] = [{ name: '20210101_initialS.ts' }, { name: '20210202_dataD.ts' }];
-            migrator.getPendingMigrations.mockResolvedValue(migrations);
+            const migrations: UmzugMigration[] = [{ name: '20210101_initialS' }, { name: '20210202_dataD' }];
+            jest.spyOn(migrator, 'getPendingMigrations').mockReturnValueOnce(Promise.resolve(migrations));
             await dbMigrationApply.run([]);
             expect(migrator.up).toHaveBeenCalledWith(migrations.map((m: UmzugMigration) => m.name));
         });
 
         it('should filter and apply only structural migrations', async () => {
-            const migrations: UmzugMigration[] = [{ name: '20210101_initialS.ts' }, { name: '20210202_dataD.ts' }];
-            migrator.getPendingMigrations.mockResolvedValue(migrations);
+            const migrations: UmzugMigration[] = [{ name: '20210101_initialS' }, { name: '20210202_dataD' }];
+            jest.spyOn(migrator, 'getPendingMigrations').mockReturnValueOnce(Promise.resolve(migrations));
             await dbMigrationApply.run([], { migration: 'structural' });
-            expect(migrator.up).toHaveBeenCalledWith(['20210101_initialS.ts']);
+            expect(migrator.up).toHaveBeenCalledWith(['20210101_initialS']);
         });
 
         it('should filter and apply only data migrations', async () => {
-            const migrations: UmzugMigration[] = [{ name: '20210101_initialS.ts' }, { name: '20210202_dataD.ts' }];
-            migrator.getPendingMigrations.mockResolvedValue(migrations);
+            const migrations: UmzugMigration[] = [{ name: '20210101_initialS' }, { name: '20210202_dataD' }];
+            jest.spyOn(migrator, 'getPendingMigrations').mockReturnValueOnce(Promise.resolve(migrations));
             await dbMigrationApply.run([], { migration: 'data' });
-            expect(migrator.up).toHaveBeenCalledWith(['20210202_dataD.ts']);
+            expect(migrator.up).toHaveBeenCalledWith(['20210202_dataD']);
         });
 
         it('should throw an error if any migration does not end with S or D', async () => {
-            const migrations: UmzugMigration[] = [{ name: '20210101_initial.ts' }];
-            migrator.getPendingMigrations.mockResolvedValue(migrations);
+            const migrations: UmzugMigration[] = [{ name: '20210101_initial' }];
+            jest.spyOn(migrator, 'getPendingMigrations').mockReturnValueOnce(Promise.resolve(migrations));
             await expect(dbMigrationApply.run([])).rejects.toThrow('Not all migrations end with a S or D');
         });
     });
