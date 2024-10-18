3c4e6c4b150c0816fd03b7145ec631526d87f3be
Marvin Rode <marvin.rode@capgemini.com>
2024-10-18 13:19:37 +0200

    Add new client and keypair for local development

---
+++ src/modules/migration/api/migration.controller.ts
@@ -0,0 +1,27 @@
+import { Body, Controller, Put } from '@nestjs/common';
+import { ApiExcludeController } from '@nestjs/swagger';
+
+import { DomainError } from '../../../shared/error/domain.error.js';
+import { Public } from '../../authentication/api/public.decorator.js';
+import { KeycloakClientService } from '../../keycloak-administration/domain/keycloak-client.service.js';
+import { UpdateKeycloakClientParams } from './update-client.params.js';
+
+// TODO: Errors, Swagger, Ingress
+@Controller({ path: 'migration' })
+@ApiExcludeController()
+export class MigrationController {
+    public constructor(private readonly keycloakClientService: KeycloakClientService) {}
+
+    @Put('kc-client')
+    @Public()
+    public async updateKeycloakClient(@Body() body: UpdateKeycloakClientParams): Promise<void> {
+        const result: Result<unknown, DomainError> = await this.keycloakClientService.updateClient(
+            body.id,
+            body.payload,
+        );
+
+        if (!result.ok) {
+            console.log(result.error);
+        }
+    }
+}
