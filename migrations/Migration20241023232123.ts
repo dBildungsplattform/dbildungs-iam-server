453aa840f2da2cadcd946cfc52b03ba4cfd07def
godtierbatuhan <boucharay1001@gmail.com>
2024-10-24 01:22:13 +0200

    New migration

---
+++ migrations/Migration20241023232123.ts
@@ -0,0 +1,11 @@
+import { Migration } from '@mikro-orm/migrations';
+
+export class Migration20241023232123 extends Migration {
+
+  async up(): Promise<void> {
+    this.addSql('alter type "rollen_system_recht_enum" add value if not exists \'CRON_DURCHFUEHREN\';');
+
+    this.addSql('alter type "rollen_system_recht_enum" add value if not exists \'PERSONEN_ANLEGEN\';');
+  }
+
+}
