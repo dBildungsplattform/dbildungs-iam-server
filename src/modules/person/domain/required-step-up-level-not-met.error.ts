e2cd7edfa429aeef07a290a056c41b26235982e5
Alexander Ungefug <82446024+AlexanderUngefug@users.noreply.github.com>
2024-11-07 13:26:38 +0100

    add details to constructor

--- src/modules/person/domain/required-step-up-level-not-met.error.ts
+++ src/modules/person/domain/required-step-up-level-not-met.error.ts
@@ -1,7 +1,7 @@
 import { AuthenticationDomainError } from '../../authentication/domain/authentication-domain.error.js';
 
 export class RequiredStepUpLevelNotMetError extends AuthenticationDomainError {
-    public constructor() {
-        super(`The action could not be perfomed, bequase the required step up level was not met`, undefined, undefined);
+    public constructor(details?: unknown[] | Record<string, undefined>) {
+        super(`The action could not be perfomed, bequase the required step up level was not met`, undefined, details);
     }
 }
