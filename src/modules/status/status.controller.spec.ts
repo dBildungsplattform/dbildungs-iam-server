316eea691b1f9c04879489e6c844e8c27bc37611
Timo K <timo.kleist@capgemini.com>
2024-11-18 11:04:33 +0100

    add configmodule to test

--- src/modules/status/status.controller.spec.ts
+++ src/modules/status/status.controller.spec.ts
@@ -1,11 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { StatusController } from './status.controller.js';
+import { ConfigTestModule } from '../../../test/utils/index.js';
 
 describe('StatusController', () => {
     let controller: StatusController;
 
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
+            imports: [ConfigTestModule],
             controllers: [StatusController],
         }).compile();
 
