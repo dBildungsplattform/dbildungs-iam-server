4ad6cb5a4bd2b7803aba79244f107943f01294ec
Alexander Ungefug <82446024+AlexanderUngefug@users.noreply.github.com>
2024-11-26 09:28:48 +0100

    fixing tests

--- src/modules/metrics/metrics.controller.spec.ts
+++ src/modules/metrics/metrics.controller.spec.ts
@@ -2,16 +2,11 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { MetricsController } from './metrics.controller.js';
 import { Registry } from 'prom-client';
 import { DBiamPersonenkontextRepo } from '../personenkontext/persistence/dbiam-personenkontext.repo.js';
-import { ScopeOperator } from '../../shared/persistence/scope.enums.js';
-import { PersonenkontextScope } from '../personenkontext/persistence/personenkontext.scope.js';
-import { RollenArt } from '../rolle/domain/rolle.enums.js';
 import { ReporterService } from './reporter/reporter.service.js';
-import { Personenkontext } from '../personenkontext/domain/personenkontext.js';
 
 describe('MetricsController', () => {
     let controller: MetricsController;
     let registry: Registry;
-    let dBiamPersonenkontextRepo: DBiamPersonenkontextRepo;
     let reporterService: ReporterService;
 
     beforeEach(async () => {
@@ -28,7 +23,7 @@ describe('MetricsController', () => {
                 {
                     provide: DBiamPersonenkontextRepo,
                     useValue: {
-                        findBy: jest.fn().mockResolvedValue([[], 0]),
+                        getPersonCountByRole: jest.fn().mockResolvedValue(10),
                     },
                 },
                 {
@@ -42,7 +37,6 @@ describe('MetricsController', () => {
 
         controller = module.get<MetricsController>(MetricsController);
         registry = module.get<Registry>(Registry);
-        dBiamPersonenkontextRepo = module.get<DBiamPersonenkontextRepo>(DBiamPersonenkontextRepo);
         reporterService = module.get<ReporterService>(ReporterService);
     });
 
@@ -52,39 +46,10 @@ describe('MetricsController', () => {
         expect(registry.metrics).toHaveBeenCalled();
     });
 
-    it('should call findBy with correct scope', async () => {
-        const scope: PersonenkontextScope = new PersonenkontextScope()
-            .setScopeWhereOperator(ScopeOperator.AND)
-            .findByRollen([RollenArt.LEHR]);
-
-        await controller.getMetrics();
-        expect(dBiamPersonenkontextRepo.findBy).toHaveBeenCalledWith(scope);
-    });
-
-    it('should handle errors correctly', async () => {
-        jest.spyOn(dBiamPersonenkontextRepo, 'findBy').mockRejectedValue(new Error('Database error'));
-
-        await expect(controller.getMetrics()).rejects.toThrow('Database error');
-    });
-
     it('should call gauge with correct metrics and count', async () => {
-        const mockPersonenkontexte: { personId: string }[] = [{ personId: '1' }, { personId: '2' }, { personId: '1' }];
-
-        jest.spyOn(dBiamPersonenkontextRepo, 'findBy').mockResolvedValue([
-            mockPersonenkontexte as Personenkontext<true>[],
-            mockPersonenkontexte.length,
-        ]);
-
         await controller.getMetrics();
-
-        expect(reporterService.gauge).toHaveBeenCalledWith('number_of_teachers', 2, { school: 'all' });
-        expect(reporterService.gauge).toHaveBeenCalledWith('number_of_students', 2, { school: 'all' });
-        expect(reporterService.gauge).toHaveBeenCalledWith('number_of_admins', 2, { school: 'all' });
-    });
-
-    it('should handle errors correctly', async () => {
-        jest.spyOn(dBiamPersonenkontextRepo, 'findBy').mockRejectedValue(new Error('Database error'));
-
-        await expect(controller.getMetrics()).rejects.toThrow('Database error');
+        expect(reporterService.gauge).toHaveBeenCalledWith('number_of_teachers', 10, { school: 'all' });
+        expect(reporterService.gauge).toHaveBeenCalledWith('number_of_students', 10, { school: 'all' });
+        expect(reporterService.gauge).toHaveBeenCalledWith('number_of_admins', 10, { school: 'all' });
     });
 });
