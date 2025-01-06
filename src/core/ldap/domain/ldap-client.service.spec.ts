3d252783c61549a6385e3b87bbbd2187614dceed
Caspar Neumann <caspar.neumann@capgemini.com>
2025-01-06 16:18:13 +0100

    Fix Ldap Client Tests

--- src/core/ldap/domain/ldap-client.service.spec.ts
+++ src/core/ldap/domain/ldap-client.service.spec.ts
@@ -129,11 +129,20 @@ describe('LDAP Client Service', () => {
         await DatabaseTestModule.clearDatabase(orm);
         await orm.close();
         await app.close();
+        jest.clearAllTimers();
     });
 
     beforeEach(async () => {
         jest.resetAllMocks();
         await DatabaseTestModule.clearDatabase(orm);
+
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-explicit-any
+        jest.spyOn(ldapClientService as any, 'executeWithRetry').mockImplementation((...args: unknown[]) => {
+            //Needed To globally mock the private executeWithRetry function (otherwise test run to long)
+            // eslint-disable-next-line @typescript-eslint/no-explicit-any
+            const func: () => Promise<Result<any>> = args[0] as () => Promise<Result<any>>;
+            return func();
+        });
     });
 
     it('should be defined', () => {
@@ -708,29 +717,61 @@ describe('LDAP Client Service', () => {
         });
     });
 
-    //Demonstrated using isLehrerExisting function
     describe('executeWithRetry', () => {
-        it('when operation fails it should automatically retry the operation', async () => {
+        beforeEach(() => {
+            jest.restoreAllMocks(); //Needed To Reset the global executeWithRetry Mock
+        });
+
+        it('when operation succeeds should return value', async () => {
             ldapClientMock.getClient.mockImplementation(() => {
                 clientMock.bind.mockResolvedValue();
-                clientMock.search.mockRejectedValue(new Error()); //mock existsLehrer
+                clientMock.search.mockResolvedValue({ searchEntries: [] } as unknown as SearchResult);
 
                 return clientMock;
             });
-            const testLehrer: PersonData = {
-                id: faker.string.uuid(),
-                vorname: faker.person.firstName(),
-                familienname: faker.person.lastName(),
-                referrer: faker.lorem.word(),
-                ldapEntryUUID: faker.string.uuid(),
-            };
             const result: Result<boolean> = await ldapClientService.isLehrerExisting(
-                testLehrer.referrer!,
+                faker.lorem.word(),
+                'schule-sh.de',
+            );
+
+            expect(result.ok).toBeTruthy();
+            expect(clientMock.bind).toHaveBeenCalledTimes(1);
+            expect(loggerMock.warning).not.toHaveBeenCalledWith(expect.stringContaining('Attempt 1 failed'));
+        });
+
+        it('when operation fails and throws Error it should automatically retry the operation', async () => {
+            ldapClientMock.getClient.mockImplementation(() => {
+                clientMock.bind.mockResolvedValue();
+                clientMock.search.mockRejectedValue(new Error());
+
+                return clientMock;
+            });
+            const result: Result<boolean> = await ldapClientService.isLehrerExisting(
+                faker.lorem.word(),
                 'schule-sh.de',
             );
 
             expect(result.ok).toBeFalsy();
             expect(clientMock.bind).toHaveBeenCalledTimes(3);
+            expect(loggerMock.warning).toHaveBeenCalledWith(expect.stringContaining('Attempt 1 failed'));
+            expect(loggerMock.warning).toHaveBeenCalledWith(expect.stringContaining('Attempt 2 failed'));
+            expect(loggerMock.warning).toHaveBeenCalledWith(expect.stringContaining('Attempt 3 failed'));
+        });
+
+        it('when operation fails and returns Error it should automatically retry the operation', async () => {
+            ldapClientMock.getClient.mockImplementation(() => {
+                clientMock.bind.mockResolvedValue();
+                clientMock.search.mockResolvedValue({} as SearchResult);
+                return clientMock;
+            });
+            const result: Result<PersonID> = await ldapClientService.changeEmailAddressByPersonId(
+                faker.string.uuid(),
+                faker.internet.userName(),
+                faker.internet.email(),
+            );
+
+            expect(result.ok).toBeFalsy();
+            expect(clientMock.bind).toHaveBeenCalledTimes(0);
             expect(loggerMock.warning).toHaveBeenCalledWith(expect.stringContaining('Attempt 1 failed'));
             expect(loggerMock.warning).toHaveBeenCalledWith(expect.stringContaining('Attempt 2 failed'));
             expect(loggerMock.warning).toHaveBeenCalledWith(expect.stringContaining('Attempt 3 failed'));
