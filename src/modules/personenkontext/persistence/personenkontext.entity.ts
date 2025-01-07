d92ac995a0436fa7d95c501ecfedb4ff252dbcfa
Marvin Rode <marvin.rode@capgemini.com>
2025-01-07 09:11:23 +0100

    Add/change indices

--- src/modules/personenkontext/persistence/personenkontext.entity.ts
+++ src/modules/personenkontext/persistence/personenkontext.entity.ts
@@ -17,13 +17,13 @@ export class PersonenkontextEntity extends TimestampedEntity {
         nullable: false,
         entity: () => PersonEntity,
     })
-    @Index({ name: 'personenkontext_person_id_index' })
+    @Index({ name: 'personenkontext_person_id_index', type: 'hash' })
     public personId!: Ref<PersonEntity>;
 
     // TODO EW-636: get from access_token, see SchulConneX (Version 1.003.003.000) page 91
     @AutoMap()
     @Property({ columnType: 'uuid', nullable: true })
-    @Index({ name: 'personenkontext_organisation_id_index' })
+    @Index({ name: 'personenkontext_organisation_id_index', type: 'hash' })
     public organisationId!: string;
 
     @ManyToOne({
@@ -33,6 +33,7 @@ export class PersonenkontextEntity extends TimestampedEntity {
         nullable: false,
         entity: () => RolleEntity,
     })
+    @Index({ name: 'personenkontext_rolle_id_index', type: 'hash' })
     public rolleId!: Ref<RolleEntity>;
 
     @AutoMap()
