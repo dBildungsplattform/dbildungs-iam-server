eff9cf0af7a8b2cd942c45c2b9376be45dd6dbbb
Yannick Clausen <yannick.clausen@capgemini.com>
2025-01-13 12:35:37 +0100

    fix linter error

--- src/modules/email/domain/email-event-handler.spec.ts
+++ src/modules/email/domain/email-event-handler.spec.ts
@@ -1141,6 +1141,7 @@ describe('EmailEventHandler', () => {
 
         beforeEach(() => {
             fakeRolleId = faker.string.uuid();
+            fakePersonId = faker.string.uuid();
             personenkontexte = [
                 createMock<Personenkontext<true>>({ personId: fakePersonId }),
                 createMock<Personenkontext<true>>({ personId: fakePersonId }),
