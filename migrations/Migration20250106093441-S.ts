baf06ffc00a3e5bca25f84dfc493bdfed6c90532
godtierbatuhan <boucharay1001@gmail.com>
2025-01-16 17:34:17 +0100

    Added D migration for data

--- migrations/Migration20250106093441-S.ts
+++ migrations/Migration20250106093441-S.ts
@@ -1,14 +1,12 @@
 import { Migration } from '@mikro-orm/migrations';
 
-export class Migration20250104001707 extends Migration {
+export class Migration20250106093441 extends Migration {
     public async up(): Promise<void> {
         // Create the enum type
         this.addSql("create type \"import_data_item_status_enum\" as enum ('FAILED', 'SUCCESS', 'PENDING');");
 
         // Add the column with the enum type (nullable for now)
-        this.addSql(
-            'alter table "importdataitem" add column "status" "import_data_item_status_enum" not null default \'PENDING\';',
-        );
+        this.addSql('alter table "importdataitem" add column "status" "import_data_item_status_enum";');
     }
 
     public override async down(): Promise<void> {
