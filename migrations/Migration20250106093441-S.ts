7bcf380924f7c52aef131ddbf4d09d538dc50f2e
godtierbatuhan <boucharay1001@gmail.com>
2025-01-16 17:49:46 +0100

    revert to default pending

--- migrations/Migration20250106093441-S.ts
+++ migrations/Migration20250106093441-S.ts
@@ -6,7 +6,9 @@ export class Migration20250104001707 extends Migration {
         this.addSql("create type \"import_data_item_status_enum\" as enum ('FAILED', 'SUCCESS', 'PENDING');");
 
         // Add the column with the enum type (nullable for now)
-        this.addSql('alter table "importdataitem" add column "status" "import_data_item_status_enum";');
+        this.addSql(
+            'alter table "importdataitem" add column "status" "import_data_item_status_enum" not null default \'PENDING\';',
+        );
     }
 
     public override async down(): Promise<void> {
