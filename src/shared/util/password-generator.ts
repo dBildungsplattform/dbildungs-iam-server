b6b73765c8d398a9410d9e5a1d29a2ad0537efb9
Timo K <timo.kleist@capgemini.com>
2025-02-06 14:25:19 +0100

    generate simple passwords

--- src/shared/util/password-generator.ts
+++ src/shared/util/password-generator.ts
@@ -1,10 +1,40 @@
 import { randomInt } from 'node:crypto';
 
-const LOWERCASE: string = 'abcdefghijklmnopqrstuvwxyz';
-const UPPERCASE: string = LOWERCASE.toUpperCase();
 const NUMBERS: string = '0123456789';
 const SYMBOLS: string = '+-*/%&!?@$#';
-const ALL_CHARACTERS: string = LOWERCASE + UPPERCASE + NUMBERS + SYMBOLS;
+
+const stamm: string[] = [
+    'Sommer',
+    'Winter',
+    'Herbst',
+    'Blumen',
+    'Wolken',
+    'Himmel',
+    'Sonne',
+    'Freunde',
+    'Familie',
+    'Brunnen',
+    'Gebirge',
+    'Inseln',
+    'Koffer',
+    'Laterne',
+    'Lichtung',
+    'Fahrrad',
+    'Garten',
+    'Schule',
+    'Strasse',
+    'Fenster',
+    'Treppe',
+    'Flasche',
+    'Bahnhof',
+    'Schatten',
+    'Taschen',
+    'Computer',
+    'Kalender',
+    'Museum',
+    'Kamera',
+    'Station',
+];
 
 function randomChar(str: string): string {
     return str.charAt(randomInt(str.length));
@@ -12,24 +42,17 @@ function randomChar(str: string): string {
 
 /**
  * Generates a random password with the following rules:
- * - At least one lowercase character
- * - At least one uppercase character
  * - At least one number
  * - At least one symbol
  *
- * Passwords will always be at least 4 characters long, to fulfill these rules
+ * Passwords will always be at least 8 characters long, to fulfill these rules
  * @param length The length of the password
  */
-export function generatePassword(length: number = 8): string {
+export function generatePassword(): string {
     let password: string = '';
-    password += randomChar(LOWERCASE); // One lowercase char
-    password += randomChar(UPPERCASE); // One uppercase char
+    password += stamm[randomInt(stamm.length)]; // Start with a random word
     password += randomChar(NUMBERS); // One number
     password += randomChar(SYMBOLS); // One symbol
-
-    for (let i: number = password.length; i < length; i++) {
-        password += randomChar(ALL_CHARACTERS); // Fill the rest with random characters
-    }
 
     return password;
 }
