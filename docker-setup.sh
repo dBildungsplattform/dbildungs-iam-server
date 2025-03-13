1f986a7fb602e8b902ba5909619e6703abacbeea
Yannick Clausen <yannick.clausen@capgemini.com>
2025-03-13 15:56:41 +0100

    wip:
    loop still happens

--- docker-setup.sh
+++ docker-setup.sh
@@ -7,15 +7,29 @@ echo "start services"
 docker compose --profile backend --profile frontend up -d
 
 echo "waiting for services"
-sleep 20
-docker compose ps
+sleep 10 # todo: use keycloak health-probe instead
+
+echo "creating certificate for ingress"
+pushd nginx || exit 1
+./create-cert.sh
+popd || exit 1
 
 # profiles messes with start order and the ingress will often try to use the ip of a container that was killed during startup
 # reloading makes nginx re-resolve names and get the correct ip
 echo "signal ingress to reload"
 docker compose exec -it ingress nginx -s reload
 
-echo "seeding"
-docker compose exec -it backend node dist/src/console/main.js db migration-apply && node dist/src/console/main.js keycloak update-clients dev && node dist/src/console/main.js db seed dev
+echo "run db migrations"
+docker compose exec -it backend node dist/src/console/main.js db migration-apply
+sleep 1
 
-echo "done"
+echo "update keycloak"
+docker compose exec -it backend node dist/src/console/main.js keycloak update-clients dev
+sleep 1
+
+echo "seed db"
+docker compose exec -it backend node dist/src/console/main.js db seed dev
+sleep 1
+
+docker compose ps
+
