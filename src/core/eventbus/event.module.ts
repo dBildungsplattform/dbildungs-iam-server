3ea12816d11e4e227a008a27d1bf5a4d6c5e9ecc
Timo K <timo.kleist@capgemini.com>
2025-03-24 16:12:39 +0100

    remove microservice package

--- src/core/eventbus/event.module.ts
+++ src/core/eventbus/event.module.ts
@@ -6,29 +6,10 @@ import { LoggerModule } from '../logging/logger.module.js';
 import { EventDiscoveryService } from './services/event-discovery.service.js';
 import { EventService } from './services/event.service.js';
 import { KafkaEventService } from './services/kafka-event.service.js';
-import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Global()
 @Module({
-    imports: [
-        LoggerModule.register(EventModule.name),
-        DiscoveryModule,
-        ClientsModule.register([
-            {
-                name: 'KAFKA_EVENT_SERVICE',
-                transport: Transport.KAFKA,
-                options: {
-                    client: {
-                        clientId: 'app-gateway',
-                        brokers: ['localhost:9094'],
-                    },
-                    consumer: {
-                        groupId: 'kafka-microservices',
-                    },
-                },
-            },
-        ]),
-    ],
+    imports: [LoggerModule.register(EventModule.name), DiscoveryModule],
     providers: [EventService, KafkaEventService, EventDiscoveryService],
     exports: [EventService, KafkaEventService],
 })
