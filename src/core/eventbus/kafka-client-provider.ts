bcab19dc727a00a719cccd1e19a2e7403768789b
Timo K <timo.kleist@capgemini.com>
2025-03-28 15:27:01 +0100

    create kafka instance based on feature flag

--- src/core/eventbus/kafka-client-provider.ts
+++ src/core/eventbus/kafka-client-provider.ts
@@ -4,12 +4,17 @@ import { KafkaConfig } from '../../shared/config/kafka.config.js';
 import { ServerConfig } from '../../shared/config/server.config.js';
 
 import { Provider } from '@nestjs/common';
+import { FeatureFlagConfig } from '../../shared/config/featureflag.config.js';
 
 export const KAFKA_INSTANCE: symbol = Symbol('KAFKA_INSTANCE');
 
-export const KafkaProvider: Provider<Kafka> = {
+export const KafkaProvider: Provider<Kafka | null> = {
     provide: KAFKA_INSTANCE,
-    useFactory: (configService: ConfigService<ServerConfig>): Kafka => {
+    useFactory: (configService: ConfigService<ServerConfig>): Kafka | null => {
+        const featureFlagConfig: FeatureFlagConfig = configService.getOrThrow<FeatureFlagConfig>('FEATUREFLAG');
+        if (featureFlagConfig.FEATURE_FLAG_USE_KAFKA === false) {
+            return null;
+        }
         const kafkaConfig: KafkaConfig = configService.getOrThrow<KafkaConfig>('KAFKA');
         return new Kafka({ brokers: [kafkaConfig.BROKER] });
     },
