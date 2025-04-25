0eacaf63830988fd7c3785806e03e8c91f73a063
DPDS93CT <cornelius.tegtmeyer@dataport.de>
2025-04-25 08:05:05 +0200

    adjust testsuite

--- src/modules/email/email.module.spec.ts
+++ src/modules/email/email.module.spec.ts
@@ -1,24 +1,26 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { ConfigTestModule, DatabaseTestModule, MapperTestModule } from '../../../test/utils/index.js';
+import {
+    ConfigTestModule,
+    DatabaseTestModule,
+    LoggingTestModule,
+    MapperTestModule
+} from "../../../test/utils/index.js";
 import { EmailModule } from './email.module.js';
 import { EmailRepo } from './persistence/email.repo.js';
 import { EmailFactory } from './domain/email.factory.js';
 import { createMock } from '@golevelup/ts-jest';
 import { PersonRepository } from '../person/persistence/person.repository.js';
-import { ClassLogger } from '../../core/logging/class-logger.js';
 
 describe('EmailModule', () => {
     let module: TestingModule;
 
     beforeAll(async () => {
         module = await Test.createTestingModule({
-            imports: [ConfigTestModule, MapperTestModule, DatabaseTestModule.forRoot(), EmailModule],
+            imports: [ConfigTestModule, LoggingTestModule, MapperTestModule, DatabaseTestModule.forRoot(), EmailModule],
             providers: [],
         })
             .overrideProvider(PersonRepository)
             .useValue(createMock<PersonRepository>())
-            .overrideProvider(ClassLogger)
-            .useValue(createMock<ClassLogger>())
             .compile();
     });
 
