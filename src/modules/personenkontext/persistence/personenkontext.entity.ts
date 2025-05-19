926799385fe43e45ec6f22b70ecca0997dada1e9
Philipp Kleybolte <philipp.kleybolte@capgemini.com>
2025-05-19 09:32:22 +0200

    remove personenkontext.do an other deprecated files (#945)
    * remove personenkontext.do an other deprecated files
    
    * remove occurances of deleted class PersonenkontextRepo
    
    * remove automap from personenkontext entity

--- src/modules/personenkontext/persistence/personenkontext.entity.ts
+++ src/modules/personenkontext/persistence/personenkontext.entity.ts
@@ -3,13 +3,11 @@ import { TimestampedEntity } from '../../../persistence/timestamped.entity.js';
 import { Jahrgangsstufe, Personenstatus, SichtfreigabeType } from '../domain/personenkontext.enums.js';
 import { PersonEntity } from '../../person/persistence/person.entity.js';
 import { RolleEntity } from '../../rolle/entity/rolle.entity.js';
-import { AutoMap } from '@automapper/classes';
 import { OrganisationEntity } from '../../organisation/persistence/organisation.entity.js';
 
 @Entity({ tableName: 'personenkontext' })
 @Unique({ properties: ['personId', 'organisationId', 'rolleId'] })
 export class PersonenkontextEntity extends TimestampedEntity {
-    @AutoMap()
     @ManyToOne({
         fieldName: 'person_id',
         columnType: 'uuid',
@@ -19,11 +17,9 @@ export class PersonenkontextEntity extends TimestampedEntity {
         entity: () => PersonEntity,
     })
     @Index({ name: 'personenkontext_person_id_index', type: 'hash' })
-    @Index({ name: 'personenkontext_person_id_index', type: 'hash' })
     public personId!: Ref<PersonEntity>;
 
     // TODO EW-636: get from access_token, see SchulConneX (Version 1.003.003.000) page 91
-    @AutoMap()
     @ManyToOne({
         fieldName: 'organisation_id',
         columnType: 'uuid',
@@ -42,40 +38,31 @@ export class PersonenkontextEntity extends TimestampedEntity {
         entity: () => RolleEntity,
     })
     @Index({ name: 'personenkontext_rolle_id_index', type: 'hash' })
-    @Index({ name: 'personenkontext_rolle_id_index', type: 'hash' })
     public rolleId!: Ref<RolleEntity>;
 
-    @AutoMap()
     @Property({ nullable: true })
     public referrer?: string;
 
     // TODO EW-636: mandant is related to organizations so it is not set for now. When implemented should be set to nullable: false
 
-    @AutoMap()
     @Property({ nullable: true })
     public mandant?: string;
 
-    @AutoMap(() => String)
     @Enum({ nullable: true, items: () => Personenstatus, nativeEnumName: 'personenstatus_enum' })
     public personenstatus?: Personenstatus;
 
-    @AutoMap(() => String)
     @Enum({ nullable: true, items: () => Jahrgangsstufe, nativeEnumName: 'jahrgangsstufe_enum' })
     public jahrgangsstufe?: Jahrgangsstufe;
 
-    @AutoMap(() => String)
     @Property({ nullable: true, default: SichtfreigabeType.NEIN })
     public sichtfreigabe?: SichtfreigabeType;
 
-    @AutoMap(() => Date)
     @Property({ nullable: true, type: DateTimeType })
     public loeschungZeitpunkt?: Date;
 
-    @AutoMap(() => String)
     @Property({ nullable: false, default: '1' })
     public revision!: string & Opt;
 
-    @AutoMap(() => Date)
     @Property({ nullable: true, type: DateTimeType })
     public readonly befristung?: Date;
 }
