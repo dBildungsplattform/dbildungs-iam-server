32e792f35c2ce7cde3633cd974b54ab770f5c9d1
Yannick Clausen <yannick.clausen@capgemini.com>
2025-06-25 15:17:26 +0200

    fix: updated syntax for fileType-Regex

--- src/modules/import/api/import.controller.ts
+++ src/modules/import/api/import.controller.ts
@@ -92,7 +92,7 @@ export class ImportController {
         @UploadedFile(
             new ParseFilePipeBuilder()
                 .addFileTypeValidator({
-                    fileType: 'text/csv|application/vnd.ms-excel', //added application/vnd.ms-excel for firefox compatibility with csv files
+                    fileType: new RegExp('text/csv|application/vnd.ms-excel') //added application/vnd.ms-excel for firefox compatibility with csv files
                 })
                 .build({
                     errorHttpStatusCode: HttpStatus.BAD_REQUEST,
