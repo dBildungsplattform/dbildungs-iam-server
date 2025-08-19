d172b802bf2082045ad654ea84e37c521c6400c6
Salima Hassouni <salima.hassouni@capgemini.com>
2025-08-19 10:52:59 +0000

    DBP-1587-Adjust kafka.config.ts file

--- src/shared/config/kafka.config.ts
+++ src/shared/config/kafka.config.ts
@@ -1,43 +1,45 @@
-import { IsBoolean, IsNumber, IsString } from 'class-validator';
+import { IsBoolean, IsNumber, IsString, ValidateIf } from 'class-validator';
 import { Type } from 'class-transformer';
-
+ 
 export class KafkaConfig {
-    @IsString()
-    public readonly BROKER!: string;
-
-    @IsString()
-    public readonly TOPIC_PREFIX!: string;
-
-    @IsString()
-    public readonly USER_TOPIC!: string;
-
-    @IsString()
-    public readonly USER_DLQ_TOPIC!: string;
-
-    @IsString()
-    public readonly GROUP_ID!: string;
-
-    @IsNumber()
-    public readonly SESSION_TIMEOUT!: number;
-
-    @IsNumber()
-    public readonly HEARTBEAT_INTERVAL!: number;
-
-    @IsBoolean()
-    public readonly ENABLED!: boolean;
-
-    @IsBoolean()
-    public readonly KAFKA_SSL_ENABLED!: boolean;
-
-    @ValidateIf((o: KafkaConfig) => o.KAFKA_SSL_ENABLED === true)
-    @IsString()
-    public readonly KAFKA_SSL_CA_PATH?: string;
-
-    @ValidateIf((o: KafkaConfig) => o.KAFKA_SSL_ENABLED === true)
-    @IsString()
-    public readonly KAFKA_SSL_CERT_PATH?: string;
-
-    @ValidateIf((o: KafkaConfig) => o.KAFKA_SSL_ENABLED === true)
-    @IsString()
-    public readonly KAFKA_SSL_KEY_PATH?: string;
+  @IsString()
+  public readonly BROKER!: string;
+ 
+  @IsString()
+  public readonly TOPIC_PREFIX!: string;
+ 
+  @IsString()
+  public readonly USER_TOPIC!: string;
+ 
+  @IsString()
+  public readonly USER_DLQ_TOPIC!: string;
+ 
+  @IsString()
+  public readonly GROUP_ID!: string;
+ 
+  @Type(() => Number)
+  @IsNumber()
+  public readonly SESSION_TIMEOUT!: number;
+ 
+  @Type(() => Number)
+  @IsNumber()
+  public readonly HEARTBEAT_INTERVAL!: number;
+ 
+  @IsBoolean()
+  public readonly ENABLED!: boolean;
+ 
+  @IsBoolean()
+  public readonly KAFKA_SSL_ENABLED!: boolean;
+ 
+  @ValidateIf(o => o.KAFKA_SSL_ENABLED === true)
+  @IsString()
+  public readonly KAFKA_SSL_CA_PATH?: string;
+ 
+  @ValidateIf(o => o.KAFKA_SSL_ENABLED === true)
+  @IsString()
+  public readonly KAFKA_SSL_CERT_PATH?: string;
+ 
+  @ValidateIf(o => o.KAFKA_SSL_ENABLED === true)
+  @IsString()
+  public readonly KAFKA_SSL_KEY_PATH?: string;
 }
