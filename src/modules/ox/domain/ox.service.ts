9aec70869bf71f707054c678c85a8e4b49f227c5
DPDS93CT <cornelius.tegtmeyer@dataport.de>
2025-08-26 16:52:41 +0200

    add tests, add ox-non-retryable-error

--- src/modules/ox/domain/ox.service.ts
+++ src/modules/ox/domain/ox.service.ts
@@ -13,7 +13,7 @@ import { ServerConfig } from '../../../shared/config/server.config.js';
 import { DomainError } from '../../../shared/error/domain.error.js';
 import { OxErrorMapper } from './ox-error.mapper.js';
 import { ClassLogger } from '../../../core/logging/class-logger.js';
-import { OxMemberAlreadyInGroupError } from '../error/ox-member-already-in-group.error.js';
+import { OxNonRetryableError } from '../error/ox-non-retryable.error.js';
 
 export type OxErrorType = {
     message: string;
@@ -79,7 +79,11 @@ export class OxService {
                     throw result.error;
                 }
             } catch (error) {
-                if (failCounter < this.max_retries && !(error instanceof OxMemberAlreadyInGroupError)) {
+                if (error instanceof OxNonRetryableError) {
+                    this.logger.info('Skipping retry for non-retryable error', error);
+                    return result;
+                }
+                if (failCounter < this.max_retries) {
                     this.logger.logUnknownAsError(
                         `Attempt ${failCounter + 1} failed. Retrying in ${delay}ms... Remaining retries: ${this.max_retries - failCounter}`,
                         error,
--- src/modules/ox/error/ox-member-already-in-group.error.ts
+++ src/modules/ox/error/ox-member-already-in-group.error.ts
@@ -1,6 +1,6 @@
-import { OxError } from '../../../shared/error/ox.error.js';
+import { OxNonRetryableError } from './ox-non-retryable.error.js';
 
-export class OxMemberAlreadyInGroupError extends OxError {
+export class OxMemberAlreadyInGroupError extends OxNonRetryableError {
     public constructor(message: string, details?: unknown[] | Record<string, unknown>) {
         super(message, 'OX_MEMBER_ALREADY_IN_GROUP_ERROR', details);
     }
