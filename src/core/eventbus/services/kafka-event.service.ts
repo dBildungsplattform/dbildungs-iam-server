d77d9cb7114aaab0b6d2302abed48b84cf54bbf0
Marvin Rode <marvin.rode@capgemini.com>
2025-09-08 11:03:49 +0200

    Try keepalive

--- src/core/eventbus/services/kafka-event.service.ts
+++ src/core/eventbus/services/kafka-event.service.ts
@@ -59,7 +59,11 @@ export class KafkaEventService implements OnModuleInit, OnModuleDestroy {
             'allow.auto.create.topics': false,
             'enable.auto.commit': true,
         });
-        this.producer = this.kafka.producer();
+        this.producer = this.kafka.producer({
+            'allow.auto.create.topics': false,
+            'log.connection.close': true, // 0.9 Broker? Is this relevant for us and might be the cause of disconnect?
+            'socket.keepalive.enable': true, // Do we want to do this?
+        });
     }
 
     public async onModuleInit(): Promise<void> {
