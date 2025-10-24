cdb778985a4e2e7f68a0744832366b7dea99a3bf
martkrau <151523378+martkrau@users.noreply.github.com>
2025-10-24 10:35:44 +0200

    SPSH-2359 (#1105)
    * get endpoint for addresses with status; find all emails with statuses via em populate join
    
    * emailAddress and status are now in dto object
    
    * tests
    
    * lint
    
    * changed order of last status
    
    * sort statuses descending in repo; removed logger in service; adapted tests
    
    * pr comment fixes
    
    * pr comment fixes
    
    * new test for test coverage
    
    * removed service; now test coverage
    
    * lint fix

--- src/email/modules/core/api/controller/email-read.controller.ts
+++ src/email/modules/core/api/controller/email-read.controller.ts
@@ -11,19 +11,22 @@ import {
 import { FindEmailAddressBySpshPersonIdParams } from '../dtos/params/find-email-address-by-spsh-person-id.params.js';
 import { EmailAddressResponse } from '../dtos/response/email-address.response.js';
 import { ClassLogger } from '../../../../../core/logging/class-logger.js';
-import { EmailAddress } from '../../domain/email-address.js';
 import { Public } from '../../decorator/public.decorator.js';
+import { AddressWithStatusesDescDto } from '../dtos/address-with-statuses/address-with-statuses-desc.dto.js';
 import { EmailAddressStatus } from '../../domain/email-address-status.js';
-import { EmailAddressStatusEnum } from '../../persistence/email-address-status.entity.js';
+import { EmailAddressRepo } from '../../persistence/email-address.repo.js';
 
 @ApiTags('email')
 @ApiBearerAuth()
 @ApiOAuth2(['openid'])
 @Controller({ path: 'read' })
 export class EmailReadController {
-    public constructor(private readonly logger: ClassLogger) {}
+    public constructor(
+        private readonly logger: ClassLogger,
+        private readonly emailAddressRepo: EmailAddressRepo,
+    ) {}
 
-    @Get(':personId')
+    @Get(':spshPersonId')
     @Public()
     @ApiOperation({ description: 'Get email-addresses by personId.' })
     @ApiOkResponse({
@@ -31,32 +34,29 @@ export class EmailReadController {
         type: [EmailAddressResponse],
     })
     @ApiInternalServerErrorResponse({ description: 'Internal server error while getting email-addresses by personId.' })
-    // eslint-disable-next-line @typescript-eslint/require-await
     public async findEmailAddressesByPersonId(
         @Param() findEmailAddressByPersonIdParams: FindEmailAddressBySpshPersonIdParams,
     ): Promise<EmailAddressResponse[]> {
-        //currently just a dummy
         this.logger.info(`PersonId:${findEmailAddressByPersonIdParams.spshPersonId}`);
-        const emailAddress: EmailAddress<true> = EmailAddress.construct({
-            id: '0',
-            createdAt: new Date(),
-            updatedAt: new Date(),
-            address: 'test@schule-sh.de',
-            priority: 0,
-            spshPersonId: undefined,
-            oxUserId: undefined,
-            markedForCron: undefined,
-        });
 
-        const emailStatus: EmailAddressStatus<true> = EmailAddressStatus.construct({
-            id: '0',
-            createdAt: new Date(),
-            updatedAt: new Date(),
-            emailAddressId: emailAddress.id,
-            status: EmailAddressStatusEnum.PENDING,
-        });
+        const addresses: AddressWithStatusesDescDto[] =
+            await this.emailAddressRepo.findAllEmailAddressesWithStatusesDescBySpshPersonId(
+                findEmailAddressByPersonIdParams.spshPersonId,
+            );
 
-        const response: EmailAddressResponse = new EmailAddressResponse(emailAddress, emailStatus);
-        return [response];
+        if (addresses.length === 0) {
+            return [];
+        }
+
+        return addresses
+            .filter((address: AddressWithStatusesDescDto) => address.statuses.length > 0)
+            .map((address: AddressWithStatusesDescDto) => {
+                const status: EmailAddressStatus<true> | undefined = address.statuses.at(0);
+                if (status) {
+                    return new EmailAddressResponse(address.emailAddress, status);
+                }
+                return undefined;
+            })
+            .filter((response: EmailAddressResponse | undefined) => response !== undefined);
     }
 }
