b75ab30eab3a615bd9a9018909996dcde69d0fa5
Caspar Neumann <caspar.neumann@capgemini.com>
2025-10-24 13:21:04 +0200

    Remove Uneeded Error

--- src/email/modules/ox/error/ox-sync.error.ts
+++
@@ -1,7 +0,0 @@
-import { OxError } from '../../../../shared/error/ox.error.js';
-
-export class OxSyncError extends OxError {
-    public constructor(message: string, details?: unknown[] | Record<string, unknown>) {
-        super(message, 'OX_SYNC_ERROR', details);
-    }
-}
