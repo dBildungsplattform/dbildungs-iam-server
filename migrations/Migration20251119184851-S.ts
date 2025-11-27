b54644f6d38c18d3ad680dc5ec34fa5007e142e5
Marvin Rode <marvin.rode@capgemini.com>
2025-11-27 10:31:49 +0100

    migration

--- migrations/Migration20251119184851-S.ts
+++ migrations/Migration20251119184851-S.ts
@@ -10,6 +10,12 @@ export class Migration20251119184851 extends Migration {
             'alter table "email"."address" alter column "spsh_person_id" type varchar(255) using ("spsh_person_id"::varchar(255));',
         );
         this.addSql('alter table "email"."address" alter column "spsh_person_id" set not null;');
+
+        this.addSql('alter table "email"."address_status" drop constraint "address_status_email_address_id_foreign";');
+
+        this.addSql(
+            'alter table "email"."address_status" add constraint "address_status_email_address_id_foreign" foreign key ("email_address_id") references "email"."address" ("id") on update cascade on delete cascade;',
+        );
     }
 
     public override async down(): Promise<void> {
@@ -21,5 +27,11 @@ export class Migration20251119184851 extends Migration {
             'alter table "email"."address" alter column "spsh_person_id" type varchar(255) using ("spsh_person_id"::varchar(255));',
         );
         this.addSql('alter table "email"."address" alter column "spsh_person_id" drop not null;');
+
+        this.addSql('alter table "email"."address_status" drop constraint "address_status_email_address_id_foreign";');
+
+        this.addSql(
+            'alter table "email"."address_status" add constraint "address_status_email_address_id_foreign" foreign key ("email_address_id") references "email"."address" ("id") on update cascade;',
+        );
     }
 }
