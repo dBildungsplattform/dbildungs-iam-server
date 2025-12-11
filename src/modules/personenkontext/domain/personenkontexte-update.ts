eb339ab8c04863a41b54433c7fb075b504fdf79e
Marvin Rode <marvin.rode@capgemini.com>
2025-12-11 11:59:48 +0100

    change specification

--- src/modules/personenkontext/domain/personenkontexte-update.ts
+++ src/modules/personenkontext/domain/personenkontexte-update.ts
@@ -27,7 +27,7 @@ import { PersonenkontextBefristungRequiredError } from './error/personenkontext-
 import { DBiamPersonenkontextRepoInternal } from '../persistence/internal-dbiam-personenkontext.repo.js';
 import { EventRoutingLegacyKafkaService } from '../../../core/eventbus/services/event-routing-legacy-kafka.service.js';
 import { KafkaPersonenkontextUpdatedEvent } from '../../../shared/events/kafka-personenkontext-updated.event.js';
-import { LernHatKlasse } from '../specification/lern-hat-klasse.js';
+import { LernAnSchuleUndKlasse } from '../specification/lern-an-schule-und-klasse.js';
 import { LernHatKeineKlasseError } from '../specification/error/lern-hat-keine-klasse.error.js';
 import { DuplicatePersonalnummerError } from '../../../shared/error/duplicate-personalnummer.error.js';
 import { CheckDuplicateKlassenkontextSpecification } from '../specification/check-duplicate-klassenkontext.js';
@@ -311,12 +311,13 @@ export class PersonenkontexteUpdate {
         return undefined;
     }
 
-    private async checkLernHatKlasseSpecification(
+    private async checkLernAnSchuleUndKlasseSpecification(
         sentPKs: Personenkontext<boolean>[],
     ): Promise<Option<PersonenkontexteUpdateError>> {
-        const isSatisfied: boolean = await new LernHatKlasse(this.organisationRepo, this.rolleRepo).isSatisfiedBy(
-            sentPKs,
-        );
+        const isSatisfied: boolean = await new LernAnSchuleUndKlasse(
+            this.organisationRepo,
+            this.rolleRepo,
+        ).isSatisfiedBy(sentPKs);
 
         if (!isSatisfied) {
             return new LernHatKeineKlasseError();
@@ -354,7 +355,7 @@ export class PersonenkontexteUpdate {
         }
 
         const validationForLernHatKlasseError: Option<PersonenkontexteUpdateError> =
-            await this.checkLernHatKlasseSpecification(sentPKs);
+            await this.checkLernAnSchuleUndKlasseSpecification(sentPKs);
         if (validationForLernHatKlasseError) {
             return validationForLernHatKlasseError;
         }
---
+++ src/modules/personenkontext/specification/lern-an-schule-und-klasse.ts
@@ -0,0 +1,80 @@
+import { uniq } from 'lodash-es';
+
+import { OrganisationID, RolleID } from '../../../shared/types/aggregate-ids.types.js';
+import { OrganisationsTyp } from '../../organisation/domain/organisation.enums.js';
+import { Organisation } from '../../organisation/domain/organisation.js';
+import { OrganisationRepository } from '../../organisation/persistence/organisation.repository.js';
+import { RollenArt } from '../../rolle/domain/rolle.enums.js';
+import { Rolle } from '../../rolle/domain/rolle.js';
+import { RolleRepo } from '../../rolle/repo/rolle.repo.js';
+import { CompositeSpecification } from '../../specification/specifications.js';
+import { Personenkontext } from '../domain/personenkontext.js';
+
+export class LernAnSchuleUndKlasse extends CompositeSpecification<Array<Personenkontext<boolean>>> {
+    public constructor(
+        private readonly organisationRepository: OrganisationRepository,
+        private readonly rolleRepo: RolleRepo,
+    ) {
+        super();
+    }
+
+    public async isSatisfiedBy(pks: Array<Personenkontext<boolean>>): Promise<boolean> {
+        const lernPKs: Personenkontext<boolean>[] = await this.filterLernPKs(pks);
+
+        if (lernPKs.length === 0) {
+            return true;
+        }
+
+        return await this.isEveryRolleAtMatchingSchuleAndKlasse(lernPKs);
+    }
+
+    /**
+     * @param pks List of all Personenkontexte
+     * @returns all Personenkontexte with rollenart LERN
+     */
+    private async filterLernPKs(pks: Personenkontext<boolean>[]): Promise<Personenkontext<boolean>[]> {
+        const rolleIDs: RolleID[] = uniq(pks.map((pk: Personenkontext<boolean>) => pk.rolleId));
+
+        const rollen: Map<RolleID, Rolle<true>> = await this.rolleRepo.findByIds(rolleIDs);
+
+        for (const [rolleID, rolle] of rollen) {
+            if (rolle.rollenart !== RollenArt.LERN) {
+                rollen.delete(rolleID);
+            }
+        }
+
+        return pks.filter((pk: Personenkontext<boolean>) => rollen.has(pk.rolleId));
+    }
+
+    private async isEveryRolleAtMatchingSchuleAndKlasse(pks: Personenkontext<boolean>[]): Promise<boolean> {
+        const orgaIDs: OrganisationID[] = uniq(pks.map((pk: Personenkontext<boolean>) => pk.organisationId));
+        const organisationen: Map<OrganisationID, Organisation<true>> = await this.organisationRepository.findByIds(
+            orgaIDs,
+        );
+
+        const pksAtSchulen: Personenkontext<boolean>[] = pks.filter(
+            (pk: Personenkontext<boolean>) => organisationen.get(pk.organisationId)?.typ === OrganisationsTyp.SCHULE,
+        );
+        const pksAtKlassen: Personenkontext<boolean>[] = pks.filter(
+            (pk: Personenkontext<boolean>) => organisationen.get(pk.organisationId)?.typ === OrganisationsTyp.KLASSE,
+        );
+
+        const everySchoolPKHasKlassePK: boolean = pksAtSchulen.every((schulePk: Personenkontext<boolean>) =>
+            pksAtKlassen.find(
+                (klassePk: Personenkontext<boolean>) =>
+                    klassePk.rolleId === schulePk.rolleId &&
+                    organisationen.get(klassePk.organisationId)?.administriertVon === schulePk.organisationId,
+            ),
+        );
+
+        const everyKlassePKHasSchulePK: boolean = pksAtKlassen.every((klassePk: Personenkontext<boolean>) =>
+            pksAtSchulen.find(
+                (schulePk: Personenkontext<boolean>) =>
+                    schulePk.rolleId === klassePk.rolleId &&
+                    organisationen.get(klassePk.organisationId)?.administriertVon === schulePk.organisationId,
+            ),
+        );
+
+        return everySchoolPKHasKlassePK && everyKlassePKHasSchulePK;
+    }
+}
