2c7c11d6f8cd35f61cb5602e94cb08f48d609872
Marvin Rode <marvin.rode@capgemini.com>
2026-01-16 08:57:54 +0100

    Fix klasse deletion

--- src/shared/events/kafka-klasse-deleted.event.ts
+++
@@ -1,8 +0,0 @@
-import { KafkaEvent } from './kafka-event.js';
-import { KlasseDeletedEvent } from './klasse-deleted.event.js';
-
-export class KafkaKlasseDeletedEvent extends KlasseDeletedEvent implements KafkaEvent {
-    public get kafkaKey(): string {
-        return this.organisationId;
-    }
-}
