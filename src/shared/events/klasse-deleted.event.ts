551d548d72e2118d9314c473d944adbe909b485c
Marvin Rode (Cap) <127723478+marode-cap@users.noreply.github.com>
2026-01-19 14:25:45 +0100

    SPSH-3166 Fix itslearning organisation delete (#1152)
    * Fix klasse deletion
    
    * Fix lint
    
    * Fix root check
    
    * Fix logging in kafka service

--- src/shared/events/klasse-deleted.event.ts
+++
@@ -1,8 +0,0 @@
-import { BaseEvent } from './base-event.js';
-import { OrganisationID } from '../types/index.js';
-
-export class KlasseDeletedEvent extends BaseEvent {
-    public constructor(public readonly organisationId: OrganisationID) {
-        super();
-    }
-}
