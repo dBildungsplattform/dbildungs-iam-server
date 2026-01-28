6542764391875863ddd303c990fbad475e8610a7
Marvin Rode <marvin.rode@capgemini.com>
2026-01-28 17:04:39 +0100

    Update seeding generator and create some data

--- src/console/dbseed/db-seed-data-generator.console.integration-spec.ts
+++ src/console/dbseed/db-seed-data-generator.console.integration-spec.ts
@@ -1,10 +1,29 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { DEFAULT_TIMEOUT_FOR_TESTCONTAINERS, LoggingTestModule } from '../../../test/utils/index.js';
-import { DbSeedDataGeneratorConsole } from './db-seed-data-generator.console.js';
+import { DbSeedDataGeneratorConsole, SeedDataGeneratorOptions } from './db-seed-data-generator.console.js';
+import fs from 'fs/promises';
+import path from 'path';
+import os from 'os';
 
 describe('DbSeedDataGeneratorConsoleIntegration', () => {
     let module: TestingModule;
     let sut: DbSeedDataGeneratorConsole;
+
+    let tempdir: string;
+
+    const defaultOptions: SeedDataGeneratorOptions = {
+        baseId: 1,
+        classesPerSchool: 1,
+        personPassword: 'SPSHtest1!',
+        schoolCount: 1,
+        schoolName: 'Schule',
+        studentCount: 1,
+        studentName: 'schueler',
+        studentRoleId: 1,
+        teacherCount: 1,
+        teacherRoleId: 2,
+        teacherName: 'Lehrer',
+    };
 
     beforeAll(async () => {
         module = await Test.createTestingModule({
@@ -12,6 +31,8 @@ describe('DbSeedDataGeneratorConsoleIntegration', () => {
             providers: [DbSeedDataGeneratorConsole],
         }).compile();
         sut = module.get(DbSeedDataGeneratorConsole);
+
+        tempdir = await fs.mkdtemp(path.join(os.tmpdir(), 'seed-data-test-'));
     }, DEFAULT_TIMEOUT_FOR_TESTCONTAINERS);
 
     afterAll(async () => {
@@ -23,114 +44,86 @@ describe('DbSeedDataGeneratorConsoleIntegration', () => {
     });
 
     describe('run', () => {
-        describe('when parameter for numberOfSchools and classesPerSchool are provided and values are valid', () => {
+        describe('when options are provided and valid', () => {
             it('should NOT fail', async () => {
-                const params: string[] = [
-                    'seeding/seeding-integration-test/organisation/',
-                    'numberOfSchools=1',
-                    'classesPerSchool=3',
-                ];
-                await expect(sut.run(params)).resolves.not.toThrow();
+                const params: string[] = [tempdir];
+                await expect(sut.run(params, defaultOptions)).resolves.not.toThrow();
             });
         });
 
         describe('when the directory is not provided', () => {
             it('should fail with error', async () => {
-                await expect(sut.run([])).rejects.toThrow(new Error('No directory provided!'));
-            });
-        });
-
-        describe('when the parameter numberOfSchools is not provided', () => {
-            it('should fail with error', async () => {
-                await expect(sut.run(['seeding/seeding-integration-test/organisation/'])).rejects.toThrow(
-                    new Error('The parameter numberOfSchools was not provided'),
-                );
-            });
-        });
-
-        describe('when the parameter classesPerSchool is not provided', () => {
-            it('should fail with error', async () => {
-                const params: string[] = ['seeding/seeding-integration-test/organisation/', 'numberOfSchools=50'];
-                await expect(sut.run(params)).rejects.toThrow(
-                    new Error('The parameter classesPerSchool was not provided'),
-                );
-            });
-        });
-
-        describe('when the value of parameter numberOfSchools is negative', () => {
-            it('should fail with error', async () => {
-                const params: string[] = [
-                    'seeding/seeding-integration-test/organisation/',
-                    'numberOfSchools=-1',
-                    'classesPerSchool=10',
-                ];
-                await expect(sut.run(params)).rejects.toThrow(
-                    Error('The value for numberOfSchools provided muss be greater than 0'),
-                );
-            });
-        });
-
-        describe('when the value of parameter numberOfSchools is 0', () => {
-            it('should fail with error', async () => {
-                const params: string[] = [
-                    'seeding/seeding-integration-test/organisation/',
-                    'numberOfSchools=0',
-                    'classesPerSchool=10',
-                ];
-                await expect(sut.run(params)).rejects.toThrow(
-                    Error('The value for numberOfSchools provided muss be greater than 0'),
-                );
-            });
-        });
-
-        describe('when the value of parameter classesPerSchool is negative', () => {
-            it('should fail with error', async () => {
-                const params: string[] = [
-                    'seeding/seeding-integration-test/organisation/',
-                    'numberOfSchools=1',
-                    'classesPerSchool=-1',
-                ];
-                await expect(sut.run(params)).rejects.toThrow(
-                    Error('The value for classesPerSchool provided muss be greater than 0'),
-                );
-            });
-        });
-
-        describe('when the value of parameter classesPerSchool is 0', () => {
-            it('should fail with error', async () => {
-                const params: string[] = [
-                    'seeding/seeding-integration-test/organisation/',
-                    'numberOfSchools=2',
-                    'classesPerSchool=0',
-                ];
-                await expect(sut.run(params)).rejects.toThrow(
-                    Error('The value for classesPerSchool provided muss be greater than 0'),
-                );
-            });
-        });
-
-        describe('when the value of parameter classesPerSchool is not divisible by 3', () => {
-            it('should fail with error', async () => {
-                const params: string[] = [
-                    'seeding/seeding-integration-test/organisation/',
-                    'numberOfSchools=50',
-                    'classesPerSchool=1',
-                ];
-                await expect(sut.run(params)).rejects.toThrow(
-                    Error('The value for classesPerSchool provided muss be divisible by 3'),
-                );
+                await expect(sut.run([], defaultOptions)).rejects.toThrow(new Error('No directory provided!'));
             });
         });
 
         describe('if directory cannot be found', () => {
             it('should fail with error', async () => {
-                const params: string[] = [
-                    'seeding/seeding-integration-test/fakeDir/',
-                    'numberOfSchools=2',
-                    'classesPerSchool=3',
-                ];
-                await expect(sut.run(params)).rejects.toThrow();
+                const params: string[] = [`${tempdir}fake`];
+
+                await expect(sut.run(params, defaultOptions)).rejects.toThrow();
             });
+        });
+
+        describe('validate options', () => {
+            it.each([
+                { baseId: -1 },
+                { schoolCount: -1 },
+                { classesPerSchool: -1 },
+                { teacherCount: -1 },
+                { studentCount: -1 },
+                { teacherRoleId: NaN },
+                { studentRoleId: NaN },
+            ] satisfies Partial<SeedDataGeneratorOptions>[])(
+                'Should fail when %s',
+                async (optionsMixin: Partial<SeedDataGeneratorOptions>) => {
+                    const params: string[] = [tempdir];
+                    const options: SeedDataGeneratorOptions = { ...defaultOptions, ...optionsMixin };
+                    await expect(sut.run(params, options)).rejects.toThrow();
+                },
+            );
+        });
+    });
+
+    describe('option parsers', () => {
+        it('should parse baseId', () => {
+            expect(sut.parseBaseId('10')).toBe(10);
+        });
+
+        it('should parse schoolCount', () => {
+            expect(sut.parseSchoolCount('10')).toBe(10);
+        });
+
+        it('should parse schoolName', () => {
+            expect(sut.parseSchoolName('SchulePrefix')).toBe('SchulePrefix');
+        });
+
+        it('should parse classesPerSchool', () => {
+            expect(sut.parseClassesPerSchool('10')).toBe(10);
+        });
+
+        it('should parse teacherCount', () => {
+            expect(sut.parseTeacherCount('10')).toBe(10);
+        });
+
+        it('should parse teacherName', () => {
+            expect(sut.parseTeacherName('LehrerPrefix')).toBe('LehrerPrefix');
+        });
+
+        it('should parse studentCount', () => {
+            expect(sut.parseStudentCount('10')).toBe(10);
+        });
+
+        it('should parse studentName', () => {
+            expect(sut.parseStudentName('SchuelerPrefix')).toBe('SchuelerPrefix');
+        });
+
+        it('should parse studentRoleId', () => {
+            expect(sut.parseStudentRoleId('10')).toBe(10);
+        });
+
+        it('should parse personPassword', () => {
+            expect(sut.parsePersonPassword('password')).toBe('password');
         });
     });
 });
