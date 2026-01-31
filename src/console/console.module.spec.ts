a883ccf963d01ca08e72536f1558fb31d020ca2d
godtierbatuhan <boucharay1001@gmail.com>
2026-01-31 20:48:22 +0100

    console.module mocks mikroorm

--- src/console/console.module.spec.ts
+++ src/console/console.module.spec.ts
@@ -4,6 +4,44 @@ import { DbConsole } from './db.console.js';
 import { DbInitConsole } from './db-init.console.js';
 import { ConfigTestModule, LoggingTestModule } from '../../test/utils/index.js';
 
+vi.mock('@mikro-orm/nestjs', async () => {
+    const actual: Record<string, unknown> = await vi.importActual('@mikro-orm/nestjs');
+
+    const { MikroORM, EntityManager }: typeof import('@mikro-orm/core') = await import('@mikro-orm/core');
+    const { SqlEntityManager }: typeof import('@mikro-orm/postgresql') = await import('@mikro-orm/postgresql');
+    type SqlEntityManagerType = import('@mikro-orm/postgresql').SqlEntityManager;
+
+    const mockEntityManager: Partial<SqlEntityManagerType> = {};
+
+    const mockMikroORM: Partial<InstanceType<typeof MikroORM>> = {
+        em: mockEntityManager as SqlEntityManagerType,
+        close: vi.fn(),
+        getMetadata: vi.fn(),
+    };
+
+    const mikroOrmModule: object =
+        typeof actual['MikroOrmModule'] === 'object' && actual['MikroOrmModule'] !== null
+            ? actual['MikroOrmModule']
+            : {};
+
+    return {
+        ...actual,
+        MikroOrmModule: {
+            ...mikroOrmModule,
+            forRootAsync: vi.fn().mockReturnValue({
+                module: class MockMikroOrmModule {},
+                providers: [
+                    { provide: MikroORM, useValue: mockMikroORM },
+                    { provide: EntityManager, useValue: mockEntityManager },
+                    { provide: SqlEntityManager, useValue: mockEntityManager },
+                ],
+                exports: [MikroORM, EntityManager, SqlEntityManager],
+                global: true,
+            }),
+        },
+    };
+});
+
 describe('ConsoleModule', () => {
     let module: TestingModule;
 
