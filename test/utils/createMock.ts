8f2fd03d232770961313d4c361d74b7b5af5082c
godtierbatuhan <boucharay1001@gmail.com>
2026-02-03 11:52:36 +0100

    Review comments

--- test/utils/createMock.ts
+++ test/utils/createMock.ts
@@ -4,29 +4,5 @@ export function createMock<T extends object>(clazz: { prototype: T }, implementa
     // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
     const instance: T = Object.create(clazz.prototype);
     const mockObject: DeepMocked<T> = vi.mockObject<T>(Object.assign(instance, implementation));
-    // if (implementation) {
-    //     for (const key of Object.getOwnPropertyNames(implementation)) {
-    //         // Set the return values of the mock to the implementation provided
-    //         if ((mockObject[key as keyof typeof mockObject] as Mock).mockReset) {
-    //             (mockObject[key as keyof typeof mockObject] as Mock).mockReset();
-    //         }
-    //         // if (
-    //         //     implementation[key as keyof typeof implementation] &&
-    //         //     implementation[key as keyof typeof implementation] instanceof Function
-    //         // ) {
-    //         //     (mockObject[key as keyof typeof mockObject] as Mock).mockImplementation(
-    //         //         implementation[key as keyof typeof implementation] as (...args: unknown[]) => unknown,
-    //         //     );
-    //         // } else if (mockObject[key as keyof typeof mockObject] instanceof Function) {
-    //         //     (mockObject[key as keyof typeof mockObject] as Mock).mockReturnValue(
-    //         //         implementation[key as keyof typeof implementation] as unknown,
-    //         //     );
-    //         // } else {
-    //         //     mockObject[key as keyof typeof mockObject] = implementation[
-    //         //         key as keyof typeof implementation
-    //         //     ] as T[keyof T] as unknown as DeepMocked<T>[keyof DeepMocked<T>];
-    //         // }
-    //     }
-    // }
     return mockObject;
 }
