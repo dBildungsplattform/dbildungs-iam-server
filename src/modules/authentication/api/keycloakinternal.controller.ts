b5232dc7b8063a97aff13ba30b449f028bebdc46
martkrau <martin.krause@capgemini.com>
2026-02-13 12:49:12 +0100

    timeout for testing

--- src/modules/authentication/api/keycloakinternal.controller.ts
+++ src/modules/authentication/api/keycloakinternal.controller.ts
@@ -99,6 +99,10 @@ export class KeycloakInternalController {
                 };
             }
 
+            setTimeout(() => {
+                console.log('wait 200 ms');
+            }, 200);
+
             return UserExternalDataResponse.createNew(
                 workflow.person,
                 workflow.checkedExternalPkData,
