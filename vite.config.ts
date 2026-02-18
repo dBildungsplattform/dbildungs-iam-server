057054eb98e0b6b2de88ad05d2acc3abaf5e9002
Marvin Rode <marvin.rode@capgemini.com>
2026-02-18 08:57:22 +0100

    coverage report fix

--- vite.config.ts
+++ vite.config.ts
@@ -17,7 +17,7 @@ export default defineConfig({
         testTimeout: 30000, // 30 seconds default timeout
         coverage: {
             provider: 'v8',
-            reporter: ['text', 'lcov'],
+            reporter: [['text', { maxCols: 200 }], 'lcov'],
             reportsDirectory: 'coverage',
             reportOnFailure: true,
             include: ['src/**/*.ts'],
