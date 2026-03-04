990abef29558873be1a5a1dbf2f5f00a84f4a66c
martkrau <martin.krause@capgemini.com>
2026-03-04 16:51:00 +0100

    new schulconnex specific errors and filters

---
+++ src/shared/error/shared.error.ts
@@ -0,0 +1,11 @@
+import { DomainError } from './domain.error.js';
+
+export class SharedError extends DomainError {
+    public constructor(
+        public override readonly message: string,
+        public override readonly code: string = 'GENERIC_SHARED_ERROR',
+        public override readonly details?: unknown[] | Record<string, unknown>,
+    ) {
+        super(message, code, details);
+    }
+}
