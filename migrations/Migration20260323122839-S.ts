bd365c6d3c76c6b962da37364d885c2bffe1f371
Kristoff Kiefer <kristoff.kiefer@capgemini.com>
2026-03-23 13:38:57 +0100

    Fixed missing keywords

--- migrations/Migration20260323122839-S.ts
+++ migrations/Migration20260323122839-S.ts
@@ -2,13 +2,13 @@ import { Migration } from '@mikro-orm/migrations';
 
 export class Migration20260323122839S extends Migration {
 
-  async up(): Promise<void> {
+  override async up(): Promise<void> {
     this.addSql('drop index "person_referrer_trgm_index";');
 
     this.addSql('create unique index "person_referrer_trgm_index_unique" on "person" using gin ("username" gin_trgm_ops);;');
   }
 
-  async down(): Promise<void> {
+  override async down(): Promise<void> {
     this.addSql('drop index "person_referrer_trgm_unique_index";');
 
     this.addSql('create index "person_referrer_trgm_index" on "person" using gin ("username" gin_trgm_ops);;');
