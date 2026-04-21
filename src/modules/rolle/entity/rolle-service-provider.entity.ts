ebb3b37d59776cd36ad65eba6cbf88d9d2792c06
Marvin Rode (Cap) <127723478+marode-cap@users.noreply.github.com>
2026-04-21 14:39:00 +0200

    SPSH-3454 Optimize manageable provider query (#1200)
    * Change query
    
    * add seeding data for testing
    
    * coverage
    
    * Also optimize query for rollenerweiterung
    
    * remove seeding data from tests

--- src/modules/rolle/entity/rolle-service-provider.entity.ts
+++ src/modules/rolle/entity/rolle-service-provider.entity.ts
@@ -1,12 +1,14 @@
-import { BaseEntity, Entity, ManyToOne, PrimaryKeyProp, Rel } from '@mikro-orm/core';
+import { BaseEntity, Entity, Index, ManyToOne, PrimaryKeyProp, Rel } from '@mikro-orm/core';
 import { RolleEntity } from './rolle.entity.js';
 import { ServiceProviderEntity } from '../../service-provider/repo/service-provider.entity.js';
 
 @Entity({ tableName: 'rolle_service_provider' })
 export class RolleServiceProviderEntity extends BaseEntity {
+    @Index()
     @ManyToOne({ primary: true, entity: () => RolleEntity })
     public rolle!: Rel<RolleEntity>;
 
+    @Index()
     @ManyToOne({ primary: true, entity: () => ServiceProviderEntity })
     public serviceProvider!: Rel<ServiceProviderEntity>;
 
