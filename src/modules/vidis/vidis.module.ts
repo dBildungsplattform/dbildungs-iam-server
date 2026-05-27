211bfad8d2b79dd5680d26647bf241bb657a6f27
Caspar Neumann <caspar.neumann@capgemini.com>
2026-05-27 08:21:29 +0200

    Remove Controller And Make ApiService Private

--- src/modules/vidis/vidis.module.ts
+++ src/modules/vidis/vidis.module.ts
@@ -2,12 +2,11 @@ import { Module } from '@nestjs/common';
 import { VidisApiService } from './domain/vidis.api-service.js';
 import { HttpModule } from '@nestjs/axios';
 import { LoggerModule } from '../../core/logging/logger.module.js';
-import { VidisTestController } from './api/vidis-test.controller.js';
 
 @Module({
     imports: [LoggerModule.register(VidisModule.name), HttpModule],
     providers: [VidisApiService],
-    exports: [VidisApiService],
-    controllers: [VidisTestController],
+    exports: [],
+    controllers: [],
 })
 export class VidisModule {}
