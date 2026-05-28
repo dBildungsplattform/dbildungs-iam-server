d321038183a8bea196aefea163fc6f631022b713
Caspar Neumann <146704428+casparneumann-cap@users.noreply.github.com>
2026-05-28 09:31:18 +0200

    SPSH-3666 (#1219)

--- src/modules/vidis/vidis.module.ts
+++ src/modules/vidis/vidis.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
-import { VidisService } from './vidis.service.js';
+import { VidisApiService } from './domain/vidis.api-service.js';
 import { HttpModule } from '@nestjs/axios';
 import { LoggerModule } from '../../core/logging/logger.module.js';
 
 @Module({
     imports: [LoggerModule.register(VidisModule.name), HttpModule],
-    providers: [VidisService],
-    exports: [VidisService],
+    providers: [VidisApiService],
+    exports: [],
+    controllers: [],
 })
 export class VidisModule {}
