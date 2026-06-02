03a45957e28691dcf685f38a0c71a1e378f5d79f
Marvin Rode <marvin.rode@capgemini.com>
2026-06-02 09:12:54 +0200

    fix retry error import

--- src/email/modules/ox/adapter/domain/error/ox-displayname-already-used.error.ts
+++ src/email/modules/ox/adapter/domain/error/ox-displayname-already-used.error.ts
@@ -1,4 +1,4 @@
-import { OxNonRetryableError } from '../../../../../../modules/ox/adapter/domain/error/ox-non-retryable.error.js';
+import { OxNonRetryableError } from './ox-non-retryable.error.js';
 
 export class OxDisplaynameAlreadyUsedError extends OxNonRetryableError {
     public constructor(message: string, details?: unknown[] | Record<string, unknown>) {
