4315a95645ccb9ab7adeeba5bfaca58dd9c43f91
Caspar Neumann <caspar.neumann@capgemini.com>
2026-06-08 07:51:07 +0200

    Fix Import

--- src/modules/vidis/vidis.module.ts
+++ src/modules/vidis/vidis.module.ts
@@ -2,10 +2,10 @@ import { Module } from '@nestjs/common';
 import { VidisApiAdapter } from './adapter/domain/vidis-api.adapter.js';
 import { HttpModule } from '@nestjs/axios';
 import { LoggerModule } from '../../core/logging/logger.module.js';
-import { VidisSyncService } from './domain/vidis.sync-service.js';
 import { OrganisationModule } from '../organisation/organisation.module.js';
 import { RolleModule } from '../rolle/rolle.module.js';
 import { ServiceProviderModule } from '../service-provider/service-provider.module.js';
+import { VidisSyncService } from './adapter/domain/vidis.sync-service.js';
 
 @Module({
     imports: [
