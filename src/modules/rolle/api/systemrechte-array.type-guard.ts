0b96f90d02c92cc093920e59fdf8790bd99465aa
Max Bischof <maximilian.bischof@dataport.de>
2026-08-13 09:46:36 +0200

    Remove redundant array check in isRollenSystemRechteArray function

--- src/modules/rolle/api/systemrechte-array.type-guard.ts
+++ src/modules/rolle/api/systemrechte-array.type-guard.ts
@@ -3,9 +3,7 @@ import { RollenSystemRechtEnum } from '../domain/systemrecht.js';
 
 export function isRollenSystemRechteArray(value: unknown): value is Array<RollenSystemRechtEnum> {
     const isValid: boolean =
-        Array.isArray(value) &&
-        Array.isArray(value) &&
-        value.every((recht: unknown) => isEnum(recht, RollenSystemRechtEnum));
+        Array.isArray(value) && value.every((recht: unknown) => isEnum(recht, RollenSystemRechtEnum));
 
     return isValid;
 }
