6e690807d520c7be14825ccc53140a8401833749
Max Bischof <106820326+bischofmax@users.noreply.github.com>
2026-08-18 14:04:24 +0200

    SPSH-3961 - Add role filter to findRollen endpoint (#1266)

---
+++ src/modules/rolle/api/systemrechte-array.type-guard.ts
@@ -0,0 +1,17 @@
+import { isEnum } from 'class-validator';
+import { RollenSystemRechtEnum } from '../domain/systemrecht.js';
+
+export function isRollenSystemRechteArray(value: unknown): value is Array<RollenSystemRechtEnum> {
+    const isValid: boolean =
+        Array.isArray(value) && value.every((recht: unknown) => isEnum(recht, RollenSystemRechtEnum));
+
+    return isValid;
+}
+
+export function toRollenSystemRechteArray(value: unknown): Array<RollenSystemRechtEnum> {
+    if (isRollenSystemRechteArray(value)) {
+        return value;
+    }
+
+    return [];
+}
