972f56fb3c74d513b3bc181c89be1e12260aaf27
martkrau <martin.krause@capgemini.com>
2026-08-20 11:47:47 +0200

    test

--- src/email/modules/health/email-health.controller.ts
+++ src/email/modules/health/email-health.controller.ts
@@ -1,24 +1,38 @@
 import { Controller, Get } from '@nestjs/common';
-import { HealthCheck, HealthCheckResult, HealthCheckService } from '@nestjs/terminus';
+import {
+    HealthCheck,
+    HealthCheckResult,
+    HealthCheckService,
+    HealthIndicatorResult,
+    MikroOrmHealthIndicator,
+} from '@nestjs/terminus';
 import { ApiExcludeController } from '@nestjs/swagger';
 import { Public } from '../../../modules/authentication/api/public.decorator.js';
 
 @Controller('health')
 @ApiExcludeController()
 export class EmailHealthController {
-    public constructor(private readonly health: HealthCheckService) {}
+    public constructor(
+        private readonly health: HealthCheckService,
+        private readonly mikroOrm: MikroOrmHealthIndicator,
+    ) {}
 
     @Get()
     @Public()
     @HealthCheck()
-    public check(): Promise<HealthCheckResult> {
-        return this.health.check([
+    public async check(): Promise<HealthCheckResult> {
+        try {
+            const result: HealthIndicatorResult = await this.mikroOrm.pingCheck('database', {
+                timeout: 1500,
+            });
+
+            console.log('health result', result);
+
             // eslint-disable-next-line @typescript-eslint/require-await
-            async () => ({
-                database: {
-                    status: 'up',
-                },
-            }),
-        ]);
+            return this.health.check([async () => result]);
+        } catch (error) {
+            console.error('health error', error);
+            throw error;
+        }
     }
 }
